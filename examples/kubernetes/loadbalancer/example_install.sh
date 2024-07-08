8e7a01089e781214a98bad8eb268b039d9ca9923
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-07-08 10:51:22 +0200

    lb: introduce more LB examples
    This commit adds some more LB examples / testcases.
    
    - HTTPS only
    - HTTP only
    - Mixed HTTP & HTTPS
    - HTTP with wildcard
    - HTTPS only on port 80
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- examples/kubernetes/loadbalancer/example_install.sh
+++ examples/kubernetes/loadbalancer/example_install.sh
@@ -43,11 +43,16 @@ BGP_FRR_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAd
 kubectl patch bgpp frr --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/virtualRouters/0/neighbors/0/peerAddress\", \"value\":\"${BGP_FRR_IP}/32\"}]"
 
 # LB TLS secret for LB frontend
-kubectl -n default delete secret test 2>/dev/null || true
+kubectl -n default delete secret test-secure 2>/dev/null || true
+kubectl -n default delete secret test-secure80 2>/dev/null || true
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${script_dir}/tls.key" -out "${script_dir}/tls.crt" -subj "/CN=foo.acme.io"
-kubectl -n default create secret tls test --key="${script_dir}/tls.key" --cert="${script_dir}/tls.crt"
-docker cp ${script_dir}/tls.crt frr:/tmp/tls.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${script_dir}/tls-secure.key" -out "${script_dir}/tls-secure.crt" -subj "/CN=secure.acme.io"
+kubectl -n default create secret tls test-secure --key="${script_dir}/tls-secure.key" --cert="${script_dir}/tls-secure.crt"
+docker cp ${script_dir}/tls-secure.crt frr:/tmp/tls-secure.crt
+
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "${script_dir}/tls-secure80.key" -out "${script_dir}/tls-secure80.crt" -subj "/CN=secure-80.acme.io"
+kubectl -n default create secret tls test-secure80 --key="${script_dir}/tls-secure80.key" --cert="${script_dir}/tls-secure80.crt"
+docker cp ${script_dir}/tls-secure80.crt frr:/tmp/tls-secure80.crt
 
 # LB frontends, backends & ippools
 kubectl apply -f "${script_dir}/example/lb-frontends.yaml"
@@ -67,3 +72,8 @@ kubectl patch lbbackend lb-2 --type='json' -p="[{\"op\": \"replace\", \"path\": 
 kubectl patch lbbackend lb-3 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/0/ip\", \"value\":\"${BACKEND2_IP}\"}]"
 kubectl patch lbbackend lb-3 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/1/ip\", \"value\":\"${BACKEND3_IP}\"}]"
 
+kubectl patch lbbackend lb-4 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/0/ip\", \"value\":\"${BACKEND2_IP}\"}]"
+kubectl patch lbbackend lb-4 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/1/ip\", \"value\":\"${BACKEND3_IP}\"}]"
+
+kubectl patch lbbackend lb-5 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/0/ip\", \"value\":\"${BACKEND2_IP}\"}]"
+kubectl patch lbbackend lb-5 --type='json' -p="[{\"op\": \"replace\", \"path\": \"/spec/addresses/1/ip\", \"value\":\"${BACKEND3_IP}\"}]"
--- examples/kubernetes/loadbalancer/example_test.sh
+++ examples/kubernetes/loadbalancer/example_test.sh
@@ -9,11 +9,13 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VIP_LB1=$(kubectl -n default get lbfe lb-1 -ojson | jq -r '.status.vip')
 VIP_LB2=$(kubectl -n default get lbfe lb-2 -ojson | jq -r '.status.vip')
 VIP_LB3=$(kubectl -n default get lbfe lb-3 -ojson | jq -r '.status.vip')
+VIP_LB4=$(kubectl -n default get lbfe lb-4 -ojson | jq -r '.status.vip')
+VIP_LB5=$(kubectl -n default get lbfe lb-5 -ojson | jq -r '.status.vip')
 
 echo "Calling VIPs (might take some time until everything is up & running)"
 
-docker exec frr bash -c "echo -n 'HTTP  T1: ' && curl -s http://${VIP_LB1}:80/"
-docker exec frr bash -c "echo -n 'HTTP  T2: ' && curl -s http://${VIP_LB2}:80/"
-docker exec frr bash -c "echo -n 'HTTP  T3: ' && curl -s http://${VIP_LB3}:81/"
-
-docker exec frr bash -c "echo -n 'HTTPS T1: ' && curl -s --cacert /tmp/tls.crt --resolve foo.acme.io:80:${VIP_LB1} https://foo.acme.io:80/"
+docker exec frr bash -c "echo -n 'HTTPS frontend1: ' && curl -s --cacert /tmp/tls-secure.crt --resolve secure.acme.io:443:${VIP_LB1} https://secure.acme.io:443/"
+docker exec frr bash -c "echo -n 'HTTP  frontend2: ' && curl -s --resolve insecure.acme.io:80:${VIP_LB2} http://insecure.acme.io:80/"
+docker exec frr bash -c "echo -n 'HTTP  frontend3: ' && curl -s http://${VIP_LB3}:81/"
+docker exec frr bash -c "echo -n 'HTTP  frontend4: ' && curl -s --resolve mixed.acme.io:80:${VIP_LB4} http://mixed.acme.io:80/"
+docker exec frr bash -c "echo -n 'HTTPS frontend5: ' && curl -s --cacert /tmp/tls-secure80.crt --resolve secure-80.acme.io:80:${VIP_LB5} https://secure-80.acme.io:80/"
