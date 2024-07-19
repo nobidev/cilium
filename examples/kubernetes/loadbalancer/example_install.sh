caedcb2ac4cbd9097a369b2c572445a3db4deab4
Martynas Pumputis <m@lambda.lt>
2024-07-19 15:20:33 +0200

    ci-lb: Reuse examples/kubernetes/loadbalancer
    DRY.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- examples/kubernetes/loadbalancer/example_install.sh
+++ examples/kubernetes/loadbalancer/example_install.sh
@@ -3,6 +3,7 @@
 set -e          # Exit if any command has a non-zero exit status
 set -u          # Exit in error if there is a reference to a non previously defined variable.
 set -o pipefail # Exit if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline.
+set -x
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
