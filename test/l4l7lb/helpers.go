db8fa5e1cdf2310709e715cb87e6988a7a6708a8
Martynas Pumputis <m@lambda.lt>
2024-07-25 09:46:37 +0200

    test/l4l7lb: Add LB tests
    For now they are replica of examples/lb tests.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

---
+++ test/l4l7lb/helpers.go
@@ -0,0 +1,216 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package l4l7lb
+
+import (
+	"bytes"
+	"context"
+	_ "embed"
+	"fmt"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/docker/docker/api/types/container"
+	docker_client "github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	"github.com/cilium/cilium/pkg/inctimer"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	cilium_clientset "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned"
+)
+
+const (
+	shortTimeout = 30 * time.Second
+	pollInterval = 1 * time.Second
+)
+
+type clients struct {
+	ciliumClientset *cilium_clientset.Clientset
+	dockerClient    *docker_client.Client
+}
+
+func newClients() (*clients, error) {
+	var clients clients
+
+	kubeConfigPath := filepath.Join(homedir.HomeDir(), ".kube", "config")
+
+	restConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		panic(err)
+	}
+
+	httpClient, err := rest.HTTPClientFor(restConfig)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create k8s REST client: %w", err)
+	}
+
+	clients.ciliumClientset, err = cilium_clientset.NewForConfigAndClient(restConfig, httpClient)
+	if err != nil {
+		return nil, fmt.Errorf("unable to create cilium k8s client: %w", err)
+	}
+
+	clients.dockerClient, err = docker_client.NewClientWithOpts(docker_client.FromEnv)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to open Docker client: %w", err)
+	}
+
+	return &clients, err
+}
+
+func (c *clients) CreateLBVIP(ctx context.Context, namespace string, obj *v1alpha1.LBVIP, opts v1.CreateOptions) error {
+	_, err := c.ciliumClientset.IsovalentV1alpha1().LBVIPs(namespace).Create(ctx, obj, opts)
+	return err
+}
+
+func (c *clients) DeleteLBVIP(ctx context.Context, namespace, name string, opts v1.DeleteOptions) error {
+	return c.ciliumClientset.IsovalentV1alpha1().LBVIPs(namespace).Delete(ctx, name, opts)
+}
+
+func (c *clients) CreateLBFrontend(ctx context.Context, namespace string, obj *v1alpha1.LBFrontend, opts v1.CreateOptions) error {
+	_, err := c.ciliumClientset.IsovalentV1alpha1().LBFrontends(namespace).Create(ctx, obj, opts)
+	return err
+}
+
+func (c *clients) DeleteLBFrontend(ctx context.Context, namespace, name string, opts v1.DeleteOptions) error {
+	return c.ciliumClientset.IsovalentV1alpha1().LBFrontends(namespace).Delete(ctx, name, opts)
+}
+
+func (c *clients) GetLBFrontend(ctx context.Context, namespace, name string, opts v1.GetOptions) (*v1alpha1.LBFrontend, error) {
+	return c.ciliumClientset.IsovalentV1alpha1().LBFrontends(namespace).Get(ctx, name, opts)
+}
+
+func (c *clients) CreateLBBackend(ctx context.Context, namespace string, obj *v1alpha1.LBBackend, opts v1.CreateOptions) error {
+	_, err := c.ciliumClientset.IsovalentV1alpha1().LBBackends(namespace).Create(ctx, obj, opts)
+	return err
+}
+
+func (c *clients) DeleteLBBackend(ctx context.Context, namespace, name string, opts v1.DeleteOptions) error {
+	return c.ciliumClientset.IsovalentV1alpha1().LBBackends(namespace).Delete(ctx, name, opts)
+}
+
+func (c *clients) CreateLBIPPool(ctx context.Context, obj *v2alpha1.CiliumLoadBalancerIPPool, opts v1.CreateOptions) error {
+	_, err := c.ciliumClientset.CiliumV2alpha1().CiliumLoadBalancerIPPools().Create(ctx, obj, opts)
+	return err
+}
+
+func (c *clients) DeleteLBIPPool(ctx context.Context, name string, opts v1.DeleteOptions) error {
+	return c.ciliumClientset.CiliumV2alpha1().CiliumLoadBalancerIPPools().Delete(ctx, name, opts)
+}
+
+func (c *clients) GetContainerIP(ctx context.Context, containerName string) (string, error) {
+	obj, err := c.dockerClient.ContainerInspect(ctx, containerName)
+	if err != nil {
+		return "", err
+	}
+
+	for _, network := range obj.NetworkSettings.Networks {
+		return network.IPAddress, nil
+	}
+
+	return "", fmt.Errorf("no network found")
+}
+
+func (c *clients) WaitForLBVIP(ctx context.Context, namespace, name string) (string, error) {
+	ctx, cancel := context.WithTimeout(ctx, shortTimeout)
+	defer cancel()
+
+	for {
+		obj, err := c.GetLBFrontend(ctx, namespace, name, v1.GetOptions{})
+		if err != nil {
+			return "", err
+		}
+
+		if ip := obj.Status.Addresses.IPv4; ip != "" {
+			return ip, nil
+		}
+
+		select {
+		case <-inctimer.After(pollInterval):
+		case <-ctx.Done():
+			return "",
+				fmt.Errorf("timeout reached waiting for LB Frontend %s to get VIP assigned (last error: %w)",
+					name, err)
+		}
+	}
+}
+
+func (c *clients) ContainerExec(ctx context.Context, name string, cmds []string) (string, string, error) {
+	var stdout, stderr bytes.Buffer
+
+	execConfig := container.ExecOptions{
+		AttachStderr: true,
+		AttachStdout: true,
+		Cmd:          cmds,
+	}
+
+	execID, err := c.dockerClient.ContainerExecCreate(ctx, name, execConfig)
+	if err != nil {
+		return "", "", nil
+	}
+
+	resp, err := c.dockerClient.ContainerExecAttach(ctx, execID.ID, container.ExecAttachOptions{})
+	if err != nil {
+		return "", "", err
+	}
+	defer resp.Close()
+
+	_, err = stdcopy.StdCopy(&stdout, &stderr, resp.Reader)
+	if err != nil {
+		return stdout.String(), stderr.String(), err
+	}
+
+	inspect, err := c.dockerClient.ContainerExecInspect(ctx, execID.ID)
+	if err != nil {
+		return stdout.String(), stderr.String(), err
+	}
+
+	if inspect.ExitCode != 0 {
+		return stdout.String(), stderr.String(), fmt.Errorf("cmd failed: %d", inspect.ExitCode)
+	}
+
+	return stdout.String(), stderr.String(), err
+}
+
+func yamlToObjects[T runtime.Object](input string, scheme *runtime.Scheme) (output []T, err error) {
+	if input == "" {
+		return nil, nil
+	}
+
+	yamls := strings.Split(input, "\n---")
+
+	for _, yaml := range yamls {
+		if strings.TrimSpace(yaml) == "" {
+			continue
+		}
+
+		obj, kind, err := serializer.NewCodecFactory(scheme, serializer.EnableStrict).UniversalDeserializer().Decode([]byte(yaml), nil, nil)
+		if err != nil {
+			return nil, fmt.Errorf("decoding yaml file: %s\nerror: %w", yaml, err)
+		}
+
+		switch policy := obj.(type) {
+		case T:
+			output = append(output, policy)
+		default:
+			return nil, fmt.Errorf("unknown type '%s' in: %s", kind.Kind, yaml)
+		}
+	}
+
+	return output, nil
+}
---
+++ test/l4l7lb/lb_test.go
@@ -0,0 +1,184 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package l4l7lb
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"testing"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	scheme "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/scheme"
+)
+
+//go:embed manifests/lb-vips.yaml
+var yamlLBVIPs string
+
+//go:embed manifests/lb-frontends.yaml
+var yamlLBFrontends string
+
+//go:embed manifests/lb-backends.yaml
+var yamlLBBackends string
+
+//go:embed manifests/lb-ippools.yaml
+var yamlLBIPPools string
+
+const (
+	defaultNamespace = "default"
+
+	clientContainerName = "frr"
+)
+
+func TestLB(t *testing.T) {
+	ctx := context.Background()
+
+	clients, err := newClients()
+	if err != nil {
+		t.Fatalf("Failed to create k8s client(s): %s", err)
+	}
+
+	// 1. Install LB VIPS
+
+	lbVIPs, err := yamlToObjects[*v1alpha1.LBVIP](yamlLBVIPs, scheme.Scheme)
+	if err != nil {
+		t.Fatalf("Failed to deserialize LB VIP: %s", err)
+	}
+
+	for _, obj := range lbVIPs {
+		clients.DeleteLBVIP(ctx, defaultNamespace, obj.GetObjectMeta().GetName(), v1.DeleteOptions{})
+
+		t.Logf("Creating LB VIP %s...", obj.GetObjectMeta().GetName())
+		if err := clients.CreateLBVIP(ctx, defaultNamespace, obj, v1.CreateOptions{}); err != nil {
+			t.Fatalf("Failed to creeate LB VIP: %s", err)
+		}
+	}
+
+	// 2. Install LB Frontends
+
+	frontends, err := yamlToObjects[*v1alpha1.LBFrontend](yamlLBFrontends, scheme.Scheme)
+	if err != nil {
+		t.Fatalf("Failed to deserialize LB Frontend: %s", err)
+	}
+
+	for _, obj := range frontends {
+		clients.DeleteLBFrontend(ctx, defaultNamespace, obj.GetObjectMeta().GetName(), v1.DeleteOptions{})
+
+		t.Logf("Creating LB Frontend %s...", obj.GetObjectMeta().GetName())
+		if err := clients.CreateLBFrontend(ctx, "default", obj, v1.CreateOptions{}); err != nil {
+			t.Fatalf("Failed to create LB VIP: %s", err)
+		}
+	}
+
+	// 3. Install LB Backends
+
+	backends, err := yamlToObjects[*v1alpha1.LBBackend](yamlLBBackends, scheme.Scheme)
+	if err != nil {
+		t.Fatalf("Failed to deserialize LB Backend: %s", err)
+	}
+
+	appIPAddrs := map[int]string{}
+	for i := 1; i <= 5; i++ {
+		ip, err := clients.GetContainerIP(ctx, fmt.Sprintf("app%d", i))
+		if err != nil {
+			t.Fatalf("Failed to retrieve container app%d IP: %s", i, err)
+		}
+		if ip == "" {
+			t.Fatalf("app%d does not have any IP addr", i)
+		}
+		appIPAddrs[i] = ip
+	}
+
+	backends[0].Spec.Addresses[0].IP = appIPAddrs[1]
+	backends[0].Spec.Addresses[1].IP = appIPAddrs[2]
+
+	backends[1].Spec.Addresses[0].IP = appIPAddrs[1]
+	backends[1].Spec.Addresses[1].IP = appIPAddrs[3]
+
+	backends[2].Spec.Addresses[0].IP = appIPAddrs[2]
+	backends[2].Spec.Addresses[1].IP = appIPAddrs[3]
+
+	backends[3].Spec.Addresses[0].IP = appIPAddrs[2]
+	backends[3].Spec.Addresses[1].IP = appIPAddrs[3]
+
+	backends[4].Spec.Addresses[0].IP = appIPAddrs[2]
+	backends[4].Spec.Addresses[1].IP = appIPAddrs[3]
+
+	backends[5].Spec.Addresses[0].IP = appIPAddrs[4]
+
+	backends[6].Spec.Addresses[0].IP = appIPAddrs[5]
+
+	for _, obj := range backends {
+		clients.DeleteLBBackend(ctx, defaultNamespace, obj.GetObjectMeta().GetName(), v1.DeleteOptions{})
+
+		t.Logf("Creating LB Backend %s...", obj.GetObjectMeta().GetName())
+		if err := clients.CreateLBBackend(ctx, "default", obj, v1.CreateOptions{}); err != nil {
+			t.Fatalf("Failed to create LB Backend: %s", err)
+		}
+	}
+
+	// 4. Install LB IPPOOLS
+
+	lbIPPools, err := yamlToObjects[*v2alpha1.CiliumLoadBalancerIPPool](yamlLBIPPools, scheme.Scheme)
+	if err != nil {
+		t.Fatalf("Failed to deserialize LB IP Pool: %s", err)
+	}
+
+	for _, obj := range lbIPPools {
+		clients.DeleteLBIPPool(ctx, obj.GetObjectMeta().GetName(), v1.DeleteOptions{})
+
+		t.Logf("Creating LB IP Pool %s...", obj.GetObjectMeta().GetName())
+		if err := clients.CreateLBIPPool(ctx, obj, v1.CreateOptions{}); err != nil {
+			t.Fatalf("Failed to create LB IP Pool: %s", err)
+		}
+	}
+
+	// 5. Wait for LB VIPs
+
+	vips := map[int]string{}
+	for i := 1; i <= 6; i++ {
+		name := fmt.Sprintf("lb-%d", i)
+		t.Logf("Waiting for LB VIP %s...", name)
+		vip, err := clients.WaitForLBVIP(ctx, defaultNamespace, name)
+		if err != nil {
+			t.Fatalf("Failed to wait for LB VIP %s: %s", name, err)
+		}
+		vips[i] = vip
+	}
+
+	// 6. Run connectivity tests
+
+	// TODO(brb) wait for FRR route propagation
+
+	testCmds := []string{
+		fmt.Sprintf("curl -s --cacert /tmp/tls-secure.crt --resolve secure.acme.io:443:%s https://secure.acme.io:443/", vips[1]),
+		fmt.Sprintf("curl -s --resolve insecure.acme.io:80:%s http://insecure.acme.io:80/api/foo-insecure", vips[2]),
+		fmt.Sprintf("curl -s http://%s:81/", vips[3]),
+		fmt.Sprintf("curl -s --resolve mixed.acme.io:80:%s http://mixed.acme.io:80/", vips[4]),
+		fmt.Sprintf("curl -s --cacert /tmp/tls-secure80.crt --resolve secure-80.acme.io:80:%s https://secure-80.acme.io:80/", vips[5]),
+		fmt.Sprintf("curl -s --cacert /tmp/tls-secure-backend.crt --resolve passthrough.acme.io:80:%s https://passthrough.acme.io:80/", vips[6]),
+		fmt.Sprintf("curl -s --cacert /tmp/tls-secure-backend2.crt --resolve passthrough-2.acme.io:80:%s https://passthrough-2.acme.io:80/", vips[6]),
+	}
+
+	for _, cmd := range testCmds {
+		t.Logf("Running cmd %q...", cmd)
+		stdout, stderr, err := clients.ContainerExec(ctx, clientContainerName,
+			[]string{"bash", "-c", cmd},
+		)
+		fmt.Println(stdout, stderr)
+		if err != nil {
+			t.Fatalf("Failed cmd %q: %s (stdout: %s, stderr: %s)", cmd, err, stdout, stderr)
+		}
+	}
+}
