d4b80b786d08c422296efc459e757e9d9a8cec8a
Sebastian Wicki <sebastian@isovalent.com>
2024-08-21 19:13:33 +0200

    enterprise/encryption: Add encryption policy parser
    This commit adds the parsing functionality to parse
    `IsovalentClusterwideEncryptionPolicy` resources. Parsing in this
    context means translating the K8s resource into an internal
    representation (i.e. a list of `parsedSelectorRule`). This ensures that:
    
      1. Each rule has been validated to not contain any invalid selectors
         or ports.
      2. Selectors use the Cilium-native `api.EndpointSelector` type and are
         therefore easier to handle.
    
    Parsing also rejects policies with unsupported constructs (such as e.g.
    peer or port wildcarding).
    
    For forward-compatibility, the label selector parser itself supports
    cases dealing with namespaced resources, even though there is currently
    no way to express those.
    
    For exact details on how the K8s selectors are translated,
    see the inline source code comments. In addition, unit tests are
    provided to further assert the expected behavior.
    
    Co-authored-by: Robin Gögge <r.goegge@isovalent.com>
    Signed-off-by: Robin Gögge <r.goegge@isovalent.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

---
+++ enterprise/pkg/encryption/policy/parse.go
@@ -0,0 +1,207 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package policy
+
+import (
+	"errors"
+	"fmt"
+	"maps"
+	"slices"
+
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	iso_v1alpha1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/labels"
+	networkPolicy "github.com/cilium/cilium/pkg/policy"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/u8proto"
+)
+
+var (
+	allowAllNamespacesRequirement = slim_metav1.LabelSelectorRequirement{
+		Key:      k8sConst.PodNamespaceLabel,
+		Operator: slim_metav1.LabelSelectorOpExists,
+	}
+)
+
+// portProto is a port and proto pair
+type portProto struct {
+	port  uint16
+	proto u8proto.U8proto
+}
+
+// parsedSelectorRule is a parsed IsovalentClusterwideEncryptionPolicy rule
+// where selectors and ports have been converted into Cilium-native types.
+type parsedSelectorRule struct {
+	subject   api.EndpointSelector
+	peer      api.EndpointSelector
+	peerPorts []portProto
+}
+
+// parsePeerPorts parses the IsovalentClusterwideEncryptionPolicy port list into
+// Cilium-native types.
+func parsePeerPorts(ports []iso_v1alpha1.PortProtocol) ([]portProto, error) {
+	peerPorts := make([]portProto, 0, len(ports))
+	for _, p := range ports {
+		proto, err := u8proto.ParseProtocol(p.Protocol)
+		switch {
+		case err != nil:
+			return nil, err
+		case proto == u8proto.ANY:
+			return nil, errors.New("protocol ANY not supported")
+		case p.Port == 0:
+			return nil, errors.New("invalid port: 0")
+		}
+
+		peerPorts = append(peerPorts, portProto{
+			port:  p.Port,
+			proto: proto,
+		})
+	}
+
+	return peerPorts, nil
+}
+
+// parsePodSelector translates a podSelector into a Cilium-native EndpointSelector.
+// An optional namespace can be provided to ensure the selector is evaluated only
+// the context of the provided namespace.
+func parsePodSelector(namespace string, podSelectorIn *slim_metav1.LabelSelector) *slim_metav1.LabelSelector {
+	podSelector := &slim_metav1.LabelSelector{
+		MatchLabels: maps.Clone(podSelectorIn.MatchLabels),
+	}
+	if namespace != "" {
+		// The PodSelector should only reflect to the same namespace
+		// the policy is being stored, thus we add the namespace to
+		// the MatchLabels map.
+		if podSelector.MatchLabels == nil {
+			podSelector.MatchLabels = make(map[string]slim_metav1.MatchLabelsValue, 1)
+		}
+		podSelector.MatchLabels[k8sConst.PodNamespaceLabel] = namespace
+	}
+
+	if len(podSelectorIn.MatchExpressions) > 0 {
+		podSelector.MatchExpressions = make([]slim_metav1.LabelSelectorRequirement, 0, len(podSelectorIn.MatchExpressions))
+		for _, matchExp := range podSelectorIn.MatchExpressions {
+			lsr := slim_metav1.LabelSelectorRequirement{
+				Key:      matchExp.Key,
+				Operator: matchExp.Operator,
+				Values:   slices.Clone(matchExp.Values),
+			}
+			podSelector.MatchExpressions = append(podSelector.MatchExpressions, lsr)
+		}
+	}
+
+	return podSelector
+}
+
+// parseSelector translates a (namespaceSelector, podSelector) pair into a Cilium-native EndpointSelector.
+// An optional namespace can be provided to evaluate the selectors within that namespace in case no
+// namespaceSelector was provided:
+//
+//	namespaceSelector podSelector namespace  Outcome
+//	----------------- ----------- ---------- -----------------------------------------------------------------------------
+//	   Yes               Yes         -       Selects endpoints matching both the namespaceSelector and podSelector
+//	   Yes               No          -       Selects all endpoints within all namespaces selected by the namespaceSelector
+//	   No                Yes         Yes     Selects endpoints matching the podSelector within the provided namespace
+//	   No                Yes         No      Selects endpoints matching the podSelector in all namespaces*
+//	   No                No          Yes     Selects all endpoints within the provided namespace
+//	   No                No          No      Selects all endpoints in all namespaces*
+//
+// Note: Cases marked with an asterisks (*) are currently disallowed by IsovalentClusterwideEncryptionPolicy,
+// but may be supported in the future, which is why this function already supports and tests it.
+func parseSelector(namespace string, namespaceSelectorIn, podSelectorIn *slim_metav1.LabelSelector) api.EndpointSelector {
+	if namespaceSelectorIn != nil {
+		// Namespace selector provided. Translate it into a Cilium label selector and
+		// combine it with the optional pod selector
+		namespaceSelector := &slim_metav1.LabelSelector{
+			MatchLabels: make(map[string]string, len(namespaceSelectorIn.MatchLabels)),
+		}
+		// We use our own special label prefix for namespace metadata,
+		// thus we need to prefix that prefix to all NamespaceSelector.MatchLabels
+		for k, v := range namespaceSelectorIn.MatchLabels {
+			namespaceSelector.MatchLabels[networkPolicy.JoinPath(k8sConst.PodNamespaceMetaLabels, k)] = v
+		}
+
+		// We use our own special label prefix for namespace metadata,
+		// thus we need to prefix that prefix to all NamespaceSelector.MatchExpressions
+		for _, matchExp := range namespaceSelectorIn.MatchExpressions {
+			lsr := slim_metav1.LabelSelectorRequirement{
+				Key:      networkPolicy.JoinPath(k8sConst.PodNamespaceMetaLabels, matchExp.Key),
+				Operator: matchExp.Operator,
+				Values:   slices.Clone(matchExp.Values),
+			}
+			namespaceSelector.MatchExpressions = append(namespaceSelector.MatchExpressions, lsr)
+		}
+
+		// Empty namespace selector selects all namespaces (i.e., a namespace label exists).
+		if len(namespaceSelector.MatchLabels) == 0 && len(namespaceSelector.MatchExpressions) == 0 {
+			namespaceSelector.MatchExpressions = []slim_metav1.LabelSelectorRequirement{allowAllNamespacesRequirement}
+		}
+
+		// Combine translated namespaceSelector with provided podSelector
+		return api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, namespaceSelector, podSelectorIn)
+	} else if podSelectorIn != nil {
+		// Only a podSelector was provided. Make sure it only matches in the current namespace
+		podSelector := parsePodSelector(namespace, podSelectorIn)
+		return api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, podSelector)
+	} else if namespace != "" {
+		// Empty selector on namespaced resource. Make sure we only select endpoints in current namespace
+		podSelector := &slim_metav1.LabelSelector{MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			k8sConst.PodNamespaceLabel: namespace,
+		}}
+		return api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, podSelector)
+	}
+
+	// Empty podSelector on non-namespaced resource selects all endpoints
+	return api.WildcardEndpointSelector
+}
+
+// parseEncryptionPolicy translates an IsovalentClusterwideEncryptionPolicy spec into a list parsedSelectorTuple structs.
+// It basically performs the following transformation:
+//
+//	(subjectSelector, [(peerSelector, peerPorts)]) -> [(subjectSelector, peerSelector, peerPorts)]
+//
+// It also translates the subject and peer K8s label selectors into Cilium-native endpoint selectors and parses and
+// validates the provided port list.
+func parseEncryptionPolicy(resourceKey resource.Key, spec iso_v1alpha1.ClusterwideEncryptionPolicySpec) ([]parsedSelectorRule, error) {
+	switch {
+	case spec.NamespaceSelector == nil:
+		return nil, fmt.Errorf("missing namespaceSelector in resource %q", resourceKey.String())
+	case len(spec.Peers) == 0:
+		return nil, fmt.Errorf("missing peers in resource %q", resourceKey.String())
+	}
+
+	tuples := make([]parsedSelectorRule, 0, len(spec.Peers))
+	subjectEndpointSelector := parseSelector(resourceKey.Namespace, spec.NamespaceSelector, spec.PodSelector)
+	for idx, peer := range spec.Peers {
+		switch {
+		case peer.NamespaceSelector == nil:
+			return nil, fmt.Errorf("missing namespaceSelector for peer %d in resource %q", idx, resourceKey.String())
+		case len(peer.Ports) == 0:
+			return nil, fmt.Errorf("missing ports for peer %d in resource %q", idx, resourceKey.String())
+		}
+
+		peerEndpointSelector := parseSelector(resourceKey.Namespace, peer.NamespaceSelector, peer.PodSelector)
+		peerPorts, err := parsePeerPorts(peer.Ports)
+		if err != nil {
+			return nil, fmt.Errorf("invalid port for peer %d in resource %q: %w", idx, resourceKey.String(), err)
+		}
+
+		tuples = append(tuples, parsedSelectorRule{
+			subject:   subjectEndpointSelector,
+			peer:      peerEndpointSelector,
+			peerPorts: peerPorts,
+		})
+	}
+
+	return tuples, nil
+}
---
+++ enterprise/pkg/encryption/policy/parse_test.go
@@ -0,0 +1,427 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package policy
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
+	iso_v1alpha1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/resource"
+	slim_metav1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
+	"github.com/cilium/cilium/pkg/labels"
+	"github.com/cilium/cilium/pkg/policy/api"
+	"github.com/cilium/cilium/pkg/u8proto"
+)
+
+func Test_parsePeerPorts(t *testing.T) {
+	type args struct {
+		ports []iso_v1alpha1.PortProtocol
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []portProto
+		wantErr bool
+	}{
+		{
+			name: "regular port list",
+			args: args{
+				ports: []iso_v1alpha1.PortProtocol{
+					{
+						Port:     8080,
+						Protocol: "TCP",
+					},
+					{
+						Port:     53,
+						Protocol: "UDP",
+					},
+				},
+			},
+			want: []portProto{
+				{
+					port:  8080,
+					proto: u8proto.TCP,
+				},
+				{
+					port:  53,
+					proto: u8proto.UDP,
+				},
+			},
+		},
+		{
+			name: "invalid protocol",
+			args: args{
+				ports: []iso_v1alpha1.PortProtocol{
+					{
+						Port:     8080,
+						Protocol: "HTTP",
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid port",
+			args: args{
+				ports: []iso_v1alpha1.PortProtocol{
+					{
+						Port:     0,
+						Protocol: "UDP",
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "ANY protocol",
+			args: args{
+				ports: []iso_v1alpha1.PortProtocol{
+					{
+						Port:     8080,
+						Protocol: "ANY",
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parsePeerPorts(tt.args.ports)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_parseSelector(t *testing.T) {
+	podSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"foo": "bar",
+		},
+		MatchExpressions: []slim_metav1.LabelSelectorRequirement{
+			{
+				Key:      "baz",
+				Operator: "In",
+				Values:   []string{"a", "b"},
+			},
+		},
+	}
+
+	namespaceSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"baz": "qux",
+		},
+		MatchExpressions: []slim_metav1.LabelSelectorRequirement{
+			{
+				Key:      "bat",
+				Operator: "In",
+				Values:   []string{"e", "f"},
+			},
+		},
+	}
+
+	translatedNamespaceSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"io.cilium.k8s.namespace.labels.baz": "qux",
+		},
+		MatchExpressions: []slim_metav1.LabelSelectorRequirement{
+			{
+				Key:      "io.cilium.k8s.namespace.labels.bat",
+				Operator: "In",
+				Values:   []string{"e", "f"},
+			},
+		},
+	}
+
+	type args struct {
+		namespace         string
+		podSelector       *slim_metav1.LabelSelector
+		namespaceSelector *slim_metav1.LabelSelector
+	}
+	tests := []struct {
+		name string
+		args args
+		want api.EndpointSelector
+	}{
+		{
+			name: "empty selector without namespace",
+			args: args{
+				namespace: "",
+			},
+			want: api.WildcardEndpointSelector,
+		},
+		{
+			name: "pod selector without namespace",
+			args: args{
+				namespace:   "",
+				podSelector: podSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, podSelector),
+		},
+		{
+			name: "namespace selector without namespace",
+			args: args{
+				namespace:         "",
+				namespaceSelector: namespaceSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, translatedNamespaceSelector),
+		},
+		{
+			name: "pod and namespace selector without namespace",
+			args: args{
+				namespace:         "",
+				podSelector:       podSelector,
+				namespaceSelector: namespaceSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, translatedNamespaceSelector, podSelector),
+		},
+		{
+			name: "empty selector with namespace",
+			args: args{
+				namespace: "test-namespace",
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, &slim_metav1.LabelSelector{
+				MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+					k8sConst.PodNamespaceLabel: "test-namespace",
+				},
+			}),
+		},
+		{
+			name: "empty pod selector with namespace",
+			args: args{
+				namespace:   "test-namespace",
+				podSelector: &slim_metav1.LabelSelector{},
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, &slim_metav1.LabelSelector{
+				MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+					k8sConst.PodNamespaceLabel: "test-namespace",
+				},
+			}),
+		},
+		{
+			name: "pod selector with namespace",
+			args: args{
+				namespace:   "test-namespace",
+				podSelector: podSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, podSelector, &slim_metav1.LabelSelector{
+				MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+					k8sConst.PodNamespaceLabel: "test-namespace",
+				},
+			}),
+		},
+		{
+			name: "namespace selector with namespace",
+			args: args{
+				namespace:         "test-namespace",
+				namespaceSelector: namespaceSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, translatedNamespaceSelector),
+		},
+		{
+			name: "pod and namespace selector with namespace",
+			args: args{
+				namespace:         "test-namespace",
+				podSelector:       podSelector,
+				namespaceSelector: namespaceSelector,
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, translatedNamespaceSelector, podSelector),
+		},
+		{
+			name: "pod selector and empty namespace selector with namespace",
+			args: args{
+				namespace:         "test-namespace",
+				podSelector:       podSelector,
+				namespaceSelector: &slim_metav1.LabelSelector{},
+			},
+			want: api.NewESFromK8sLabelSelector(labels.LabelSourceK8sKeyPrefix, &slim_metav1.LabelSelector{
+				MatchExpressions: []slim_metav1.LabelSelectorRequirement{
+					allowAllNamespacesRequirement,
+				},
+			}, podSelector),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, parseSelector(tt.args.namespace, tt.args.namespaceSelector, tt.args.podSelector))
+		})
+	}
+}
+
+func Test_parseEncryptionPolicy(t *testing.T) {
+	type args struct {
+		resourceKey resource.Key
+		spec        iso_v1alpha1.ClusterwideEncryptionPolicySpec
+	}
+	subjectNamespaceSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"kubernetes.io/metadata.name": "subject-namespace",
+		},
+	}
+	subjectPodSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"app": "subject-pod",
+		},
+	}
+	peer1NamespaceSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"kubernetes.io/metadata.name": "peer1-namespace",
+		},
+	}
+	peer1PodSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"app": "peer1-pod",
+		},
+	}
+	peer2NamespaceSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"kubernetes.io/metadata.name": "peer2-namespace",
+		},
+	}
+	peer2PodSelector := &slim_metav1.LabelSelector{
+		MatchLabels: map[string]slim_metav1.MatchLabelsValue{
+			"app": "peer2-pod",
+		},
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []parsedSelectorRule
+		wantErr bool
+	}{
+		{
+			name: "regular policy with two rules",
+			args: args{
+				resourceKey: resource.Key{
+					Name: "encrypt-foo",
+				},
+				spec: iso_v1alpha1.ClusterwideEncryptionPolicySpec{
+					NamespaceSelector: subjectNamespaceSelector,
+					PodSelector:       subjectPodSelector,
+					Peers: []iso_v1alpha1.ClusterwideEncryptionPeerSelector{
+						{
+							NamespaceSelector: peer1NamespaceSelector,
+							PodSelector:       peer1PodSelector,
+							Ports: []iso_v1alpha1.PortProtocol{
+								{
+									Port:     8080,
+									Protocol: "TCP",
+								},
+							},
+						},
+						{
+							NamespaceSelector: peer2NamespaceSelector,
+							PodSelector:       peer2PodSelector,
+							Ports: []iso_v1alpha1.PortProtocol{
+								{
+									Port:     53,
+									Protocol: "UDP",
+								},
+							},
+						},
+					},
+				},
+			},
+			want: []parsedSelectorRule{
+				{
+					subject: parseSelector("", subjectNamespaceSelector, subjectPodSelector),
+					peer:    parseSelector("", peer1NamespaceSelector, peer1PodSelector),
+					peerPorts: []portProto{
+						{
+							port:  8080,
+							proto: u8proto.TCP,
+						},
+					},
+				},
+				{
+					subject: parseSelector("", subjectNamespaceSelector, subjectPodSelector),
+					peer:    parseSelector("", peer2NamespaceSelector, peer2PodSelector),
+					peerPorts: []portProto{
+						{
+							port:  53,
+							proto: u8proto.UDP,
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "missing subject namespace selector",
+			args: args{
+				resourceKey: resource.Key{
+					Name: "encrypt-foo",
+				},
+				spec: iso_v1alpha1.ClusterwideEncryptionPolicySpec{
+					NamespaceSelector: nil,
+					PodSelector:       subjectPodSelector,
+					Peers: []iso_v1alpha1.ClusterwideEncryptionPeerSelector{
+						{
+							NamespaceSelector: peer1NamespaceSelector,
+							PodSelector:       peer1PodSelector,
+							Ports: []iso_v1alpha1.PortProtocol{
+								{
+									Port:     8080,
+									Protocol: "TCP",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "missing peer namespace selector",
+			args: args{
+				resourceKey: resource.Key{
+					Name: "encrypt-foo",
+				},
+				spec: iso_v1alpha1.ClusterwideEncryptionPolicySpec{
+					NamespaceSelector: subjectNamespaceSelector,
+					PodSelector:       subjectPodSelector,
+					Peers: []iso_v1alpha1.ClusterwideEncryptionPeerSelector{
+						{
+							NamespaceSelector: nil,
+							PodSelector:       peer1PodSelector,
+							Ports: []iso_v1alpha1.PortProtocol{
+								{
+									Port:     8080,
+									Protocol: "TCP",
+								},
+							},
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseEncryptionPolicy(tt.args.resourceKey, tt.args.spec)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
