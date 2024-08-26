78b50bc60bac0e9203bc4767f2f3276cc6f7a18c
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-08-26 15:41:33 +0200

    lb: example control script: support T2 hc control via unix socket
    Currently, the example control script is only able to control the T2
    health check status if the admin endpoint is exposed via dedicated
    admin listener.
    
    With the existing dev setup, we don't configure this admin endpoint.
    Therefore, this commit changes the script to access the Envoy admin
    interface via UNIX domain socket.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- examples/kubernetes/loadbalancer/example_control.sh
+++ examples/kubernetes/loadbalancer/example_control.sh
@@ -16,35 +16,36 @@
 set -e
 
 if [ -z ${1+x} ]; then
-	echo "no instance type set [t2-lb|backend]"
-	exit 1
+  echo "no instance type set [t2-lb|backend]"
+  exit 1
 fi
 
 if [ -z ${2+x} ]; then
-	echo "no instance name set [<cilium-node-name>|<backend-container-name>]"
-	exit 1
+  echo "no instance name set [<cilium-node-name>|<backend-container-name>]"
+  exit 1
 fi
 
 if [ -z ${3+x} ]; then
-	echo "no type set [healthcheck|response]"
-	exit 1
+  echo "no type set [healthcheck|response]"
+  exit 1
 fi
 
 if [ -z ${4+x} ]; then
-	echo "no status set [ok|fail]"
-	exit 1
+  echo "no status set [ok|fail]"
+  exit 1
 fi
 
 if [ "$1" = "backend" ]; then
-	docker exec "$2" curl -s -X POST "http://localhost:8080/control/$3/$4"
+  docker exec "$2" curl -s -X POST "http://localhost:8080/control/$3/$4"
 
 elif [ "$1" = "t2-lb" ]; then
-	NODE_IP=$(kubectl get nodes "$2" -oyaml | yq '.status.addresses[] | select(.type == "InternalIP").address | select(. == "*.*.*.*")')
+  if [ "$3" = "response" ]; then
+    echo "controlling response type currently not possible for t2-lb"
+    exit 1
+  fi
 
-	if [ "$3" = "response" ]; then
-		echo "controlling response type currently not possible for t2-lb"
-		exit 1
-	fi
-
-	curl -X POST "http://$NODE_IP:10000/$3/$4"
+  agentPod=$(kubectl -n kube-system get pods --field-selector spec.nodeName="${2}" -l k8s-app=cilium -oyaml | yq '.items[0].metadata.name')
+  kubectl -n kube-system exec "${agentPod}" -c cilium-agent -- apt-get update -qq
+  kubectl -n kube-system exec "${agentPod}" -c cilium-agent -- apt-get install -y -qq curl
+  kubectl -n kube-system exec "${agentPod}" -c cilium-agent -- curl -X POST -s --unix-socket /var/run/cilium/envoy/sockets/admin.sock http:/admin/"$3"/"$4"
 fi
