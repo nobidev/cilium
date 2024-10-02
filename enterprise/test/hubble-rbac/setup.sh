6caf9d54ec0da316ad6f94746265118d1f9e4d51
Chance Zibolski <chance.zibolski@gmail.com>
2024-10-02 10:53:04 -0700

    .github: Improve Hubble RBAC test output
    - Add set -x to enterprise/test/hubble-rbac/setup.sh to simplify
    diagnostics.
    - Add some echos statements to explain each stage of setup.sh
    - Add a timeout to the setup step and remove the if: ${{ always() }}.
    
    Signed-off-by: Chance Zibolski <chance.zibolski@gmail.com>

--- enterprise/test/hubble-rbac/setup.sh
+++ enterprise/test/hubble-rbac/setup.sh
@@ -3,12 +3,16 @@ set -euo pipefail
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+set -x
+
+echo "Installing Cilium"
 helm upgrade --install cilium "$CILIUM_CHART_REPO" \
   --wait \
   --namespace kube-system \
   --version "$CILIUM_VERSION" \
   --values "$SCRIPT_DIR"/helm/cilium.yaml
 
+echo "Installing ingress-nginx"
 # install ingress
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
@@ -27,15 +31,20 @@ echo "Requesting sudo to update /etc/hosts and point 'dex.text' to the KinD IP"
 sudo sed -i -n -e '/^.*\ dex.test/!p' -e '$a\'"${KIND_CTRLPLANE_IP}"' dex.test' /etc/hosts
 helm repo add dex https://charts.dexidp.io
 helm repo update
+
+echo "Installing Dex"
 helm upgrade --install dex dex/dex \
   --wait \
   --namespace dex \
   --create-namespace \
   --version "$DEX_VERSION" \
   --values "$SCRIPT_DIR"/helm/dex.yaml
+
+echo "Checking dex ingress"
 until [ "$(kubectl -n dex get ingress dex -ojson | jq '.status.loadBalancer.ingress[]?.hostname' -r)" != "" ]; do
   echo "waiting for Dex ingress to become ready"
   sleep 5
 done
 
+echo "Waiting for Hubble Relay to become ready"
 kubectl -n kube-system rollout status deployment hubble-relay
