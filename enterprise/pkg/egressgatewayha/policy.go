nobidev/cilium

commit 28860f6cd7f27727af476f95e05c3853d06eef74

egressgwha: Configure egress IP and route in the gateway node
Add support for reconciling the egress gateway network configuration as
per IEGP Egress Group Status section.

When the operator assigns an Egress IP to a gateway node, the agent:

- add the egress IP to the requested egress interface, or the default
  one if left empty
- add a rule to make the traffic with a source IP address equal to the
  egress IP use the routing table reserved for egress gateway IPAM
  (source-address policy based routing)
- add a route into the egress gateway IPAM routing table so that the
  datapath is able to send SNATed traffic to the selected egress
  interface with a BPF fib_lookup

After receiving an egw policy update, the agent updates the `egress-ips`
statedb table, and in turn a statedb reconciler updates the network
configuration as needed (IP, rule and route), based on the policy event
type (insert, update or delete). The reconciler guarantees that the
operation is retried in case of a failure. Also, if (all or part of) the
required configuration is already in place (e.g: the route or the rule
have alrady been created) the reconciler acknowledge it without making
any unnecessary change.

The usage of a separate routing table allows the egress-gateway IPAM
reconciler to avoid tinkering with the rest of the node network
configuration, including the routes not installed by Cilium. At the same
time, the reconciler collects all the stale egress IPs and routes
whenever an IEGP gets deleted.

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

enterprise/pkg/egressgatewayha/policy.go
@@ -21,16 +21,21 @@ import (
 	"slices"
 	"sort"
 
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/reconciler"
 	"github.com/google/go-cmp/cmp"
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 	"go4.org/netipx"
 	"golang.org/x/exp/maps"
 	core_v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 
+	"github.com/cilium/cilium/enterprise/datapath/tables"
 	"github.com/cilium/cilium/pkg/datapath/linux/netdevice"
 	"github.com/cilium/cilium/pkg/datapath/linux/route"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
@@ -194,6 +199,11 @@ type PolicyConfig struct {
 
 // PolicyID includes policy name and namespace
 type policyID = types.NamespacedName
+
+type gwEgressIPConfig struct {
+	addr  netip.Addr
+	iface string
+}
 
 // matchesEndpointLabels determines if the given endpoint is a match for the
 // policy config based on matching labels.
@@ -784,6 +794,8 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 		return
 	}
 
+	var egressIPs []gwEgressIPConfig
+
 	gwc := &config.gatewayConfig
 	for groupIndex, gc := range config.groupConfigs {
 		groupStatus := &config.groupStatuses[groupIndex]
@@ -823,30 +835,81 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 			}
 		}
 
+		logger := log.WithFields(logrus.Fields{
+			logfields.IsovalentEgressGatewayPolicyName: config.id,
+			logfields.Interface:                        gc.iface,
+			logfields.EgressIP:                         gc.egressIP,
+		})
+
 		if localNodeMatchesGatewayIP {
-			if err := gwc.deriveFromGroupConfig(&gc); err != nil {
-				log.WithFields(logrus.Fields{
-					logfields.IsovalentEgressGatewayPolicyName: config.id,
-					logfields.Interface:                        gc.iface,
-					logfields.EgressIP:                         gc.egressIP,
-				}).WithError(err).Error("Failed to derive policy gateway configuration")
+			// If localNodeConfiguredAsGateway is already set it means that another
+			// egress group for the same policy has already selected it as gateway. In
+			// this case don't regenerate a new gatewayConfig and return an error
+			if gwc.localNodeConfiguredAsGateway {
+				logger.WithError(err).Error("Local node selected by multiple egress gateway groups from the same policy")
 			}
+
+			if egressIP, found := groupStatus.egressIPByGatewayIP[localNodeK8sAddr]; found {
+				var (
+					iface netlink.Link
+					err   error
+				)
+				if gc.iface != "" {
+					iface, err = netlink.LinkByName(gc.iface)
+				} else {
+					iface, err = route.NodeDeviceWithDefaultRoute(true, false)
+				}
+				if err != nil {
+					logger.WithError(err).Error("Failed to find interface while updating node egress IP config")
+					continue
+				}
+
+				egressIPs = append(egressIPs, gwEgressIPConfig{egressIP, iface.Attrs().Name})
+
+				gwc.ifaceName = iface.Attrs().Name
+				gwc.egressIP = egressIP
+			} else if err := gwc.deriveFromGroupConfig(&gc); err != nil {
+				logger.WithError(err).Error("Failed to derive policy gateway configuration")
+			}
+
+			gwc.localNodeConfiguredAsGateway = true
 		}
 	}
+
+	nextEgressIPs := sets.New(egressIPs...)
+	curEgressIPs := manager.egressConfigsByPolicy[config.id]
+	toAdd, toDel := nextEgressIPs.Difference(curEgressIPs), curEgressIPs.Difference(nextEgressIPs)
+	updateEgressIPsConfig(manager.db, manager.egressIPTable, toAdd, toDel)
+	manager.egressConfigsByPolicy[config.id] = nextEgressIPs
+
+}
+
+func updateEgressIPsConfig(db *statedb.DB, table statedb.RWTable[*tables.EgressIPEntry], toAdd, toDel sets.Set[gwEgressIPConfig]) {
+	txn := db.WriteTxn(table)
+	defer txn.Abort()
+
+	for _, config := range toDel.UnsortedList() {
+		table.Delete(txn, &tables.EgressIPEntry{
+			Addr:      config.addr,
+			Interface: config.iface,
+		})
+	}
+
+	for _, config := range toAdd.UnsortedList() {
+		table.Insert(txn, &tables.EgressIPEntry{
+			Addr:      config.addr,
+			Interface: config.iface,
+			Status:    reconciler.StatusPending(),
+		})
+	}
+
+	txn.Commit()
 }
 
 // deriveFromGroupConfig retrieves all the missing gateway configuration data
 // (such as egress IP or interface) given a policy group config
 func (gwc *gatewayConfig) deriveFromGroupConfig(gc *groupConfig) error {
-	// If localNodeConfiguredAsGateway is already set it means that another
-	// egress group for the same policy has already selected it as gateway. In
-	// this case don't regenerate a new gatewayConfig and return an error
-	if gwc.localNodeConfiguredAsGateway {
-		return fmt.Errorf("local node selected by multiple egress gateway groups from the same policy")
-	}
-
 	var err error
-	gwc.localNodeConfiguredAsGateway = false
 
 	switch {
 	case gc.iface != "":
@@ -882,8 +945,6 @@ func (gwc *gatewayConfig) deriveFromGroupConfig(gc *groupConfig) error {
 			return fmt.Errorf("failed to retrieve IPv4 address for egress interface: %w", err)
 		}
 	}
-
-	gwc.localNodeConfiguredAsGateway = true
 
 	return nil
 }

CODEOWNERS
@@ -768,6 +768,7 @@ Makefile* @cilium/build
 /enterprise/cilium-dbg/cmd/bpf_egress* @isovalent/egress-gateway
 /enterprise/cilium-dbg/cmd/*bfd_* @isovalent/traffic-engineering
 /enterprise/daemon/ @isovalent/cilium-agent
+/enterprise/datapath/tables/egressip.go @isovalent/egress-gateway
 /enterprise/featurelist/ @isovalent/cilium-agent
 /enterprise/fips/ @isovalent/cilium-agent
 /enterprise/fqdn-proxy/ @isovalent/dns-proxy

enterprise/datapath/tables/egressip.go
@@ -1,0 +1,92 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package tables
+
+import (
+	"net/netip"
+
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/index"
+	"github.com/cilium/statedb/reconciler"
+)
+
+// EgressIPTableName is the name of the stateDB table used to keep track of the
+// allocated egress IPs that needs to be configured on the gateway.
+const EgressIPTableName = "egress-ips"
+
+// EgressIPKey is an egress-ips stateDB table key
+type EgressIPKey struct {
+	Addr      netip.Addr
+	Interface string
+}
+
+// Key extracts the key from an EgressIPKey as an index.Key type
+func (e EgressIPKey) Key() index.Key {
+	key := append(index.NetIPAddr(e.Addr), '+')
+	return append(key, index.String(e.Interface)...)
+}
+
+// EgressIPEntryIndex is the primary index for the egress-ips table
+var EgressIPEntryIndex = statedb.Index[*EgressIPEntry, EgressIPKey]{
+	Name: "id",
+	FromObject: func(e *EgressIPEntry) index.KeySet {
+		return index.NewKeySet(EgressIPKey{e.Addr, e.Interface}.Key())
+	},
+	FromKey: EgressIPKey.Key,
+	Unique:  true,
+}
+
+// NewEgressIPTable creates a new instance of the "egress-ips" stateDB table
+func NewEgressIPTable(db *statedb.DB) (statedb.RWTable[*EgressIPEntry], error) {
+	tbl, err := statedb.NewTable(
+		EgressIPTableName,
+		EgressIPEntryIndex,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return tbl, db.RegisterTable(tbl)
+}
+
+// TableHeader returns the names of the "egress-ips" table columns
+func (e *EgressIPEntry) TableHeader() []string {
+	return []string{"Address", "Interface", "Status"}
+}
+
+// TableRow returns the values of an "egress-ips" table entry
+func (e *EgressIPEntry) TableRow() []string {
+	return []string{e.Addr.String(), e.Interface, e.Status.String()}
+}
+
+// EgressIPEntry describes a single row of the "egress-ips" stateDB table
+type EgressIPEntry struct {
+	Addr      netip.Addr
+	Interface string
+
+	Status reconciler.Status
+}
+
+// GetStatus is a getter for the entry status
+func (e *EgressIPEntry) GetStatus() reconciler.Status {
+	return e.Status
+}
+
+// SetStatus is a setter for the entry status
+func (e *EgressIPEntry) SetStatus(newStatus reconciler.Status) *EgressIPEntry {
+	e.Status = newStatus
+	return e
+}
+
+// Clone returns a shallow copy of the entry
+func (e *EgressIPEntry) Clone() *EgressIPEntry {
+	e2 := *e
+	return &e2
+}

enterprise/pkg/egressgatewayha/egressipconf/cell.go
@@ -1,0 +1,43 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package egressipconf
+
+import (
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/reconciler"
+
+	"github.com/cilium/cilium/enterprise/datapath/tables"
+)
+
+// Cell manages the configuration of Egress IPs and associated routes
+// on behalf of the IsovalentEgressGatewayPolicies.
+var Cell = cell.Group(
+	cell.ProvidePrivate(tables.NewEgressIPTable),
+	cell.Invoke(
+		registerReconciler,
+	),
+)
+
+func registerReconciler(params reconciler.Params, table statedb.RWTable[*tables.EgressIPEntry]) error {
+	_, err := reconciler.Register(
+		params,
+		table,
+		(*tables.EgressIPEntry).Clone,
+		(*tables.EgressIPEntry).SetStatus,
+		(*tables.EgressIPEntry).GetStatus,
+		newOps(),
+		nil,
+
+		reconciler.WithoutPruning(),
+	)
+	return err
+}

enterprise/pkg/egressgatewayha/egressipconf/ops_linux.go
@@ -1,0 +1,124 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+//go:build linux
+
+package egressipconf
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/netip"
+	"os"
+	"syscall"
+
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/reconciler"
+	"github.com/vishvananda/netlink"
+	"go4.org/netipx"
+	"golang.org/x/sys/unix"
+
+	"github.com/cilium/cilium/enterprise/datapath/tables"
+	"github.com/cilium/cilium/pkg/datapath/linux/linux_defaults"
+	"github.com/cilium/cilium/pkg/datapath/linux/route"
+	"github.com/cilium/cilium/pkg/defaults"
+)
+
+const (
+	// RouteTableEgressGatewayIPAM is the default table ID to use for routing rules related to Egress Gateway IPAM.
+	RouteTableEgressGatewayIPAM = 2050
+
+	// RulePriorityEgressGatewayIPAM is the priority of the rule installed by Egress Gateway IPAM to route
+	// SNATed traffic to the proper egress interface.
+	RulePriorityEgressGatewayIPAM = 30
+)
+
+func (ops *ops) Update(ctx context.Context, _ statedb.ReadTxn, entry *tables.EgressIPEntry) error {
+	if !entry.Addr.IsValid() {
+		return fmt.Errorf("egress IP %s is not valid", entry.Addr)
+	}
+
+	iface, err := netlink.LinkByName(entry.Interface)
+	if err != nil {
+		return fmt.Errorf("failed to get device %s by name: %w", entry.Interface, err)
+	}
+
+	if err := netlink.AddrAdd(iface, addrForEgressIP(entry.Addr)); err != nil && !errors.Is(err, os.ErrExist) {
+		return fmt.Errorf("failed to add egress IP %s to interface %s", entry.Addr, iface.Attrs().Name)
+	}
+
+	if err := route.ReplaceRule(ruleForEgressIP(entry.Addr)); err != nil {
+		return fmt.Errorf("failed to upsert rule for address %s: %w", entry.Addr, err)
+	}
+
+	if err := route.Upsert(routeForEgressIP(entry.Addr, iface)); err != nil {
+		return fmt.Errorf("failed to append route for egress IP %s and interface %s", entry.Addr, iface.Attrs().Name)
+	}
+
+	return nil
+}
+
+func (ops *ops) Delete(ctx context.Context, _ statedb.ReadTxn, entry *tables.EgressIPEntry) error {
+	iface, err := netlink.LinkByName(entry.Interface)
+	if err != nil {
+		return fmt.Errorf("failed to get device %s by name: %w", entry.Interface, err)
+	}
+
+	if err := netlink.AddrDel(iface, addrForEgressIP(entry.Addr)); err != nil && !errors.Is(err, unix.EADDRNOTAVAIL) {
+		return fmt.Errorf("failed to delete egress IP %s to interface %s", entry.Addr, iface.Attrs().Name)
+	}
+
+	if err := route.DeleteRule(netlink.FAMILY_V4, ruleForEgressIP(entry.Addr)); err != nil && !errors.Is(err, os.ErrNotExist) {
+		return fmt.Errorf("failed to upsert rule for address %s: %w", entry.Addr, err)
+	}
+
+	if err := route.DeleteV4(routeForEgressIP(entry.Addr, iface)); err != nil && !errors.Is(err, syscall.ESRCH) {
+		return fmt.Errorf("failed to delete route for egress IP %s and interface %s", entry.Addr, iface.Attrs().Name)
+	}
+
+	return nil
+}
+
+func (ops *ops) Prune(ctx context.Context, _ statedb.ReadTxn, iter statedb.Iterator[*tables.EgressIPEntry]) error {
+	// addresses not in the table will never be pruned
+	return nil
+}
+
+func newOps() *ops {
+	return &ops{}
+}
+
+type ops struct{}
+
+var _ reconciler.Operations[*tables.EgressIPEntry] = &ops{}
+
+func addrForEgressIP(addr netip.Addr) *netlink.Addr {
+	return &netlink.Addr{IPNet: netipx.AddrIPNet(addr)}
+}
+
+func ruleForEgressIP(addr netip.Addr) route.Rule {
+	return route.Rule{
+		Priority: RulePriorityEgressGatewayIPAM,
+		From:     netipx.AddrIPNet(addr),
+		Table:    RouteTableEgressGatewayIPAM,
+		Protocol: linux_defaults.RTProto,
+	}
+}
+
+func routeForEgressIP(addr netip.Addr, iface netlink.Link) route.Route {
+	return route.Route{
+		Prefix: defaults.IPv4DefaultRoute,
+		Local:  addr.AsSlice(),
+		Device: iface.Attrs().Name,
+		Table:  RouteTableEgressGatewayIPAM,
+		Proto:  linux_defaults.RTProto,
+	}
+}

enterprise/pkg/egressgatewayha/manager.go
@@ -21,12 +21,16 @@ import (
 	"sync/atomic"
 
 	"github.com/cilium/hive/cell"
+	"github.com/cilium/statedb"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/pflag"
 	"go4.org/netipx"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/util/workqueue"
 
+	enterprise_tables "github.com/cilium/cilium/enterprise/datapath/tables"
+	"github.com/cilium/cilium/enterprise/pkg/egressgatewayha/egressipconf"
 	"github.com/cilium/cilium/enterprise/pkg/maps/egressmapha"
 	"github.com/cilium/cilium/pkg/bgpv1/agent/signaler"
 	"github.com/cilium/cilium/pkg/datapath/linux/config/defines"
@@ -69,6 +73,9 @@ var (
 var Cell = cell.Module(
 	"egressgatewayha",
 	"Egress Gateway allows originating traffic from specific IPv4 addresses",
+
+	egressipconf.Cell,
+
 	cell.Config(defaultConfig),
 	cell.Provide(NewEgressGatewayManager),
 )
@@ -177,6 +184,17 @@ type Manager struct {
 
 	// bgpSignaler is used to signal reconciliation events to the BGP Control Plane
 	bgpSignaler *signaler.BGPCPSignaler
+
+	// egressConfigsByPolicy stores all the configurations (addr and iface) for IPAM
+	// allocations entitled to the local node, as reported in each Egress Group Status
+	// of the IEGPs.
+	// The key of the map is the policy reporting the address allocation, the value is
+	// the set of configuration pairs <egressIP, net_inteface> for that policy.
+	egressConfigsByPolicy map[policyID]sets.Set[gwEgressIPConfig]
+
+	egressIPTable statedb.RWTable[*enterprise_tables.EgressIPEntry]
+
+	db *statedb.DB
 }
 
 type Params struct {
@@ -193,6 +211,9 @@ type Params struct {
 	LocalNodeStore    *node.LocalNodeStore
 	Sysctl            sysctl.Sysctl
 	BGPSignaler       *signaler.BGPCPSignaler
+
+	DB            *statedb.DB
+	EgressIPTable statedb.RWTable[*enterprise_tables.EgressIPEntry]
 
 	Lifecycle cell.Lifecycle
 }
@@ -249,6 +270,7 @@ func newEgressGatewayManager(p Params) (*Manager, error) {
 		nodeDataStore:                 make(map[string]nodeTypes.Node),
 		policyConfigs:                 make(map[policyID]*PolicyConfig),
 		policyConfigsBySourceIP:       make(map[string][]*PolicyConfig),
+		egressConfigsByPolicy:         make(map[policyID]sets.Set[gwEgressIPConfig]),
 		epDataStore:                   make(map[endpointID]*endpointMetadata),
 		identityAllocator:             p.IdentityAllocator,
 		reconciliationTriggerInterval: p.Config.EgressGatewayHAReconciliationTriggerInterval,
@@ -260,6 +282,8 @@ func newEgressGatewayManager(p Params) (*Manager, error) {
 		localNodeStore:                p.LocalNodeStore,
 		sysctl:                        p.Sysctl,
 		bgpSignaler:                   p.BGPSignaler,
+		db:                            p.DB,
+		egressIPTable:                 p.EgressIPTable,
 	}
 
 	t, err := trigger.NewTrigger(trigger.Parameters{
@@ -628,6 +652,42 @@ func (manager *Manager) policyMatches(sourceIP netip.Addr, f func(*endpointMetad
 	return false
 }
 
+func (manager *Manager) removeStaleEgressIPConfigs() {
+	for policyID := range manager.egressConfigsByPolicy {
+		if _, found := manager.policyConfigs[policyID]; found {
+			continue
+		}
+
+		// policy has been removed, so remove egress IPs and routes too
+		manager.removePolicyEgressIPs(manager.egressConfigsByPolicy[policyID])
+		delete(manager.egressConfigsByPolicy, policyID)
+	}
+}
+
+func (manager *Manager) removePolicyEgressIPs(egressIPs sets.Set[gwEgressIPConfig]) {
+	txn := manager.db.WriteTxn(manager.egressIPTable)
+	defer txn.Abort()
+
+	for _, egressIP := range egressIPs.UnsortedList() {
+		key := enterprise_tables.EgressIPKey{
+			Addr:      egressIP.addr,
+			Interface: egressIP.iface,
+		}
+		obj, _, found := manager.egressIPTable.Get(txn, enterprise_tables.EgressIPEntryIndex.Query(key))
+		if !found {
+			continue
+		}
+		if _, _, err := manager.egressIPTable.Delete(txn, obj); err != nil {
+			log.WithFields(logrus.Fields{
+				logfields.EgressIP:  egressIP.addr,
+				logfields.Interface: egressIP.iface,
+			}).WithError(err).Error("Failed to delete entry from egress-ips stateDB table")
+		}
+	}
+
+	txn.Commit()
+}
+
 func (manager *Manager) regenerateGatewayConfigs() {
 	for _, policyConfig := range manager.policyConfigs {
 		policyConfig.regenerateGatewayConfig(manager)
@@ -834,6 +894,8 @@ func (manager *Manager) reconcileLocked() {
 			manager.updateNodesByIP()
 		}
 	}
+
+	manager.removeStaleEgressIPConfigs()
 
 	if manager.eventBitmapIsSet(eventK8sSyncDone, eventAddPolicy, eventDeletePolicy, eventUpdateNode, eventDeleteNode) {
 		manager.regenerateGatewayConfigs()

enterprise/pkg/egressgatewayha/manager_privileged_test.go
@@ -4,6 +4,7 @@
 package egressgatewayha
 
 import (
+	"context"
 	"fmt"
 	"net"
 	"net/netip"
@@ -11,16 +12,20 @@ import (
 	"time"
 
 	"github.com/cilium/ebpf/rlimit"
+	"github.com/cilium/hive/cell"
 	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/statedb"
 	"github.com/spf13/afero"
 	"github.com/stretchr/testify/require"
 	"github.com/vishvananda/netlink"
 	"k8s.io/apimachinery/pkg/types"
 
+	"github.com/cilium/cilium/enterprise/datapath/tables"
 	"github.com/cilium/cilium/enterprise/pkg/maps/egressmapha"
 	"github.com/cilium/cilium/pkg/bgpv1/agent/signaler"
 	"github.com/cilium/cilium/pkg/bpf"
 	"github.com/cilium/cilium/pkg/datapath/linux/sysctl"
+	"github.com/cilium/cilium/pkg/hive"
 	"github.com/cilium/cilium/pkg/identity"
 	cilium_api_v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
 	slimv1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/apis/meta/v1"
@@ -70,8 +75,7 @@ func setupEgressGatewayTestSuite(t *testing.T) *EgressGatewayTestSuite {
 		},
 	})
 
-	var err error
-	k.manager, err = newEgressGatewayManager(Params{
+	manager, err := newEgressGatewayManager(Params{
 		Lifecycle:         lc,
 		Config:            Config{2 * time.Second, 1 * time.Millisecond},
 		DaemonConfig:      &option.DaemonConfig{},
@@ -86,7 +90,28 @@ func setupEgressGatewayTestSuite(t *testing.T) *EgressGatewayTestSuite {
 		Sysctl:            k.sysctl,
 	})
 	require.NoError(t, err)
-	require.NotNil(t, k.manager)
+	require.NotNil(t, manager)
+
+	k.manager = manager
+
+	// create a hive to provide statedb and egress-ips table
+	h := hive.New(
+		cell.Provide(
+			tables.NewEgressIPTable,
+		),
+
+		cell.Invoke(func(db *statedb.DB, table statedb.RWTable[*tables.EgressIPEntry]) {
+			k.manager.db = db
+			k.manager.egressIPTable = table
+		}),
+	)
+
+	tlog := hivetest.Logger(t)
+	require.NoError(t, h.Start(tlog, context.TODO()))
+
+	t.Cleanup(func() {
+		require.NoError(t, h.Stop(tlog, context.TODO()))
+	})
 
 	k.reconciliationEventsCount = k.manager.reconciliationEventsCount.Load()
 

commit ec929ea5d4e6a1d6bc4e9a865656df9c45dedad2

egressgwha: Break early if local node is selected by multiple groups
While adding the egress gateway IPAM feature, the gateway config
regeneration logic was inadvertently changed and missed the early break
in case the local node was already selected as an egress gateway. Fix
this to be sure that neither address nor interface are set in that case.

Fixes: 28860f6cd7 ("egressgwha: Configure egress IP and route in the gateway node")
Reported-by: Yusuke Suzuki <yusuke.suzuki@isovalent.com>
Reported-by: Simone Magnani <simone.magnani@isovalent.com>
Reported-by: Julian Wiedmann <jwi@isovalent.com>

Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

enterprise/pkg/egressgatewayha/policy.go
@@ -854,6 +854,7 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 			// this case don't regenerate a new gatewayConfig and return an error
 			if gwc.localNodeConfiguredAsGateway {
 				logger.WithError(err).Error("Local node selected by multiple egress gateway groups from the same policy")
+				continue
 			}
 
 			if egressIP, found := groupStatus.egressIPByGatewayIP[localNodeK8sAddr]; found {
@@ -877,6 +878,7 @@ func (config *PolicyConfig) regenerateGatewayConfig(manager *Manager) {
 				gwc.egressIP = egressIP
 			} else if err := gwc.deriveFromGroupConfig(&gc); err != nil {
 				logger.WithError(err).Error("Failed to derive policy gateway configuration")
+				continue
 			}
 
 			gwc.localNodeConfiguredAsGateway = true