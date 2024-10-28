3334e95f565a27135040c623e859c544e626ee7b
Marco Hofstetter <marco.hofstetter@isovalent.com>
2024-10-28 11:51:44 +0100

    lb: add e2e tests for HTTPS request filtering
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

---
+++ enterprise/test/ilb/lb_https_requestfiltering_test.go
@@ -0,0 +1,292 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package ilb
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestHTTPSRequestFiltering(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		appOpt    func(clients []*frrContainer) httpApplicationRouteOption
+		testCalls []testCall
+	}{
+		{
+			desc: "deny-by-exact-path",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyByExactPath("/admin")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/admin/users",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/admin",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "deny-by-prefix-path",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyByPrefixPath("/admin")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/path",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/admin",
+					blocked:  true,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/admin/users",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "deny-by-exact-hostname",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyByExactHostname("secure2.acme.io")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "admin.secure2.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure2.acme.io",
+					path:     "/",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "deny-by-suffix-hostname",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyBySuffixHostname("moresecure.acme.io")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "moresecure.acme.io",
+					path:     "/",
+					blocked:  true,
+				},
+				{
+					clientNr: 0,
+					hostName: "admin.moresecure.acme.io",
+					path:     "/",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "deny-by-sourceip",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyBySourceIP(clients[1].ip + "/32")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "deny-by-sourceip-hostname-path",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringDenyBySourceIPExactHostnameExactPath(clients[1].ip+"/32", "secure2.acme.io", "/admin")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure2.acme.io",
+					path:     "/",
+					blocked:  false,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure.acme.io",
+					path:     "/admin",
+					blocked:  false,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure2.acme.io",
+					path:     "/admin",
+					blocked:  false,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure2.acme.io",
+					path:     "/admin",
+					blocked:  true,
+				},
+			},
+		},
+		{
+			desc: "allow-by-sourceip-hostname-path",
+			appOpt: func(clients []*frrContainer) httpApplicationRouteOption {
+				return withHttpRequestFilteringAllowBySourceIPExactHostnameExactPath(clients[1].ip+"/32", "secure2.acme.io", "/admin")
+			},
+			testCalls: []testCall{
+				{
+					clientNr: 0,
+					hostName: "secure.acme.io",
+					path:     "/",
+					blocked:  true,
+				},
+				{
+					clientNr: 0,
+					hostName: "secure2.acme.io",
+					path:     "/admin",
+					blocked:  true,
+				},
+				{
+					clientNr: 1,
+					hostName: "secure2.acme.io",
+					path:     "/admin",
+					blocked:  false,
+				},
+			},
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			skipIfOnSingleNode(t, ">1 FRR clients are not supported")
+
+			ctx := context.Background()
+			testName := fmt.Sprintf("https-requestfiltering-%s", tC.desc)
+			testK8sNamespace := "default"
+
+			ciliumCli, k8sCli := newCiliumAndK8sCli(t)
+			dockerCli := newDockerCli(t)
+
+			// 0. Setup test scenario (backends, clients & LB resources)
+			scenario := newLBTestScenario(t, testName, testK8sNamespace, ciliumCli, k8sCli, dockerCli)
+
+			t.Log("Creating cert and secret...")
+			scenario.createLBServerCertificate(ctx, testName+"-secure", "secure.acme.io")
+			scenario.createLBServerCertificate(ctx, testName+"-adminsecure", "admin.secure.acme.io")
+			scenario.createLBServerCertificate(ctx, testName+"-moresecure", "moresecure.acme.io")
+			scenario.createLBServerCertificate(ctx, testName+"-adminmoresecure", "admin.moresecure.acme.io")
+			scenario.createLBServerCertificate(ctx, testName+"-secure2", "secure2.acme.io")
+			scenario.createLBServerCertificate(ctx, testName+"-adminsecure2", "admin.secure2.acme.io")
+
+			t.Log("Creating backend apps...")
+			scenario.addBackendApplications(ctx, 2, backendApplicationConfig{h2cEnabled: true})
+
+			t.Log("Creating clients and add BGP peering ...")
+			clients := scenario.addFRRClients(ctx, 2, frrClientConfig{trustedCertsHostnames: []string{"secure.acme.io", "admin.secure.acme.io", "moresecure.acme.io", "admin.moresecure.acme.io", "secure2.acme.io", "admin.secure2.acme.io"}})
+
+			t.Logf("Creating LB VIP resources...")
+			vip := lbVIP(testK8sNamespace, testName)
+			scenario.createLBVIP(ctx, vip)
+
+			t.Logf("Creating LB BackendPool resources...")
+			backends := []backendPoolOption{}
+			for _, b := range scenario.backendApps {
+				backends = append(backends, withIPBackend(b.ip, b.port))
+			}
+			backendPool := lbBackendPool(testK8sNamespace, testName, backends...)
+			scenario.createLBBackendPool(ctx, backendPool)
+
+			t.Logf("Creating LB Service resources...")
+			opts := []httpsApplicationOption{}
+			opts = append(opts, withHttpsRoute(testName, withHttpHostname("*.acme.io"), tC.appOpt(clients)))
+			opts = append(opts, withCertificate(testName+"-secure"))
+			opts = append(opts, withCertificate(testName+"-adminsecure"))
+			opts = append(opts, withCertificate(testName+"-moresecure"))
+			opts = append(opts, withCertificate(testName+"-adminmoresecure"))
+			opts = append(opts, withCertificate(testName+"-secure2"))
+			opts = append(opts, withCertificate(testName+"-adminsecure2"))
+			opts = append(opts, withCertificate(testName))
+			service := lbService(testK8sNamespace, testName, withPort(443), withHTTPSProxyApplication(opts...))
+			scenario.createLBService(ctx, service)
+
+			t.Logf("Waiting for full VIP connectivity of %q...", testName)
+			vipIP := scenario.waitForFullVIPConnectivity(ctx, testName)
+
+			for _, tt := range tC.testCalls {
+				testCmd := curlCmdVerbose(fmt.Sprintf("--max-time 10 --cacert /tmp/%s.crt --resolve %s:443:%s https://%s:443%s", tt.hostName, tt.hostName, vipIP, tt.hostName, tt.path))
+				t.Logf("Testing %q...", testCmd)
+				eventually(t, func() error {
+					stdout, stderr, err := clients[tt.clientNr].Exec(ctx, testCmd)
+					if !tt.blocked && err != nil {
+						return fmt.Errorf("curl failed (cmd: %q, stdout: %q, stderr: %q): %w", testCmd, stdout, stderr, err)
+					} else if tt.blocked && (err == nil || err.Error() != "cmd failed: 22") {
+						return fmt.Errorf("curl request wasn't filtered (cmd: %q, stdout: %q, stderr: %q): %w", testCmd, stdout, stderr, err)
+					}
+
+					return nil
+				}, shortTimeout, pollInterval)
+			}
+		})
+	}
+}
