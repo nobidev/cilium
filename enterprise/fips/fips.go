a5478151c84a03ad7845835534627af0e5d5e277
Alexandre Barone <abalexandrebarone@gmail.com>
2024-11-22 16:20:49 -0500

    hubble: fix FIPS images by downgrading MinTLSVersion to 1.2
    Use fips build flag to force TLS1.2 only when building FIPS images.
    
    Note that Go plans to release a new fips package in the standard
    library targetted for 1.24 that will support TLS1.3.
    https://github.com/golang/go/issues/69536
    
    Signed-off-by: Alexandre Barone <abalexandrebarone@gmail.com>

--- enterprise/fips/fips.go
+++ enterprise/fips/fips.go
@@ -12,4 +12,24 @@
 
 package fips
 
-import _ "crypto/tls/fipsonly"
+import (
+	"crypto/tls"
+	_ "crypto/tls/fipsonly"
+
+	"github.com/cilium/cilium/pkg/hubble/relay/server"
+	"github.com/cilium/cilium/pkg/hubble/server/serveroption"
+)
+
+func init() {
+	// As of 2024/11/22, boringcrypto only supports `tls.Config.MinVersion == TLS1.2`. An issue in the
+	// Go repository details the work toward allowing more recent FIPS-approved protocols/algorithms
+	// (including TLS 1.3) but was closed because of missing algorithm in the latest version of
+	// BoringCrypto.
+	// https://github.com/golang/go/issues/62372
+	//
+	// An issue was opened in the Go repository on 2024/09/19 to add a FIPS 140-3 compliant package in
+	// the standard library targeted for Go 1.24.
+	// https://github.com/golang/go/issues/69536
+	server.MinTLSVersion = tls.VersionTLS12
+	serveroption.MinTLSVersion = tls.VersionTLS12
+}
