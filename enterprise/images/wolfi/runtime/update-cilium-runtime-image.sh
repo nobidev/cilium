c92c534a855c7b2125079df2b4a174d42872078b
Maciej Kwiek <maciej@isovalent.com>
2024-11-27 13:01:25 +0100

    Add enterprise runtime GHA workflow
    Signed-off-by: Maciej Kwiek <maciej@isovalent.com>

---
+++ enterprise/images/wolfi/runtime/update-cilium-runtime-image.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#  Copyright (C) Isovalent, Inc. - All Rights Reserved. 
+# 
+#  NOTICE: All information contained herein is, and remains the property of 
+#  Isovalent Inc and its suppliers, if any. The intellectual and technical 
+#  concepts contained herein are proprietary to Isovalent Inc and its suppliers 
+#  and may be covered by U.S. and Foreign Patents, patents in process, and are 
+#  protected by trade secret or copyright law.  Dissemination of this information 
+#  or reproduction of this material is strictly forbidden unless prior written 
+#  permission is obtained from Isovalent Inc.
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+image_full=${1}
+root_dir="$(git rev-parse --show-toplevel)"
+
+cd "${root_dir}"
+
+image="quay.io/isovalent/cilium-runtime-wfi"
+
+# shellcheck disable=SC2207
+used_by=($(git grep -l "${image}:" .github/actions/; find . -type f -name enterprise-overrides.sh -print0 | xargs -0 git grep -l CILIUM_RUNTIME_IMAGE=))
+
+for i in "${used_by[@]}" ; do
+  sed -E "s#\"(CILIUM_RUNTIME_IMAGE=)${image}:.*\" >>#\"\1${image_full}\" >>#" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
+done
+
+do_check="${CHECK:-false}"
+if [ "${do_check}" = "true" ] ; then
+  git diff --exit-code "${used_by[@]}" || (echo "Runtime images out of date, " \
+    "see https://docs.cilium.io/en/latest/contributing/development/images/#update-cilium-builder-runtime-images." && \
+    exit 1)
+fi
