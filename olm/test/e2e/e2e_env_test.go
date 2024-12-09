bedaf9768c0f2ba417e243e070352e50c61dbc7f
Frederic Giloux <frederic.giloux@isovalent.com>
2024-12-09 09:58:42 +0100

    test: Add E2E tests
    E2E tests based on the envtest framework check that the controller
    - create and update resources according to the CiliumConfig
    - revert out-of-band-changes
    - update the status (conditions) of the CiliumConfig
    
    Signed-off-by: Frederic Giloux <frederic.giloux@isovalent.com>

---
+++ olm/test/e2e/e2e_env_test.go
@@ -0,0 +1,140 @@
+/*
+Copyright (C) Isovalent, Inc. - All Rights Reserved.
+
+NOTICE: All information contained herein is, and remains the property of
+Isovalent Inc and its suppliers, if any. The intellectual and technical
+concepts contained herein are proprietary to Isovalent Inc and its suppliers
+and may be covered by U.S. and Foreign Patents, patents in process, and are
+protected by trade secret or copyright law.  Dissemination of this information
+or reproduction of this material is strictly forbidden unless prior written
+permission is obtained from Isovalent Inc.
+*/
+
+package e2e_test
+
+import (
+	"context"
+	"flag"
+	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+
+	appsv1 "k8s.io/api/apps/v1"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	amtypes "k8s.io/apimachinery/pkg/types"
+
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	ciliumiov1alpha1 "github.com/isovalent/cilium/olm/api/v1alpha1"
+	"github.com/isovalent/cilium/olm/manager"
+	"github.com/isovalent/cilium/olm/test/e2e"
+)
+
+var ciliumConfig = ciliumiov1alpha1.CiliumConfig{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "config",
+	},
+	Spec: ciliumiov1alpha1.CiliumConfigSpec{
+		RawExtension: runtime.RawExtension{
+			Raw: []byte(`{"securityContext": {"privileged": true}, "ipam":{"mode": "cluster-pool"}, "cni": {"binPath": "/var/lib/cni/bin", "confPath": "/var/run/multus/cni/net.d"}}`),
+		},
+	},
+}
+
+var updatedCiliumValues = []byte(`{"operator": {"replicas": 1}, "securityContext": {"privileged": true}, "ipam":{"mode": "cluster-pool"}, "cni": {"binPath": "/var/lib/cni/bin", "confPath": "/var/run/multus/cni/net.d"}}`)
+
+func TestController(t *testing.T) {
+	// Setup
+	const ns = "cilium"
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	env := e2e.Cluster(ctx, t)
+	kcfg := e2e.NewUser(t, env, "cluster-admin", []string{"system:masters"})
+	flag.Set("kubeconfig", kcfg)
+	flag.Set("helm-path", "../../manifests")
+	k8sClient, err := client.New(env.Config, client.Options{Scheme: scheme.Scheme})
+	require.NoError(t, err, "expect no error by creating a new client")
+	require.NotNil(t, k8sClient)
+	e2e.CreateNamespace(ctx, k8sClient, ns)
+	envtest.WaitForCRDs(env.Config, []*apiextensionsv1.CustomResourceDefinition{
+		{
+			Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+				Group: "isovalent.io",
+				Names: apiextensionsv1.CustomResourceDefinitionNames{
+					Plural: "ciliumconfigs",
+				},
+				Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+					{
+						Name:    "v1alpha1",
+						Storage: true,
+						Served:  false,
+					},
+				},
+			},
+		}},
+		envtest.CRDInstallOptions{MaxTime: 50 * time.Millisecond, PollInterval: 15 * time.Millisecond})
+
+	// Check that helm resources are applied when the CiliumConfig is created
+	err = k8sClient.Create(ctx, &ciliumConfig)
+	require.NoError(t, err, "expect no error by creating a CiliumConfig")
+	go startManager(ctx)
+	ccfg := ciliumiov1alpha1.CiliumConfig{}
+	ccfgName := amtypes.NamespacedName{
+		Name: ciliumConfig.Name,
+	}
+	require.Eventuallyf(t, func() bool {
+		err = k8sClient.Get(ctx, ccfgName, &ccfg)
+		require.NoError(t, err, "expect no error by retrieving the CiliumConfig")
+		t.Logf("conditions: %s", spew.Sdump(ccfg.Status.Conditions))
+		return meta.IsStatusConditionTrue(ccfg.Status.Conditions, ciliumiov1alpha1.APINotAvailableCondition) &&
+			meta.IsStatusConditionFalse(ccfg.Status.Conditions, ciliumiov1alpha1.ProcessingErrorCondition) &&
+			meta.IsStatusConditionFalse(ccfg.Status.Conditions, ciliumiov1alpha1.ValuesErrorsCondition)
+	}, 10*time.Second, 100*time.Millisecond, "conditions not as expected")
+	depl := appsv1.Deployment{}
+	deplName := amtypes.NamespacedName{
+		Namespace: ns,
+		Name:      "cilium-operator",
+	}
+	err = k8sClient.Get(ctx, deplName, &depl)
+	require.NoError(t, err, "expect no error by retrieving cilium operator deployment after the CiliumConfig has been created")
+	// Check that the default number of replicas has been configured
+	require.Equal(t, int32(2), *depl.Spec.Replicas)
+
+	// Check that helm resources are updated when the CiliumConfig is changed
+	ccfg.Spec.Raw = updatedCiliumValues
+	err = k8sClient.Update(ctx, &ccfg)
+	require.NoError(t, err, "expect no error by updating the ciliumConfig")
+	// Check that the number of replicas has been changed
+	require.Eventuallyf(t, func() bool {
+		err = k8sClient.Get(ctx, deplName, &depl)
+		require.NoError(t, err, "expect no error by retrieving the CiliumConfig")
+		return *depl.Spec.Replicas == int32(1)
+	}, 3*time.Second, 10*time.Millisecond, "number of replicas not as expected")
+
+	// Check that helm resources are reapplied when they are deleted out-of-band
+	err = k8sClient.Delete(ctx, &depl)
+	require.Eventuallyf(t, func() bool {
+		err = k8sClient.Get(ctx, deplName, &depl)
+		if err == nil {
+			return true
+		}
+		if !apierrors.IsNotFound(err) {
+			require.Fail(t, "expect no error by retrieving the cilium operator deployment after the CiliumConfig has been updated: %w", err)
+		}
+		return false
+	}, 3*time.Second, 10*time.Millisecond, "deployment not recreated after it has been deleted out-of-band")
+}
+
+func startManager(ctx context.Context) {
+	manager.Start(ctx)
+}
---
+++ olm/test/e2e/infra.go
@@ -0,0 +1,135 @@
+/*
+Copyright (C) Isovalent, Inc. - All Rights Reserved.
+
+NOTICE: All information contained herein is, and remains the property of
+Isovalent Inc and its suppliers, if any. The intellectual and technical
+concepts contained herein are proprietary to Isovalent Inc and its suppliers
+and may be covered by U.S. and Foreign Patents, patents in process, and are
+protected by trade secret or copyright law.  Dissemination of this information
+or reproduction of this material is strictly forbidden unless prior written
+permission is obtained from Isovalent Inc.
+*/
+
+package e2e
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/kubectl/pkg/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	ciliumiov1alpha1 "github.com/isovalent/cilium/olm/api/v1alpha1"
+
+	"github.com/isovalent/cilium/olm/helm"
+)
+
+var testEnv *envtest.Environment
+
+// Cluster provisions a new cluster or returns an existing one
+func Cluster(ctx context.Context, t *testing.T) *envtest.Environment {
+	// TODO: Add the possibility to use an existing cluster
+	// for which the location of the kubeconfig file has been set through
+	// an environment variable or a flag
+	if testEnv != nil {
+		return testEnv
+	}
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		BinaryAssetsDirectory: os.Getenv("ENVTEST_PATH"),
+		ErrorIfCRDPathMissing: true,
+	}
+
+	cfg, err := testEnv.Start()
+	require.NoError(t, err, "expect no error by starting the environment")
+	require.NotNil(t, cfg)
+	t.Cleanup(func() {
+		err = testEnv.Stop()
+		require.NoError(t, err, "expect no error by shuting down the API server and etcd")
+
+	})
+
+	err = ciliumiov1alpha1.AddToScheme(scheme.Scheme)
+	require.NoError(t, err, "expect no error adding the ciliumconfig scheme")
+
+	k8sClient, err := client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	require.NoError(t, err, "expect no error by creating a new client")
+	require.NotNil(t, k8sClient)
+
+	roles, err := os.Open(filepath.Join("..", "..", "config", "rbac", "role.yaml"))
+	require.NoError(t, err, "expect no error by reading the yaml file containing roles")
+	defer roles.Close()
+	uRoles, err := helm.Decode(bufio.NewReader(roles))
+	require.NoError(t, err, "expect no error by decoding the yaml file containing roles")
+	for _, u := range uRoles {
+		err = k8sClient.Patch(ctx, u, client.Apply, client.FieldOwner("envtest"))
+		require.NoError(t, err, "expect no error by applying roles")
+	}
+	bindings, err := os.Open(filepath.Join("..", "..", "config", "rbac", "role_binding.yaml"))
+	require.NoError(t, err, "expect no error by reading the yaml file containing role bindings")
+	defer roles.Close()
+	uBindings, err := helm.Decode(bufio.NewReader(bindings))
+	require.NoError(t, err, "expect no error by decoding the yaml file containing role bindings")
+	for _, u := range uBindings {
+		err = k8sClient.Patch(ctx, u, client.Apply, client.FieldOwner("envtest"))
+		require.NoError(t, err, "expect no error by applying role bindings")
+	}
+	/*sa, err := os.Open(filepath.Join("..", "..", "config", "rbac", "service_account.yaml"))
+	require.NoError(t, err, "expect no error by reading the yaml file containing the service account")
+	defer roles.Close()
+	uSA, err := helm.Decode(bufio.NewReader(sa))
+	require.NoError(t, err, "expect no error by decoding the yaml file containing the service account")*/
+
+	/*for _, u := range uSA {
+		err = k8sClient.Patch(ctx, u, client.Apply, client.FieldOwner("envtest"))
+		require.NoError(t, err, "expect no error by applying the service account")
+	}*/
+	return testEnv
+}
+
+func CreateNamespace(ctx context.Context, k8sClient client.Client, namespace string) {
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespace,
+		},
+	}
+	k8sClient.Create(ctx, ns)
+}
+
+func DeleteNamespace(ctx context.Context, k8sClient client.Client, namespace string) {
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: namespace,
+		},
+	}
+	k8sClient.Delete(ctx, ns)
+}
+
+func NewUser(t *testing.T, env *envtest.Environment, userName string, userGroups []string) string {
+	user, err := env.AddUser(envtest.User{
+		Name:   userName,
+		Groups: userGroups,
+	}, nil)
+	require.NoError(t, err, "expect no error by creating users")
+
+	kcfgFile, err := os.CreateTemp("", "envtest-kubeconfig-")
+	require.NoError(t, err, "expect no error by creating a temporary file")
+	t.Cleanup(func() {
+		os.Remove(kcfgFile.Name())
+		require.NoError(t, err, "expect no error by deleting the kubeconfig file")
+	})
+	kcfg, err := user.KubeConfig()
+	require.NoError(t, err, "expect no error by getting a user kubeconfig")
+	_, err = kcfgFile.Write(kcfg)
+	require.NoError(t, err, "expect no error by writing the user kubeconfig")
+	return kcfgFile.Name()
+}
