7fcc37a55b0a6fc7f179637dd90536c29c450fb2
Antony Reynaud <antony.reynaud@isovalent.com>
2025-01-22 16:23:43 +0100

    refac: ignore encrypt interface field
    This commit ignores the field EncryptInterface when verifying if there's any mutation of option.Config after its initialization.
    With this commit, triggering reinitializeIPSec path causing mutation when using it without kube-proxy replacement on EKS clusters won't fail Cilium startup.
    This is a temporary fix, once bpf_network will be removed for IPsec, EncryptInterface won't be used anymore.
    
    Signed-off-by: Antony Reynaud <antony.reynaud@isovalent.com>

--- pkg/option/config.go
+++ pkg/option/config.go
@@ -3793,6 +3793,7 @@ func (c *DaemonConfig) checksum() [32]byte {
 	sumConfig := *c
 	// Ignore variable parts
 	sumConfig.Opts = nil
+	sumConfig.EncryptInterface = nil
 	cBytes, err := json.Marshal(&sumConfig)
 	if err != nil {
 		return [32]byte{}
@@ -3848,7 +3849,8 @@ func (c *DaemonConfig) diffFromFile() error {
 
 		diff = cmp.Diff(&config, c, opts,
 			cmpopts.IgnoreTypes(&IntOptions{}),
-			cmpopts.IgnoreTypes(&OptionLibrary{}))
+			cmpopts.IgnoreTypes(&OptionLibrary{}),
+			cmpopts.IgnoreFields(DaemonConfig{}, "EncryptInterface"))
 	}
 	return fmt.Errorf("Config differs:\n%s", diff)
 }
