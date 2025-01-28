1f0209b499ee84225b754591e92eca3d2291660a
Sebastian Wicki <sebastian@isovalent.com>
2025-01-28 13:38:34 +0100

    ipmigration: Add `enterprise/pkg/ipmigration`
    This is the first of a series of commits adding a new `ipmigration`
    package to the enterprise codebase. The ipmigration package is
    responsible for supporting IP migration by allowing pods to be attached
    and detached from Cilium. A detached pod is a pod is a pod without a
    corresponding Cilium endpoint (and without connectivity), but allows the
    IP to be used concurrently by a different pod. This is useful in the
    context of KubeVirt VM migration where the target VM can be set up in a
    detached state, and become attached once migration has finished the
    source VM pod has been deleted, thereby also allowing ownership of the
    source VM pod IP to be transferred to the target pod.
    
    Pods are marked as detached by an annotation. This commit contains the
    name and value for the annotation. Later commits will add the actual
    logic to start pods in a detached state and moving pods from a detached
    state to an attached state.
    
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

---
+++ enterprise/pkg/ipmigration/types/types.go
@@ -0,0 +1,64 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package types
+
+import (
+	"fmt"
+	"net/netip"
+
+	"github.com/spf13/pflag"
+)
+
+// Config registers a command-line flag on the agent to enable this subsystem
+type Config struct {
+	EnablePodIPMigration bool
+}
+
+func (c Config) Flags(flags *pflag.FlagSet) {
+	flags.Bool("enable-pod-ip-migration", c.EnablePodIPMigration, "Enable support for pod IP migration (beta)")
+	flags.MarkHidden("enable-pod-ip-migration")
+}
+
+const (
+	// DetachedAnnotation is the name of the annotation for detached pods
+	DetachedAnnotation = "cni.v1alpha1.isovalent.com/detached"
+)
+
+// DetachedIpamAddressPair is the value of detached annotations (encoded as JSON)
+type DetachedIpamAddressPair struct {
+	IPV4 *netip.Addr `json:"ipv4,omitempty"`
+	IPV6 *netip.Addr `json:"ipv6,omitempty"`
+}
+
+func (p *DetachedIpamAddressPair) ipv4Valid() bool {
+	return p.IPV4 != nil && p.IPV4.IsValid()
+}
+
+func (p *DetachedIpamAddressPair) ipv6Valid() bool {
+	return p.IPV6 != nil && p.IPV6.IsValid()
+}
+
+func (p *DetachedIpamAddressPair) IsValid() bool {
+	return p.ipv4Valid() || p.ipv6Valid()
+}
+
+func (p *DetachedIpamAddressPair) String() string {
+	switch {
+	case p.ipv4Valid() && p.ipv6Valid():
+		return fmt.Sprintf("ipv4: %s/ipv6: %s", p.IPV4, p.IPV6)
+	case p.ipv4Valid():
+		return fmt.Sprintf("ipv4: %s", p.IPV4)
+	case p.ipv6Valid():
+		return fmt.Sprintf("ipv6: %s", p.IPV6)
+	default:
+		return "<invalid>"
+	}
+}
