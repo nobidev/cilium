fdf7248bc66e00422ac77e40775f41c896384a2b
Fabio Falzoi <fabio.falzoi@isovalent.com>
2025-01-31 19:13:32 +0100

    egressgwha: Add source file non-Linux target
    Signed-off-by: Fabio Falzoi <fabio.falzoi@isovalent.com>

---
+++ enterprise/pkg/egressgatewayha/egressipconf/ops_other.go
@@ -0,0 +1,45 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+//go:build !linux
+
+package egressipconf
+
+import (
+	"context"
+	"iter"
+	"log/slog"
+
+	"github.com/cilium/cilium/enterprise/datapath/tables"
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/reconciler"
+)
+
+func (o *ops) Update(ctx context.Context, txn statedb.ReadTxn, obj *tables.EgressIPEntry) error {
+	return ErrNotImplemented
+}
+
+func (o *ops) Delete(context.Context, statedb.ReadTxn, *tables.EgressIPEntry) error {
+	return ErrNotImplemented
+}
+
+func (o *ops) Prune(ctx context.Context, txn statedb.ReadTxn, objects iter.Seq2[*tables.EgressIPEntry, statedb.Revision]) error {
+	return ErrNotImplemented
+}
+
+func newOps(logger *slog.Logger) *ops {
+	return &ops{logger}
+}
+
+type ops struct {
+	logger *slog.Logger
+}
+
+var _ reconciler.Operations[*tables.EgressIPEntry] = &ops{}
