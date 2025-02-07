e99806880651e87e8c4989889d5df08a16ac880a
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2025-02-07 20:04:07 +0900

    bgpv2: Enterprise CRD => Neighbor conversion function
    Introduce enterprise-equivalent of types.ToNeighborV2
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

---
+++ enterprise/pkg/bgpv1/manager/reconcilerv2/conversions.go
@@ -0,0 +1,126 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilerv2
+
+import (
+	"net/netip"
+
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+)
+
+// toNeighbor converts a IsovalentBGPNodePeer to Neighbor which can be used
+// with Router API. The caller must ensure that the np, np.PeerAddress,
+// np.PeerASN and pc are not nil.
+func toNeighbor(np *v1alpha1.IsovalentBGPNodePeer, pc *v1alpha1.IsovalentBGPPeerConfigSpec, password string) *types.Neighbor {
+	neighbor := &types.Neighbor{}
+
+	neighbor.Address = toPeerAddress(*np.PeerAddress)
+	neighbor.ASN = uint32(*np.PeerASN)
+	neighbor.AuthPassword = password
+	neighbor.EbgpMultihop = toNeighborEbgpMultihop(pc.EBGPMultihop)
+	neighbor.Timers = toNeighborTimers(pc.Timers)
+	neighbor.Transport = toNeighborTransport(np.LocalAddress, pc.Transport)
+	neighbor.GracefulRestart = toNeighborGracefulRestart(pc.GracefulRestart)
+	neighbor.AfiSafis = toNeighborAfiSafis(pc.Families)
+
+	return neighbor
+}
+
+func toPeerAddress(peerAddress string) netip.Addr {
+	addr, err := netip.ParseAddr(peerAddress)
+	if err != nil {
+		return netip.Addr{}
+	}
+	return addr
+}
+
+func toNeighborEbgpMultihop(ebgpMultihop *int32) *types.NeighborEbgpMultihop {
+	if ebgpMultihop == nil || *ebgpMultihop <= 1 {
+		return nil
+	}
+	return &types.NeighborEbgpMultihop{
+		TTL: uint32(*ebgpMultihop),
+	}
+}
+
+func toNeighborTimers(apiTimers *v2alpha1.CiliumBGPTimers) *types.NeighborTimers {
+	if apiTimers == nil {
+		return nil
+	}
+
+	timers := &types.NeighborTimers{}
+
+	if apiTimers.ConnectRetryTimeSeconds != nil {
+		timers.ConnectRetry = uint64(*apiTimers.ConnectRetryTimeSeconds)
+	}
+
+	if apiTimers.HoldTimeSeconds != nil {
+		timers.HoldTime = uint64(*apiTimers.HoldTimeSeconds)
+	}
+
+	if apiTimers.KeepAliveTimeSeconds != nil {
+		timers.KeepaliveInterval = uint64(*apiTimers.KeepAliveTimeSeconds)
+	}
+
+	return timers
+}
+
+func toNeighborTransport(apiLocalAddress *string, apiTransport *v2alpha1.CiliumBGPTransport) *types.NeighborTransport {
+	if apiLocalAddress == nil && apiTransport == nil {
+		return nil
+	}
+
+	transport := &types.NeighborTransport{}
+
+	if apiLocalAddress != nil {
+		transport.LocalAddress = *apiLocalAddress
+	}
+
+	if apiTransport != nil {
+		if apiTransport.LocalPort != nil {
+			transport.LocalPort = uint32(*apiTransport.LocalPort)
+		}
+		if apiTransport.PeerPort != nil {
+			transport.RemotePort = uint32(*apiTransport.PeerPort)
+		}
+	}
+
+	return transport
+}
+
+func toNeighborGracefulRestart(apiGR *v2alpha1.CiliumBGPNeighborGracefulRestart) *types.NeighborGracefulRestart {
+	if apiGR == nil || apiGR.RestartTimeSeconds == nil {
+		return nil
+	}
+	return &types.NeighborGracefulRestart{
+		Enabled:     apiGR.Enabled,
+		RestartTime: uint32(*apiGR.RestartTimeSeconds),
+	}
+}
+
+func toNeighborAfiSafis(families []v2alpha1.CiliumBGPFamilyWithAdverts) []*types.Family {
+	if len(families) == 0 {
+		return nil
+	}
+
+	afiSafis := []*types.Family{}
+
+	for _, family := range families {
+		afiSafis = append(afiSafis, &types.Family{
+			Afi:  types.ParseAfi(family.Afi),
+			Safi: types.ParseSafi(family.Safi),
+		})
+	}
+
+	return afiSafis
+}
---
+++ enterprise/pkg/bgpv1/manager/reconcilerv2/conversions_test.go
@@ -0,0 +1,317 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilerv2
+
+import (
+	"net/netip"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"k8s.io/utils/ptr"
+
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	"github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2alpha1"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+)
+
+func TestToNeighbor(t *testing.T) {
+	table := []struct {
+		name         string
+		nodePeer     *v1alpha1.IsovalentBGPNodePeer
+		peerConfig   *v1alpha1.IsovalentBGPPeerConfigSpec
+		authPassword string
+		expected     *types.Neighbor
+	}{
+		{
+			name: "IPv4 Minimal",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("10.0.0.1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("10.0.0.1"),
+				ASN:     64512,
+			},
+		},
+		{
+			name: "IPv6 Minimal",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+			},
+		},
+		{
+			name: "LocalAddress",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress:  ptr.To("fd00::1"),
+				PeerASN:      ptr.To(int64(64512)),
+				LocalAddress: ptr.To("fd00::2"),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				Transport: &types.NeighborTransport{
+					LocalAddress: "fd00::2",
+				},
+			},
+		},
+		{
+			name: "LocalPort",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					Transport: &v2alpha1.CiliumBGPTransport{
+						LocalPort: ptr.To(int32(1790)),
+					},
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				Transport: &types.NeighborTransport{
+					LocalPort: 1790,
+				},
+			},
+		},
+		{
+			name: "PeerPort",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					Transport: &v2alpha1.CiliumBGPTransport{
+						PeerPort: ptr.To(int32(1790)),
+					},
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				Transport: &types.NeighborTransport{
+					RemotePort: 1790,
+				},
+			},
+		},
+		{
+			name: "Timers",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					Timers: &v2alpha1.CiliumBGPTimers{
+						ConnectRetryTimeSeconds: ptr.To(int32(1)),
+						HoldTimeSeconds:         ptr.To(int32(3)),
+						KeepAliveTimeSeconds:    ptr.To(int32(1)),
+					},
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				Timers: &types.NeighborTimers{
+					ConnectRetry:      1,
+					HoldTime:          3,
+					KeepaliveInterval: 1,
+				},
+			},
+		},
+		{
+			name: "AuthPassword",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig:   &v1alpha1.IsovalentBGPPeerConfigSpec{},
+			authPassword: "password",
+			expected: &types.Neighbor{
+				Address:      netip.MustParseAddr("fd00::1"),
+				ASN:          64512,
+				AuthPassword: "password",
+			},
+		},
+		{
+			name: "GracefulRestart",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					GracefulRestart: &v2alpha1.CiliumBGPNeighborGracefulRestart{
+						Enabled:            true,
+						RestartTimeSeconds: ptr.To(int32(3)),
+					},
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				GracefulRestart: &types.NeighborGracefulRestart{
+					Enabled:     true,
+					RestartTime: 3,
+				},
+			},
+		},
+		{
+			name: "EBGPMultihop",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					EBGPMultihop: ptr.To(int32(3)),
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				EbgpMultihop: &types.NeighborEbgpMultihop{
+					TTL: 3,
+				},
+			},
+		},
+		{
+			name: "Families",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress: ptr.To("fd00::1"),
+				PeerASN:     ptr.To(int64(64512)),
+			},
+
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					Families: []v2alpha1.CiliumBGPFamilyWithAdverts{
+						{
+
+							CiliumBGPFamily: v2alpha1.CiliumBGPFamily{
+								Afi:  "ipv4",
+								Safi: "unicast",
+							},
+						},
+						{
+							CiliumBGPFamily: v2alpha1.CiliumBGPFamily{
+								Afi:  "ipv6",
+								Safi: "unicast",
+							},
+						},
+					},
+				},
+			},
+			expected: &types.Neighbor{
+				Address: netip.MustParseAddr("fd00::1"),
+				ASN:     64512,
+				AfiSafis: []*types.Family{
+					{
+						Afi:  types.AfiIPv4,
+						Safi: types.SafiUnicast,
+					},
+					{
+						Afi:  types.AfiIPv6,
+						Safi: types.SafiUnicast,
+					},
+				},
+			},
+		},
+		{
+			name: "Maximum",
+			nodePeer: &v1alpha1.IsovalentBGPNodePeer{
+				PeerAddress:  ptr.To("fd00::1"),
+				PeerASN:      ptr.To(int64(64512)),
+				LocalAddress: ptr.To("fd00::2"),
+			},
+			peerConfig: &v1alpha1.IsovalentBGPPeerConfigSpec{
+				CiliumBGPPeerConfigSpec: v2alpha1.CiliumBGPPeerConfigSpec{
+					Transport: &v2alpha1.CiliumBGPTransport{
+						LocalPort: ptr.To(int32(1790)),
+						PeerPort:  ptr.To(int32(1790)),
+					},
+					Timers: &v2alpha1.CiliumBGPTimers{
+						ConnectRetryTimeSeconds: ptr.To(int32(1)),
+						HoldTimeSeconds:         ptr.To(int32(3)),
+						KeepAliveTimeSeconds:    ptr.To(int32(1)),
+					},
+					GracefulRestart: &v2alpha1.CiliumBGPNeighborGracefulRestart{
+						Enabled:            true,
+						RestartTimeSeconds: ptr.To(int32(3)),
+					},
+					EBGPMultihop: ptr.To(int32(3)),
+					Families: []v2alpha1.CiliumBGPFamilyWithAdverts{
+						{
+							CiliumBGPFamily: v2alpha1.CiliumBGPFamily{
+								Afi:  "ipv4",
+								Safi: "unicast",
+							},
+						},
+						{
+							CiliumBGPFamily: v2alpha1.CiliumBGPFamily{
+								Afi:  "ipv6",
+								Safi: "unicast",
+							},
+						},
+					},
+				},
+			},
+			authPassword: "password",
+			expected: &types.Neighbor{
+				Address:      netip.MustParseAddr("fd00::1"),
+				ASN:          64512,
+				AuthPassword: "password",
+				EbgpMultihop: &types.NeighborEbgpMultihop{
+					TTL: 3,
+				},
+				Timers: &types.NeighborTimers{
+					ConnectRetry:      1,
+					HoldTime:          3,
+					KeepaliveInterval: 1,
+				},
+				Transport: &types.NeighborTransport{
+					LocalAddress: "fd00::2",
+					LocalPort:    1790,
+					RemotePort:   1790,
+				},
+				GracefulRestart: &types.NeighborGracefulRestart{
+					Enabled:     true,
+					RestartTime: 3,
+				},
+				AfiSafis: []*types.Family{
+					{
+						Afi:  types.AfiIPv4,
+						Safi: types.SafiUnicast,
+					},
+					{
+						Afi:  types.AfiIPv6,
+						Safi: types.SafiUnicast,
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			neighbor := toNeighbor(tt.nodePeer, tt.peerConfig, tt.authPassword)
+			require.Equal(t, tt.expected, neighbor)
+		})
+	}
+}
