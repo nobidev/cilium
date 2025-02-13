2a4b967a9c11135929135dfedd7e2607ce7185d1
Marco Iorio <marco.iorio@isovalent.com>
2025-02-13 14:39:26 +0100

    ent/gha/scale-egw: switch to enterprise egress gateway policy
    Configure the enterprise egress gateway policies here, rather than the
    OSS one. To this end, we leverage the dedicated override variable.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- .github/actions/set-env-variables/enterprise-scalability-overrides.sh
+++ .github/actions/set-env-variables/enterprise-scalability-overrides.sh
@@ -3,3 +3,5 @@
 echo "GCP_PERF_RESULTS_BUCKET=gs://cilium-scale-results-cee" >> "$GITHUB_ENV"
 # Memory usage is higher for CEE as we have a slightly bigger binary
 echo "CL2_MEDIAN_MEM_USAGE_THRESHOLD=270" >> "$GITHUB_ENV"
+# Create an enterprise EGW policy, rather than the OSS one
+echo "CL2_EGW_POLICY_TEMPLATE=manifests/enterprise-egw-policy.yaml" >> "$GITHUB_ENV"
