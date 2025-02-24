nobidev/cilium

commit fb4ed4c6266d81584343f28485d945df8534b28e

helm: Add integrated hubble-k8s-exporter
This change adds support to install the Hubble K8s Exporter
(https://github.com/isovalent/hubble-timescape/tree/main/install/kubernetes/hubble-k8s-exporter)
from the cilium enterprise chart. The Hubble K8s Exporter watches
changes to NetworkPolicies and exports them to Timescape.

The exporter is deployed through a simple deployment. The deployment is
automatically created if a user enables integrated Timescape, but an
also explicitly be enabled or disabled. It needs clusterwide permission
to watch (Cilium)NetworkPolicies and needs to run as root, as it exports
events to `/var/run/cilium/hubble`.

Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

install/kubernetes/cilium/templates/hubble-k8s-exporter/_helpers.tpl
@@ -1,0 +1,5 @@
+{{- define "hubble.k8s-exporter.labels" }}
+k8s-app: hubble-k8s-exporter
+app.kubernetes.io/name: hubble-k8s-exporter
+app.kubernetes.io/part-of: cilium
+{{- end }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/clusterrole.yaml
@@ -1,0 +1,31 @@
+{{- if and
+  .Values.hubble.enabled 
+  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  .Values.serviceAccounts.hubbleK8sExporter.create
+  .Values.rbac.create
+}}
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: hubble-k8s-exporter
+  labels:
+    {{- include "hubble.k8s-exporter.labels" . | nindent 4 }}
+rules:
+- apiGroups:
+  - networking.k8s.io
+  resources:
+  - networkpolicies
+  verbs:
+  - get
+  - list
+  - watch
+- apiGroups:
+  - cilium.io
+  resources:
+  - ciliumnetworkpolicies
+  - ciliumclusterwidenetworkpolicies
+  verbs:
+  - get
+  - list
+  - watch
+{{- end }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/clusterrolebinding.yaml
@@ -1,0 +1,21 @@
+{{- if and
+  .Values.hubble.enabled 
+  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  .Values.serviceAccounts.hubbleK8sExporter.create
+  .Values.rbac.create
+}}
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: hubble-k8s-exporter
+  labels:
+    {{- include "hubble.k8s-exporter.labels" . | nindent 4 }}
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: hubble-k8s-exporter
+subjects:
+- kind: ServiceAccount
+  name: {{ .Values.serviceAccounts.hubbleK8sExporter.name }}
+  namespace: {{ include "cilium.namespace" . }}
+{{- end }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/configmap.yaml
@@ -1,0 +1,20 @@
+{{- if and .Values.hubble.enabled ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )}}
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: hubble-k8s-exporter
+  namespace: {{ include "cilium.namespace" . }}
+  labels:
+    {{- include "hubble.k8s-exporter.labels" . | nindent 4 }}
+data:
+  config.yaml: |
+    debug: {{ .Values.hubble.k8sExporter.debug | default false }}
+    export:
+      cluster-name: {{ .Values.cluster.name | quote }}
+      file-compression: {{ .Values.hubble.k8sExporter.export.fileCompression | quote }}
+      file-path: {{ .Values.hubble.k8sExporter.export.filePath | quote }}
+      file-rotation-interval: {{ .Values.hubble.k8sExporter.export.fileRotationInterval | quote }}
+      k8s-endpoints: false
+      max-file-backup: {{ .Values.hubble.k8sExporter.export.maxFileBackups | quote }}
+      max-file-size: {{ .Values.hubble.k8sExporter.export.maxFileSize | quote }}
+{{- end }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/deployment.yaml
@@ -1,0 +1,76 @@
+{{- if and .Values.hubble.enabled ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )}}
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: hubble-k8s-exporter
+  namespace: {{ include "cilium.namespace" . }}
+  labels:
+    {{- include "hubble.k8s-exporter.labels" . | nindent 4 }}
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      k8s-app: hubble-k8s-exporter
+      app.kubernetes.io/name: hubble-k8s-exporter
+      app.kubernetes.io/part-of: cilium
+  template:
+    metadata:
+      {{- with .Values.hubble.k8sExporter.podAnnotations }}
+      annotations:
+        {{- toYaml . | nindent 8 }}
+      {{- end }}
+      labels:
+        k8s-app: hubble-k8s-exporter
+        app.kubernetes.io/name: hubble-k8s-exporter
+        app.kubernetes.io/part-of: cilium
+    spec:
+      {{- with .Values.imagePullSecrets }}
+      imagePullSecrets:
+        {{- toYaml . | nindent 8 }}
+      {{- end }}
+      serviceAccountName: {{ .Values.serviceAccounts.hubbleK8sExporter.name }}
+      securityContext:
+        {{- toYaml .Values.hubble.k8sExporter.podSecurityContext | nindent 8 }}
+      containers:
+        - name: hubble-k8s-exporter
+          securityContext:
+            {{- toYaml .Values.hubble.k8sExporter.securityContext | nindent 12 }}
+          image: "{{ .Values.image.repository }}:{{ .Values.image.tag | default .Chart.AppVersion }}"
+          image: {{ include "cilium.image" .Values.hubble.k8sExporter.image | quote }}
+          imagePullPolicy: {{ .Values.hubble.k8sExporter.image.pullPolicy }}
+          command:
+            - /usr/bin/hubble-k8s-exporter
+          args:
+            - export
+          resources:
+            {{- toYaml .Values.hubble.k8sExporter.resources | nindent 12 }}
+          volumeMounts:
+            - mountPath: "/etc/hubble-k8s-exporter"
+              name: config
+              readOnly: true
+            - mountPath: /var/run/cilium
+              name: cilium-run
+      {{- with .Values.hubble.k8sExporter.nodeSelector }}
+      nodeSelector:
+        {{- toYaml . | nindent 8 }}
+      {{- end }}
+      {{- with .Values.hubble.k8sExporter.affinity }}
+      affinity:
+        {{- toYaml . | nindent 8 }}
+      {{- end }}
+      {{- with .Values.hubble.k8sExporter.tolerations }}
+      tolerations:
+        {{- toYaml . | nindent 8 }}
+      {{- end }}
+      volumes:
+        - name: config
+          configMap:
+            name: hubble-k8s-exporter
+            items:
+            - key: config.yaml
+              path: config.yaml
+        - name: cilium-run
+          hostPath:
+            path: /var/run/cilium
+            type: DirectoryOrCreate
+{{- end }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/serviceaccount.yaml
@@ -1,0 +1,17 @@
+{{- if and
+  .Values.hubble.enabled 
+  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  .Values.serviceAccounts.hubbleK8sExporter.create
+}}
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: {{ .Values.serviceAccounts.hubbleK8sExporter.name | quote }}
+  namespace: {{ include "cilium.namespace" . }}
+  labels:
+    {{- include "hubble.k8s-exporter.labels" . | nindent 4 }}
+  {{- with .Values.serviceAccounts.hubbleK8sExporter.annotations }}
+  annotations:
+    {{- toYaml . | nindent 4 }}
+  {{- end }}
+{{- end }}

install/kubernetes/cilium/values.yaml
@@ -171,6 +171,11 @@ serviceAccounts:
     name: hubble-generate-certs
     automount: true
     annotations: {}
+  hubbleK8sExporter:
+    create: true
+    name: hubble-k8s-exporter
+    automount: true
+    annotations: {}
 # -- Configure termination grace period for cilium-agent DaemonSet.
 terminationGracePeriodSeconds: 1
 # -- Install the cilium agent resources.
@@ -1928,6 +1933,56 @@ hubble:
         useDigest: false
         pullPolicy: "Always"
       resources: {}
+  # -- Enables experimental support for using the Hubble Kubernetes Events Exporter.
+  # The exporter is enabled by default when using integrated Hubble Timescape
+  k8sExporter:
+    # @schema
+    # type: [null, string]
+    # @schema
+    enabled: ~
+    image:
+      # @schema
+      # type: [null, string]
+      # @schema
+      override: ~
+      repository: quay.io/isovalent/hubble-k8s-exporter
+      tag: "v1.5.1"
+      digest: ""
+      useDigest: false
+      pullPolicy: "Always"
+    # -- Specifies the resources for the hubble-k8s-exporter pods
+    resources: {}
+    # -- Affinity for hubble-k8s-exporter
+    affinity: {}
+    # -- Node labels for pod assignment
+    # ref: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#nodeselector
+    nodeSelector:
+      kubernetes.io/os: linux
+    # -- Node tolerations for pod assignment on nodes with taints
+    # ref: https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/
+    tolerations: []
+    # -- Annotations to be added to hubble-k8s-exporter pods
+    podAnnotations: {}
+    podSecurityContext:
+      fsGroup: 65532
+    # -- hubble-k8s-exporter container security context
+    securityContext:
+      # readOnlyRootFilesystem: true
+      runAsUser: 0
+      capabilities:
+        drop:
+          - ALL
+    export:
+      # Absolute path of the export file location
+      filePath: "/var/run/cilium/hubble/k8s-events.log"
+      # Compress rotated files
+      fileCompression: true
+      # Interval at which to rotate JSON export files in addition to rotating them by size
+      fileRotationInterval: 0s
+      # Maximum size of the file in megabytes
+      maxFileSize: 50
+      # Number of rotated files to keep
+      maxFileBackups: 3
   # -- Enables experimental support for using Hubble RBAC with Hubble Relay.
   # This is incompatible with Hubble RBAC installed via the Hubble Enterprise chart.
   rbac:

install/kubernetes/cilium/zz_enterprise_values.yaml.tmpl
@@ -97,6 +97,13 @@ enterprise:
     # -- Enables Loadbalancer mode (Control Plane)
     enabled: false
 
+serviceAccounts:
+  hubbleK8sExporter:
+    create: true
+    name: hubble-k8s-exporter
+    automount: true
+    annotations: {}
+
 hubble:
   # -- Enables experimental support for using integrated Hubble Timescape.
   timescape:
@@ -169,6 +176,56 @@ hubble:
         useDigest: false
         pullPolicy: "Always"
       resources: {}
+  # -- Enables experimental support for using the Hubble Kubernetes Events Exporter.
+  # The exporter is enabled by default when using integrated Hubble Timescape
+  k8sExporter:
+    # @schema
+    # type: [null, string]
+    # @schema
+    enabled: ~
+    image:
+      # @schema
+      # type: [null, string]
+      # @schema
+      override: ~
+      repository: quay.io/isovalent/hubble-k8s-exporter
+      tag: "v1.5.1"
+      digest: ""
+      useDigest: false
+      pullPolicy: "Always"
+    # -- Specifies the resources for the hubble-k8s-exporter pods
+    resources: {}
+    # -- Affinity for hubble-k8s-exporter
+    affinity: {}
+    # -- Node labels for pod assignment
+    # ref: https://kubernetes.io/docs/concepts/scheduling-eviction/assign-pod-node/#nodeselector
+    nodeSelector:
+      kubernetes.io/os: linux
+    # -- Node tolerations for pod assignment on nodes with taints
+    # ref: https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/
+    tolerations: []
+    # -- Annotations to be added to hubble-k8s-exporter pods
+    podAnnotations: {}
+    podSecurityContext:
+      fsGroup: 65532
+    # -- hubble-k8s-exporter container security context
+    securityContext:
+      # readOnlyRootFilesystem: true
+      runAsUser: 0
+      capabilities:
+        drop:
+          - ALL
+    export:
+      # Absolute path of the export file location
+      filePath: "/var/run/cilium/hubble/k8s-events.log"
+      # Compress rotated files
+      fileCompression: true
+      # Interval at which to rotate JSON export files in addition to rotating them by size
+      fileRotationInterval: 0s
+      # Maximum size of the file in megabytes
+      maxFileSize: 50
+      # Number of rotated files to keep
+      maxFileBackups: 3
   # -- Enables experimental support for using Hubble RBAC with Hubble Relay.
   # This is incompatible with Hubble RBAC installed via the Hubble Enterprise chart.
   rbac:

commit 8b876c1e20a642b33afee2664f994da5760a2c29

helm: Fix hubble-k8s-exporter enable logic
The logic to decide if the hubble-k8s-exporter should be deployed was
broken, we always deployed it when timescape was enabled, as helm's
`default` function actually treats `flase` as unset.

Also fix wrong helm schema.

Fixes: fb4ed4c ("helm: Add integrated hubble-k8s-exporter")

Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

install/kubernetes/cilium/templates/hubble-k8s-exporter/_helpers.tpl
@@ -3,3 +3,10 @@ k8s-app: hubble-k8s-exporter
 app.kubernetes.io/name: hubble-k8s-exporter
 app.kubernetes.io/part-of: cilium
 {{- end }}
+
+{{- define "hubble.k8s-exporter.enabled" -}}
+{{- or .Values.hubble.k8sExporter.enabled
+ (and (kindIs "invalid" .Values.hubble.k8sExporter.enabled) .Values.hubble.timescape.enabled)
+-}}
+{{- end }}
+

install/kubernetes/cilium/templates/hubble-k8s-exporter/clusterrole.yaml
@@ -1,6 +1,6 @@
 {{- if and
   .Values.hubble.enabled 
-  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  (eq (include "hubble.k8s-exporter.enabled" .) "true")
   .Values.serviceAccounts.hubbleK8sExporter.create
   .Values.rbac.create
 }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/clusterrolebinding.yaml
@@ -1,6 +1,6 @@
 {{- if and
   .Values.hubble.enabled 
-  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  (eq (include "hubble.k8s-exporter.enabled" .) "true")
   .Values.serviceAccounts.hubbleK8sExporter.create
   .Values.rbac.create
 }}

install/kubernetes/cilium/templates/hubble-k8s-exporter/configmap.yaml
@@ -1,4 +1,7 @@
-{{- if and .Values.hubble.enabled ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )}}
+{{- if and
+  .Values.hubble.enabled
+  (eq (include "hubble.k8s-exporter.enabled" .) "true")
+}}
 apiVersion: v1
 kind: ConfigMap
 metadata:

install/kubernetes/cilium/templates/hubble-k8s-exporter/deployment.yaml
@@ -1,4 +1,7 @@
-{{- if and .Values.hubble.enabled ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )}}
+{{- if and
+  .Values.hubble.enabled
+  (eq (include "hubble.k8s-exporter.enabled" .) "true")
+}}
 apiVersion: apps/v1
 kind: Deployment
 metadata:

install/kubernetes/cilium/templates/hubble-k8s-exporter/serviceaccount.yaml
@@ -1,6 +1,6 @@
 {{- if and
   .Values.hubble.enabled 
-  ( .Values.hubble.k8sExporter.enabled | default .Values.hubble.timescape.enabled )
+  (eq (include "hubble.k8s-exporter.enabled" .) "true")
   .Values.serviceAccounts.hubbleK8sExporter.create
 }}
 apiVersion: v1

install/kubernetes/cilium/values.yaml
@@ -2104,7 +2104,7 @@ hubble:
   # The exporter is enabled by default when using integrated Hubble Timescape
   k8sExporter:
     # @schema
-    # type: [null, string]
+    # type: [null, boolean]
     # @schema
     enabled: ~
     image:

install/kubernetes/cilium/zz_enterprise_values.yaml.tmpl
@@ -313,7 +313,7 @@ hubble:
   # The exporter is enabled by default when using integrated Hubble Timescape
   k8sExporter:
     # @schema
-    # type: [null, string]
+    # type: [null, boolean]
     # @schema
     enabled: ~
     image: