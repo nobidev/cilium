a164129baf6576a3191bdbc27aa68f3c05b50884
Jussi Maki <jussi@isovalent.com>
2025-03-12 14:17:21 +0100

    enterprise/daemon: Force disable experimental LB control-plane
    In preparation for switching to the new load-balancing control-plane
    on the OSS side, disable it by force in the enterprise agent so we
    can gradually migrate enterprise code to it.
    
    Signed-off-by: Jussi Maki <jussi@isovalent.com>

--- enterprise/daemon/main.go
+++ enterprise/daemon/main.go
@@ -14,12 +14,21 @@ import (
 	"github.com/cilium/cilium/daemon/cmd"
 	_ "github.com/cilium/cilium/enterprise/fips"
 	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/loadbalancer/experimental"
 )
 
 func main() {
 	agentHive := hive.New(
 		EnterpriseAgent,
 	)
+
+	// Force disable the new load-balancing control-plane until
+	// the enterprise code has migrated over to it.
+	hive.AddConfigOverride(
+		agentHive,
+		func(cfg *experimental.Config) { cfg.EnableExperimentalLB = false },
+	)
+
 	hiveFn := func() *hive.Hive {
 		return agentHive
 	}
