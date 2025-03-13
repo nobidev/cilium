f2b33f264eb98433dd4cfd226595885b4b5b90d8
Joe Stringer <joe@isovalent.com>
2025-03-13 13:57:02 -0700

    cilium-cli: Move CurlCommandParallelWithOutput to enterprise
    This doesn't have to be enterprise, but it's currently only used by
    internal testsuites. Move the function over to a file pattern that
    matches `*enterprise*` so it flies under the radar for regular
    'core-structure' pattern checks.
    
    We could upstream this in future if we need the function in an upstream
    test, but we don't want to reintroduce it unused upstream (as then
    someone will likely come back and remove it again; this happened
    previously)
    
        $ git log --oneline --grep CurlCommandParallelWithOutput
        5ca1b39b5696 enterprise/hooks/connectivity/tests: Revert test changes
        66116d03fb51 Revert "cilium-cli: remove unused CurlCommandParallelWithOutput"
        86e5e23328cc cilium-cli: remove unused CurlCommandParallelWithOutput
        dfd23b97ec79 check: add CurlCommandParallelWithOutput helper
    
    Signed-off-by: Joe Stringer <joe@isovalent.com>

---
+++ cilium-cli/connectivity/check/enterprise.go
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package check
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	"github.com/cilium/cilium/cilium-cli/utils/features"
+)
+
+func (ct *ConnectivityTest) CurlCommandParallelWithOutput(peer TestPeer, ipFam features.IPFamily, parallel int, opts ...string) []string {
+	cmd := []string{
+		"curl", "--silent", "--fail", "--show-error",
+		"--parallel", "--parallel-immediate", "--parallel-max", fmt.Sprint(parallel),
+	}
+
+	if connectTimeout := ct.params.ConnectTimeout.Seconds(); connectTimeout > 0.0 {
+		cmd = append(cmd, "--connect-timeout", strconv.FormatFloat(connectTimeout, 'f', -1, 64))
+	}
+	if requestTimeout := ct.params.RequestTimeout.Seconds(); requestTimeout > 0.0 {
+		cmd = append(cmd, "--max-time", strconv.FormatFloat(requestTimeout, 'f', -1, 64))
+	}
+
+	cmd = append(cmd, opts...)
+	url := fmt.Sprintf("%s://%s%s",
+		peer.Scheme(),
+		net.JoinHostPort(peer.Address(ipFam), fmt.Sprint(peer.Port())),
+		peer.Path())
+
+	for i := 0; i < parallel; i++ {
+		cmd = append(cmd, url)
+	}
+
+	return cmd
+}
