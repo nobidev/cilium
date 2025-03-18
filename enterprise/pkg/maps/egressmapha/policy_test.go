fa4c50d7fa32dcddbc8231c8c0f22cac746a9a73
Julian Wiedmann <jwi@isovalent.com>
2025-03-18 13:08:06 +0200

    egressgwha: switch map tests to use v2 policy map
    Missed when introducing the v2 map.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- enterprise/pkg/maps/egressmapha/policy_test.go
+++ enterprise/pkg/maps/egressmapha/policy_test.go
@@ -29,7 +29,7 @@ func TestPolicyMap(t *testing.T) {
 	bpf.CheckOrMountFS("")
 	assert.NoError(t, rlimit.RemoveMemlock())
 
-	egressPolicyMap := createPolicyMap(hivetest.Lifecycle(t), DefaultPolicyConfig, ebpf.PinNone)
+	egressPolicyMap := createPolicyMapV2(hivetest.Lifecycle(t), DefaultPolicyConfig, ebpf.PinNone)
 
 	sourceIP1 := netip.MustParseAddr("1.1.1.1")
 	sourceIP2 := netip.MustParseAddr("1.1.1.2")
@@ -43,6 +43,9 @@ func TestPolicyMap(t *testing.T) {
 	gatewayIP1 := netip.MustParseAddr("4.4.4.1")
 	gatewayIP2 := netip.MustParseAddr("4.4.4.2")
 
+	ifIndex1 := uint32(1)
+	ifIndex2 := uint32(2)
+
 	// This will create 2 policies, respectively with 2 and 1 egress GWs:
 	//
 	// Source IP   Destination CIDR   Egress IP   Gateway
@@ -50,15 +53,15 @@ func TestPolicyMap(t *testing.T) {
 	//                                            1 => 4.4.4.2
 	// 1.1.1.2     2.2.2.0/24         3.3.3.2     0 => 4.4.4.1
 
-	err := ApplyEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1, gatewayIP2})
+	err := ApplyEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1, gatewayIP2}, ifIndex1)
 	assert.NoError(t, err)
 
-	defer RemoveEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1)
+	defer RemoveEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1)
 
-	err = ApplyEgressPolicy(egressPolicyMap, sourceIP2, destCIDR2, egressIP2, []netip.Addr{gatewayIP1})
+	err = ApplyEgressPolicyV2(egressPolicyMap, sourceIP2, destCIDR2, egressIP2, []netip.Addr{gatewayIP1}, ifIndex2)
 	assert.NoError(t, err)
 
-	defer RemoveEgressPolicy(egressPolicyMap, sourceIP2, destCIDR2)
+	defer RemoveEgressPolicyV2(egressPolicyMap, sourceIP2, destCIDR2)
 
 	val, err := egressPolicyMap.Lookup(sourceIP1, destCIDR1)
 	assert.NoError(t, err)
@@ -67,6 +70,7 @@ func TestPolicyMap(t *testing.T) {
 	assert.Equal(t, val.EgressIP.Addr(), egressIP1)
 	assert.Equal(t, val.GatewayIPs[0].Addr(), gatewayIP1)
 	assert.Equal(t, val.GatewayIPs[1].Addr(), gatewayIP2)
+	assert.Equal(t, val.EgressIfindex, ifIndex1)
 
 	val, err = egressPolicyMap.Lookup(sourceIP2, destCIDR2)
 	assert.NoError(t, err)
@@ -74,10 +78,11 @@ func TestPolicyMap(t *testing.T) {
 	assert.EqualValues(t, uint32(1), val.Size)
 	assert.Equal(t, val.EgressIP.Addr(), egressIP2)
 	assert.Equal(t, val.GatewayIPs[0].Addr(), gatewayIP1)
+	assert.Equal(t, val.EgressIfindex, ifIndex2)
 
 	// Adding a policy with too many gateways should result in an error
 	gatewayIPs := make([]netip.Addr, maxGatewayNodes+1)
-	err = ApplyEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, gatewayIPs)
+	err = ApplyEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, gatewayIPs, ifIndex1)
 	assert.ErrorContains(t, err, "cannot apply egress policy: too many gateways")
 
 	// Update the first policy:
@@ -85,23 +90,23 @@ func TestPolicyMap(t *testing.T) {
 	// - remove gatewayIP1 from the list of active gateways (by applying a
 	//   new policy with just gatewayIP2)
 	// - remove gatewayIP1 also from the list of healthy gateways
-	err = ApplyEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP2})
+	err = ApplyEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP2}, ifIndex1)
 	assert.NoError(t, err)
 
 	// Update the first policy:
 	//
 	// - change the active gateway from gatewayIP2 -> gatewayIP1
 	//-  keep gatewayIP2 in the list of healthy gateways
-	err = ApplyEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1})
+	err = ApplyEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1}, ifIndex1)
 	assert.NoError(t, err)
 
 	// Update the first policy:
 	//
 	//-  Remove gatewayIP2 from the list of healthy gateways
-	err = ApplyEgressPolicy(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1})
+	err = ApplyEgressPolicyV2(egressPolicyMap, sourceIP1, destCIDR1, egressIP1, []netip.Addr{gatewayIP1}, ifIndex1)
 	assert.NoError(t, err)
 
 	// Remove the second policy
-	err = RemoveEgressPolicy(egressPolicyMap, sourceIP2, destCIDR2)
+	err = RemoveEgressPolicyV2(egressPolicyMap, sourceIP2, destCIDR2)
 	assert.NoError(t, err)
 }
