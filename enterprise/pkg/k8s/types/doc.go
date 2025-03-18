fbe965f0821fa6d018cfc52a5feb5dfb90b54483
Tobias Klauser <tobias@isovalent.com>
2025-03-18 14:25:55 +0100

    enterprise/k8s/types: add SlimINP type
    This will be used to store
    IsovalentNetworkPolicy/IsovalentClusterwideNetworkPolicy similar to
    what types.SlimCNP does for
    CiliumNetworkPolicy/CiliumClusterwideNetworkPolicy.
    
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

---
+++ enterprise/pkg/k8s/types/doc.go
@@ -0,0 +1,22 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+// This file originates from Ciliums's codebase and is governed by an
+// Apache 2.0 license (see original header below):
+//
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+// +k8s:deepcopy-gen=package,register
+// +deepequal-gen=package
+
+// Package types contains slimmer versions of k8s types.
+// +groupName=pkg
+package types
---
+++ enterprise/pkg/k8s/types/types.go
@@ -0,0 +1,25 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+// This file originates from Ciliums's codebase and is governed by an
+// Apache 2.0 license (see original header below):
+//
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Authors of Cilium
+
+package types
+
+import "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +deepequal-gen=true
+type SlimINP struct {
+	*v1alpha1.IsovalentNetworkPolicy
+}
