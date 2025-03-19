870a53ba189751798a9051032c8aeec09255c14b
Marco Iorio <marco.iorio@isovalent.com>
2025-03-19 16:40:37 +0100

    ent/gha/scale-egw: slightly increase CPU/memory thresholds
    Tune the CPU/memory thresholds of the masquerade delay tests,
    considering that more work needs to be done to process enterprise
    policies, and that the Cilium binary is larger.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- .github/actions/set-env-variables/enterprise-scalability-overrides.sh
+++ .github/actions/set-env-variables/enterprise-scalability-overrides.sh
@@ -3,5 +3,9 @@
 echo "GCP_PERF_RESULTS_BUCKET=gs://cilium-scale-results-cee" >> "$GITHUB_ENV"
 # Memory usage is higher for CEE as we have a slightly bigger binary
 echo "CL2_MEDIAN_MEM_USAGE_THRESHOLD=270" >> "$GITHUB_ENV"
-# Create an enterprise EGW policy, rather than the OSS one
+# Create an enterprise EGW policy, rather than the OSS one.
+# Additionally tune the CPU/memory thresholds, considering that more work needs
+# to be done to process enterprise policies, and that the Cilium binary is larger.
 echo "CL2_EGW_POLICY_TEMPLATE=manifests/enterprise-egw-policy.yaml" >> "$GITHUB_ENV"
+echo "CL2_EGW_MASQ_CPU_USAGE_THRESHOLD=0.15" >> "$GITHUB_ENV"
+echo "CL2_EGW_MASQ_MEM_USAGE_THRESHOLD=290" >> "$GITHUB_ENV"
