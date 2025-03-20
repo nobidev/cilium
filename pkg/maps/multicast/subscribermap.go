nobidev/cilium

commit cce6a96909e9a88f3f5645b9c0a67a4429c9f8cf

multicast: remove unused MCAST_GROUP_OUTER_V4_MAP node define
Signed-off-by: Timo Beckers <timo@isovalent.com>

pkg/maps/multicast/subscribermap.go
@@ -93,10 +93,6 @@ type ParamsOut struct {
 func NewGroupV4Map(in ParamsIn) ParamsOut {
 	out := ParamsOut{}
 
-	out.NodeDefines = map[string]string{
-		"MCAST_GROUP_OUTER_V4_MAP": GroupOuter4MapName,
-	}
-
 	if !in.MulticastEnabled {
 		return out
 	}

commit e5dc1a5d8c84b9dafe6ba35fc912faf9f3cfe851

multicast: Fix broken NodeDefine initialization
It is broken by
https://github.com/cilium/cilium/commit/cce6a96909e9a88f3f5645b9c0a67a4429c9f8cf
as it removes the out.NodeDefines map initialization. The later
assignment fails with assign to nil map. We couldn't notice this in the
OSS since we are only testing this code path in the enterprise. We
should cover it in the OSS test.

Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

pkg/maps/multicast/subscribermap.go
@@ -105,7 +105,9 @@ func NewGroupV4Map(in ParamsIn) ParamsOut {
 		return out
 	}
 
-	out.NodeDefines["ENABLE_MULTICAST"] = "1"
+	out.NodeDefines = defines.Map{
+		"ENABLE_MULTICAST": "1",
+	}
 
 	groupMap := NewGroupV4OuterMap(GroupOuter4MapName)
 