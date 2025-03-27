0a97556dc33dc336ffd4fb67a48977346de7770e
Marco Iorio <marco.iorio@isovalent.com>
2025-03-27 10:49:40 +0100

    gha: approve the EGW feature gates when it gets enabled
    This is necessary for the egress gateway scale test, as Cilium otherwise
    fails to be installed due to the feature gates. Differently, this change
    is not expected to impact the other workflows, as in those cases the
    minimum maturity is set to alpha.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- .github/actions/set-env-variables/enterprise-overrides.sh
+++ .github/actions/set-env-variables/enterprise-overrides.sh
@@ -18,7 +18,8 @@ echo "QUAY_CHARTS_ORGANIZATION_DEV=isovalent-charts-dev" >> "$GITHUB_ENV"
 echo "QUAY_OSS_CHARTS_ORGANIZATION_DEV=cilium-charts-dev" >> $GITHUB_ENV
 echo "BRANCH_SUFFIX=-ce" >> "$GITHUB_ENV"
 echo "TAG_SUFFIX=-cee.1" >> "$GITHUB_ENV"
-echo "EGRESS_GATEWAY_HELM_VALUES=--helm-set=egressGateway.enabled=true --helm-set=enterprise.egressGatewayHA.enabled=true" >> "$GITHUB_ENV"
+echo "EGRESS_GATEWAY_HELM_VALUES=--helm-set=egressGateway.enabled=true --helm-set=enterprise.egressGatewayHA.enabled=true \
+    --helm-set=enterprise.featureGate.approved='{EgressGatewayIPv4,EgressGatewayHA}'" >> "$GITHUB_ENV"
 echo "BGP_CONTROL_PLANE_HELM_VALUES=--helm-set=enterprise.bgpControlPlane.enabled=true --helm-set=enterprise.bfd.enabled=true" >> "$GITHUB_ENV"
 
 echo "CILIUM_CLI_RELEASE_REPO=isovalent/cilium-cli-releases" >> "$GITHUB_ENV"
