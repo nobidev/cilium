aa47fcb59b7fc3a951a83e39f951149d0f0e2bb7
Julian Wiedmann <jwi@isovalent.com>
2025-04-01 17:29:16 +0100

    (cee) bpf: encrypt: avoid looking up source identity
    When we have a valid security identity for the source endpoint, avoid the
    ipcache lookup. This prevents leaks if the source endpoint has been torn
    down by the time that its packets reach the to-netdev program, and the
    ipcache lookup falls through to the catch-all entry (which is WORLD_ID,
    and thus not eligible for encryption).
    
    In this patch we either use the source identity that's extracted by
    the to-netdev program, or (for overlay traffic) the identity that's
    embedded into the skb->mark.
    
    Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

--- bpf/lib/enterprise_encrypt.h
+++ bpf/lib/enterprise_encrypt.h
@@ -63,8 +63,7 @@ encrypt_get_l4_info_ipv4(struct iphdr *ip4, __u8 *l4_proto, __u32 *l4_off)
 static __always_inline int
 encrypt_handle_vxlan_inner_packet(struct __ctx_buff __maybe_unused *ctx,
 				  void *data, void *data_end, __u32 *l4_off, __u8 *l4_proto,
-				  struct remote_endpoint_info __maybe_unused **dst,
-				  struct remote_endpoint_info __maybe_unused **src)
+				  struct remote_endpoint_info __maybe_unused **dst)
 {
 	struct ipv6hdr __maybe_unused *inner_ip6;
 	struct iphdr __maybe_unused *inner_ip4;
@@ -84,7 +83,6 @@ encrypt_handle_vxlan_inner_packet(struct __ctx_buff __maybe_unused *ctx,
 			return DROP_INVALID;
 
 		*dst = lookup_ip6_remote_endpoint((union v6addr *)&inner_ip6->daddr, 0);
-		*src = lookup_ip6_remote_endpoint((union v6addr *)&inner_ip6->saddr, 0);
 
 		*l4_proto = inner_ip6->nexthdr;
 		/* calculate offset of inner ip packet */
@@ -108,7 +106,6 @@ encrypt_handle_vxlan_inner_packet(struct __ctx_buff __maybe_unused *ctx,
 			return DROP_INVALID;
 
 		*dst = lookup_ip4_remote_endpoint(inner_ip4->daddr, 0);
-		*src = lookup_ip4_remote_endpoint(inner_ip4->saddr, 0);
 
 		*l4_proto = inner_ip4->protocol;
 
@@ -152,8 +149,7 @@ encrypt_policy_matches(struct __ctx_buff *ctx, __u8 l4_proto, __u32 l4_off,
 
 #undef host_wg_encrypt_hook
 static __always_inline int
-host_wg_encrypt_hook(struct __ctx_buff *ctx, __be16 proto,
-		     __u32 src_sec_identity __maybe_unused)
+host_wg_encrypt_hook(struct __ctx_buff *ctx, __be16 proto, __u32 src_sec_identity)
 {
 	struct remote_endpoint_info *dst = NULL;
 	struct remote_endpoint_info __maybe_unused *src = NULL;
@@ -197,7 +193,13 @@ host_wg_encrypt_hook(struct __ctx_buff *ctx, __be16 proto,
 		}
 #endif
 		dst = lookup_ip6_remote_endpoint((union v6addr *)&ip6->daddr, 0);
-		src = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
+		if (src_sec_identity == UNKNOWN_ID) {
+			src = lookup_ip6_remote_endpoint((union v6addr *)&ip6->saddr, 0);
+			if (!src)
+				return CTX_ACT_OK;
+
+			src_sec_identity = src->sec_identity;
+		}
 
 		ret = encrypt_get_l4_info_ipv6(ctx, ip6, &l4_proto, &l4_off);
 		if (ret < 0)
@@ -220,18 +222,26 @@ host_wg_encrypt_hook(struct __ctx_buff *ctx, __be16 proto,
 		 */
 		if (ctx_is_overlay(ctx)) {
 			ret = encrypt_handle_vxlan_inner_packet(ctx, data, data_end, &l4_off,
-								&l4_proto, &dst, &src);
+								&l4_proto, &dst);
 
 			if (ret < 0)
 				return ret;
 			if (ret == CTX_ACT_OK)
 				goto out;
 
+			src_sec_identity = get_identity(ctx);
 			goto maybe_encrypt;
 		}
 # endif /* HAVE_ENCAP */
 		dst = lookup_ip4_remote_endpoint(ip4->daddr, 0);
-		src = lookup_ip4_remote_endpoint(ip4->saddr, 0);
+		if (src_sec_identity == UNKNOWN_ID) {
+			src = lookup_ip4_remote_endpoint(ip4->saddr, 0);
+			if (!src)
+				return CTX_ACT_OK;
+
+			src_sec_identity = src->sec_identity;
+		}
+
 		break;
 #endif
 	default:
@@ -262,33 +272,32 @@ host_wg_encrypt_hook(struct __ctx_buff *ctx, __be16 proto,
 	 * This means that the packet won't be encrypted. This is fine,
 	 * as with --encrypt-node=false we encrypt only pod-to-pod packets.
 	 */
-	if (!src || src->sec_identity == HOST_ID)
+	if (src_sec_identity == HOST_ID)
 		goto out;
 #endif /* !ENABLE_NODE_ENCRYPTION */
 
 	/* We don't want to encrypt any traffic that originates from outside
 	 * the cluster. This check excludes DSR traffic from the LB node to a remote backend.
 	 */
-	if (!src || !identity_is_cluster(src->sec_identity))
+	if (!identity_is_cluster(src_sec_identity))
 		goto out;
 
 	/* If source is remote node we should treat it like outside traffic.
 	 * This is possible when connection is done from pod to load balancer with DSR enabled.
 	 */
-	if (identity_is_remote_node(src->sec_identity))
+	if (identity_is_remote_node(src_sec_identity))
 		goto out;
 
 maybe_encrypt: __maybe_unused
 	/* Redirect to the WireGuard tunnel device if the encryption is
 	 * required.
 	 */
-	if (src && dst &&
-	    !encrypt_policy_matches(ctx, l4_proto, l4_off, src->sec_identity, dst->sec_identity))
+	if (dst &&
+	    !encrypt_policy_matches(ctx, l4_proto, l4_off, src_sec_identity, dst->sec_identity))
 		goto out;
 
 	if (dst && dst->key) {
-		if (src)
-			set_identity_mark(ctx, src->sec_identity, MARK_MAGIC_IDENTITY);
+		set_identity_mark(ctx, src_sec_identity, MARK_MAGIC_IDENTITY);
 overlay_encrypt: __maybe_unused
 		return ctx_redirect(ctx, WG_IFINDEX, 0);
 	}
