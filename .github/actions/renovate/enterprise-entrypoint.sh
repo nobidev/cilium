01b1d408f1d792a3f135b40be1cb852dc7b67d69
Frederic Giloux <frederic.giloux@isovalent.com>
2025-04-07 15:36:23 +0200

    ci: Add authentication for Red Hat registry to renovate
    Renovate needs access to the Red Hat registry to get information
    for tracking the UBI images. Therefore a split between the
    bot configuration and the repo configuration is required.
    The bootstrap configuration needs to be stored in js file for
    the processing of environment variables.
    
    Signed-off-by: Frederic Giloux <frederic.giloux@isovalent.com>

---
+++ .github/actions/renovate/enterprise-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+apt update
+
+apt install -y gettext docker-buildx
+
+# Check if PS1 is unset; using ${PS1:-} to avoid "unbound variable" error
+# in non-interactive shells.
+# shellcheck disable=SC2016
+sed -i '7s/\[ -z "$PS1" \]/[ -z "${PS1:-}" ]/' /etc/bash.bashrc
+
+ls -lah /var/run/docker.sock
+
+# Add the group of /var/run/docker.sock to ubuntu user
+GROUP_ID=$(stat -c '%g' /var/run/docker.sock)
+GROUP_NAME=$(getent group "$GROUP_ID" | cut -d: -f1)
+
+if [ -z "$GROUP_NAME" ]; then
+  GROUP_NAME="docker_group"
+  groupadd -g "$GROUP_ID" "$GROUP_NAME"
+fi
+
+usermod -aG "$GROUP_NAME" ubuntu
+
+sed "s/{{ process.env.RH_REGISTRY_USERNAME }}/$RH_REGISTRY_USERNAME/g" "$RENOVATE_CONFIG_FILE" > /tmp/new-renovate.js
+REPL=$(sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<< "$RH_REGISTRY_PASSWORD")
+sed -i "s/{{ process.env.RH_REGISTRY_PASSWORD }}/${REPL}/g" /tmp/new-renovate.js
+cp /tmp/new-renovate.js "$RENOVATE_CONFIG_FILE"
+
+runuser -u ubuntu -w RENOVATE_CONFIG_FILE,RENOVATE_TOKEN renovate
---
+++ .github/workflows/enterprise-renovate-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  repositories: ["isovalent/cilium"],
+  username: "isovalent-renovate[bot]",
+  secrets: {
+    RH_REGISTRY_USERNAME: "{{ process.env.RH_REGISTRY_USERNAME }}",
+    RH_REGISTRY_PASSWORD: "{{ process.env.RH_REGISTRY_PASSWORD }}",
+  },
+  hostRules: [
+    {
+      matchHost: "https://registry.redhat.io",
+      hostType: "docker",
+      username: "{{ process.env.RH_REGISTRY_USERNAME }}",
+      password: "{{ process.env.RH_REGISTRY_PASSWORD }}",
+    },
+  ],
+};
