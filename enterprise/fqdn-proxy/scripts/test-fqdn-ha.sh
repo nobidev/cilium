e050e2e15de26fe10aa878aab283d9953994f76a
Dylan Reimerink <dylan.reimerink@isovalent.com>
2025-04-14 11:44:11 +0200

    enterprise-fqdn-ha: Add IPv6 testing to the FQDN HA workflow
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

--- enterprise/fqdn-proxy/scripts/test-fqdn-ha.sh
+++ enterprise/fqdn-proxy/scripts/test-fqdn-ha.sh
@@ -2,12 +2,15 @@
 
 set -eu -o pipefail
 
-kubectl exec -n cilium-test-1 deploy/client -- nslookup cilium.io
-kubectl exec -n cilium-test-1 deploy/client -- curl --max-time 10 cilium.io
+EXTERNAL_TARGET="${1:-cilium.io}"
+EXTERNAL_OTHER_TARGET="${2:-google.com}"
 
-kubectl exec -n cilium-test-1 deploy/client -- nslookup google.com
+kubectl exec -n cilium-test-1 deploy/client -- nslookup $EXTERNAL_TARGET
+kubectl exec -n cilium-test-1 deploy/client -- curl --max-time 10 $EXTERNAL_TARGET
 
-if kubectl exec -n cilium-test-1 deploy/client -- curl --max-time 10 google.com ; then
-	echo "google.com reached despite being blocked by fqdn policy"
+kubectl exec -n cilium-test-1 deploy/client -- nslookup $EXTERNAL_OTHER_TARGET
+
+if kubectl exec -n cilium-test-1 deploy/client -- curl --max-time 10 $EXTERNAL_OTHER_TARGET ; then
+	echo "$EXTERNAL_OTHER_TARGET reached despite being blocked by fqdn policy"
 	exit 1
 fi
