nobidev/cilium

commit 8eea691a437f57ea679de7b73dfbfac5fd42939c

hubble-timescape: Update default aggregation to not renew the ttl
We were a bit too aggressive with the default aggregation. Without this,
we can miss long standing active connections, as we'll only export the
flow once. Then if we see a flow for that connection at least every
30s, we don't export the flow again as the TTL will be renewed every
time.

This change disables the TTL renew. So we'll emit one flow every 30s for
active connections.

Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

install/kubernetes/cilium/templates/_enterprise.tpl
@@ -85,6 +85,7 @@ auto-create-default-pod-network: {{ .Values.enterprise.multiNetwork.autoCreateDe
 {{- $defaultExportFilePath := "" }}
 {{- $defaultExportAggregation := "" }}
 {{- $defaultExportAggregationStateFilter := "" }}
+{{- $defaulthubbleExportAggregationRenewTTL := "true" }}
 
 # For cilium version <1.16 we enable export to /var/run/cilium/hubble by
 # default.
@@ -99,6 +100,7 @@ auto-create-default-pod-network: {{ .Values.enterprise.multiNetwork.autoCreateDe
 {{- $defaultExportFilePath = "/var/run/cilium/hubble/hubble.log"}}
 {{- $defaultExportAggregation = "connection" }}
 {{- $defaultExportAggregationStateFilter = "new error" }}
+{{- $defaulthubbleExportAggregationRenewTTL = "false" }}
 {{- end }}
 
 {{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "export-file-path"))}}
@@ -109,6 +111,9 @@ export-aggregation: {{ $defaultExportAggregation | quote }}
 {{- end }}
 {{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "export-aggregation-state-filter"))}}
 export-aggregation-state-filter: {{ $defaultExportAggregationStateFilter | quote }}
+{{- end }}
+{{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "hubble-export-aggregation-renew-ttl"))}}
+hubble-export-aggregation-renew-ttl: {{ $defaulthubbleExportAggregationRenewTTL | quote }}
 {{- end }}
 
 {{- if .Values.hubble.export }}

commit 2a25bc6f79195c7793dbc1422f0d806dfbbf6c55

hubble-timescape: fix wrong export config
I accidentally set the new export setting, but we still use the old
export settings for timescape.

Fixes: 8eea691a437f ("hubble-timescape: Update default aggregation to not renew the ttl")

Signed-off-by: Fabian Fischer <fabian.fischer@isovalent.com>

install/kubernetes/cilium/templates/_enterprise.tpl
@@ -85,7 +85,7 @@ auto-create-default-pod-network: {{ .Values.enterprise.multiNetwork.autoCreateDe
 {{- $defaultExportFilePath := "" }}
 {{- $defaultExportAggregation := "" }}
 {{- $defaultExportAggregationStateFilter := "" }}
-{{- $defaulthubbleExportAggregationRenewTTL := "true" }}
+{{- $defaultExportAggregationRenewTTL := "true" }}
 
 # For cilium version <1.16 we enable export to /var/run/cilium/hubble by
 # default.
@@ -100,7 +100,7 @@ auto-create-default-pod-network: {{ .Values.enterprise.multiNetwork.autoCreateDe
 {{- $defaultExportFilePath = "/var/run/cilium/hubble/hubble.log"}}
 {{- $defaultExportAggregation = "connection" }}
 {{- $defaultExportAggregationStateFilter = "new error" }}
-{{- $defaulthubbleExportAggregationRenewTTL = "false" }}
+{{- $defaultExportAggregationRenewTTL = "false" }}
 {{- end }}
 
 {{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "export-file-path"))}}
@@ -112,8 +112,8 @@ export-aggregation: {{ $defaultExportAggregation | quote }}
 {{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "export-aggregation-state-filter"))}}
 export-aggregation-state-filter: {{ $defaultExportAggregationStateFilter | quote }}
 {{- end }}
-{{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "hubble-export-aggregation-renew-ttl"))}}
-hubble-export-aggregation-renew-ttl: {{ $defaulthubbleExportAggregationRenewTTL | quote }}
+{{- if or (not .Values.extraConfig) (not (hasKey .Values.extraConfig "export-aggregation-renew-ttl"))}}
+export-aggregation-renew-ttl: {{ $defaultExportAggregationRenewTTL | quote }}
 {{- end }}
 
 {{- if .Values.hubble.export }}