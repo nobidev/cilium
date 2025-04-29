c4a41e22f9de030658fb36a8da90dc8aade957dc
Joe Stringer <joe@isovalent.com>
2025-04-28 17:15:34 -0700

    contrib/mindful: Ignore multicast conflicts
    Signed-off-by: Joe Stringer <joe@isovalent.com>

--- contrib/scripts/enterprise-meditation.sh
+++ contrib/scripts/enterprise-meditation.sh
@@ -72,6 +72,7 @@ DIFF_NOISY_GLOBS=(
   ':!contrib/scripts/k8s-code-gen.sh' # Generate Enterprise k8s objects
   ':!pkg/k8s/apis/cell.go' # Register Enterprise CRDs
   ':!pkg/k8s/synced/crd.go' # Register Enterprise CRDs
+  ':!pkg/maps/multicast/subscribermap.go' # https://github.com/isovalent/cilium/issues/7111
 )
 
 # BPF_NOISY_GLOBS is an array, like the ones defined above, which filters out
