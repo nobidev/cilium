390ebd33176fa683d5ded609ba89f688930e07eb
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-05-14 08:18:05 +0200

    ilb: split TCPProxy E2E test into subtests
    This commit splits the TCPProxy E2E test into subtests
    for t1-only, t1&t2 and auto.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- cilium-cli/enterprise/hooks/cli/ilb/lb_tcp_proxy.go
+++ cilium-cli/enterprise/hooks/cli/ilb/lb_tcp_proxy.go
@@ -16,57 +16,67 @@ import (
 	isovalentv1alpha1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
 )
 
-func TestTCPProxy(t T) {
+func TestTCPProxyT1Only(t T) {
+	testTCPProxy(t, isovalentv1alpha1.LBTCPProxyForceDeploymentModeT1)
+}
+
+func TestTCPProxyT1T2(t T) {
+	testTCPProxy(t, isovalentv1alpha1.LBTCPProxyForceDeploymentModeT2)
+}
+
+func TestTCPProxyAuto(t T) {
+	testTCPProxy(t, isovalentv1alpha1.LBTCPProxyForceDeploymentModeAuto)
+}
+
+func testTCPProxy(t T, mode isovalentv1alpha1.LBTCPProxyForceDeploymentModeType) {
 	ciliumCli, k8sCli := NewCiliumAndK8sCli(t)
 	dockerCli := NewDockerCli(t)
 
 	testK8sNamespace := "default"
 
-	for _, mode := range []isovalentv1alpha1.LBTCPProxyForceDeploymentModeType{isovalentv1alpha1.LBTCPProxyForceDeploymentModeT1, isovalentv1alpha1.LBTCPProxyForceDeploymentModeT2, isovalentv1alpha1.LBTCPProxyForceDeploymentModeAuto} {
-		t.RunTestCase(func(t T) {
-			testName := "tcp-proxy-" + string(mode)
+	t.RunTestCase(func(t T) {
+		testName := "tcp-proxy-" + string(mode)
 
-			// 0. Setup test scenario (backends, clients & LB resources)
-			scenario := newLBTestScenario(t, testName, testK8sNamespace, ciliumCli, k8sCli, dockerCli)
+		// 0. Setup test scenario (backends, clients & LB resources)
+		scenario := newLBTestScenario(t, testName, testK8sNamespace, ciliumCli, k8sCli, dockerCli)
 
-			t.Log("Creating backend apps...")
+		t.Log("Creating backend apps...")
 
-			backendNum := 2
-			// TCPProxy does not support backends with different ports, so create just 1 backend.
-			if IsSingleNode() {
-				backendNum = 1
-			}
-			scenario.addBackendApplications(backendNum, backendApplicationConfig{h2cEnabled: true})
+		backendNum := 2
+		// TCPProxy does not support backends with different ports, so create just 1 backend.
+		if IsSingleNode() {
+			backendNum = 1
+		}
+		scenario.addBackendApplications(backendNum, backendApplicationConfig{h2cEnabled: true})
 
-			t.Log("Creating clients and add BGP peering ...")
-			client := scenario.addFRRClients(1, frrClientConfig{})[0]
+		t.Log("Creating clients and add BGP peering ...")
+		client := scenario.addFRRClients(1, frrClientConfig{})[0]
 
-			t.Log("Creating LB VIP resources...")
-			vip := lbVIP(testK8sNamespace, testName)
-			scenario.createLBVIP(vip)
+		t.Log("Creating LB VIP resources...")
+		vip := lbVIP(testK8sNamespace, testName)
+		scenario.createLBVIP(vip)
 
-			t.Log("Creating LB BackendPool resources...")
-			backends := []backendPoolOption{}
-			for _, b := range scenario.backendApps {
-				backends = append(backends, withIPBackend(b.ip, b.port))
-			}
-			backendPool := lbBackendPool(testK8sNamespace, testName, backends...)
-			scenario.createLBBackendPool(backendPool)
+		t.Log("Creating LB BackendPool resources...")
+		backends := []backendPoolOption{}
+		for _, b := range scenario.backendApps {
+			backends = append(backends, withIPBackend(b.ip, b.port))
+		}
+		backendPool := lbBackendPool(testK8sNamespace, testName, backends...)
+		scenario.createLBBackendPool(backendPool)
 
-			t.Log("Creating LB Service resources...")
-			service := lbService(testK8sNamespace, testName, withPort(80), withTCPProxyApplication(withTCPForceDeploymentMode(mode), withTCPProxyRoute(backendPool.Name)))
-			scenario.createLBService(service)
+		t.Log("Creating LB Service resources...")
+		service := lbService(testK8sNamespace, testName, withPort(80), withTCPProxyApplication(withTCPForceDeploymentMode(mode), withTCPProxyRoute(backendPool.Name)))
+		scenario.createLBService(service)
 
-			t.Log("Waiting for full VIP connectivity...")
-			vipIP := scenario.waitForFullVIPConnectivity(testName)
+		t.Log("Waiting for full VIP connectivity...")
+		vipIP := scenario.waitForFullVIPConnectivity(testName)
 
-			// 1. Send HTTP request to test basic client -> LB T1 -> LB T2 -> app connectivity
-			testCmd := curlCmdVerbose(fmt.Sprintf("--max-time 10 http://%s:80/", vipIP))
-			t.Log("Testing %q...", testCmd)
-			stdout, stderr, err := client.Exec(t.Context(), testCmd)
-			if err != nil {
-				t.Failedf("curl failed (cmd: %q, stdout: %q, stderr: %q): %s", testCmd, stdout, stderr, err)
-			}
-		})
-	}
+		// 1. Send HTTP request to test basic client -> LB T1 -> LB T2 -> app connectivity
+		testCmd := curlCmdVerbose(fmt.Sprintf("--max-time 10 http://%s:80/", vipIP))
+		t.Log("Testing %q...", testCmd)
+		stdout, stderr, err := client.Exec(t.Context(), testCmd)
+		if err != nil {
+			t.Failedf("curl failed (cmd: %q, stdout: %q, stderr: %q): %s", testCmd, stdout, stderr, err)
+		}
+	})
 }
