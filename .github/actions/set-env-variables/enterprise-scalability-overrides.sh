fc306ab879ffab5f22f353f9a62c8ca8453a8f14
Tom Hadlaw <tom.hadlaw@isovalent.com>
2025-07-24 11:06:28 -0700

    .github: enterprise overrides: bump egwha memory threshold.
    We've been seeing this fail consistently since the statedb
    AgentPolicyConfig changes where introduced.
    
    Previous commits did some optimization to try to lower
    the impact of these changes, however we probably still need
    to bump these thresholds a bit to have the scale tests pass
    consistently:
    
    * CPU -> .15 -> 17: This one was very close, only falling
    	outside our target range by around about ~.005 (i.e.
    	.5% over the threshold). Lets bump this to ~17% CPU
    	utilization to cover the changes safely.
    
    * Memory: More impactful change, previous commits decreased
    	memory usage a bit during reconciliation but we're
    	still seeing about a 10MB increase (about 3%) in most
    	scale test runs so lets bump this to 320 to be safe for
    	now.
    
    Signed-off-by: Tom Hadlaw <tom.hadlaw@isovalent.com>

--- .github/actions/set-env-variables/enterprise-scalability-overrides.sh
+++ .github/actions/set-env-variables/enterprise-scalability-overrides.sh
@@ -7,5 +7,5 @@ echo "CL2_MEDIAN_MEM_USAGE_THRESHOLD=270" >> "$GITHUB_ENV"
 # Additionally tune the CPU/memory thresholds, considering that more work needs
 # to be done to process enterprise policies, and that the Cilium binary is larger.
 echo "CL2_EGW_POLICY_TEMPLATE=manifests/enterprise-egw-policy.yaml" >> "$GITHUB_ENV"
-echo "CL2_EGW_MASQ_CPU_USAGE_THRESHOLD=0.15" >> "$GITHUB_ENV"
-echo "CL2_EGW_MASQ_MEM_USAGE_THRESHOLD=290" >> "$GITHUB_ENV"
+echo "CL2_EGW_MASQ_CPU_USAGE_THRESHOLD=0.17" >> "$GITHUB_ENV"
+echo "CL2_EGW_MASQ_MEM_USAGE_THRESHOLD=320" >> "$GITHUB_ENV"
