c363015295642aa3d55e036f94c97a2908e2e232
Marco Hofstetter <marco.hofstetter@isovalent.com>
2025-08-18 07:35:16 +0200

    ilb: prevent load on api server due to node change events
    ILB controlplane watches Node resources via `NodeSource` that listenes for changes
    on the Node resource and translates them into controller-runtime events. Currently,
    every Resource event is forwarded as controller-runtime event.
    
    Due to the fact that Node resources can change quiet frequently (~5s), this can trigger
    a lot of `LBService` reconciliations which eventually can lead to a lot of load on the
    API server (trying to update T2 `CiliumEnvoyConfig` & T1 K8s `Service`).
    
    All of this isn't really necessary because the ILB controlplane is actually only
    interested in the labels and addresses of a Node. Therefore, this commit modifies the
    `ciliumNodeSource` to keep track of the addresses per Node and only forwards the events as
    controller-runtime events in case of any changes - which is either on a new or deleted Node
    or when an address/label of an existing Node changes.
    
    Signed-off-by: Marco Hofstetter <marco.hofstetter@isovalent.com>

--- enterprise/operator/pkg/lb/node_source.go
+++ enterprise/operator/pkg/lb/node_source.go
@@ -12,6 +12,9 @@ package lb
 
 import (
 	"context"
+	"log/slog"
+	"maps"
+	"slices"
 
 	"github.com/cilium/hive/job"
 	"sigs.k8s.io/controller-runtime/pkg/event"
@@ -20,6 +23,7 @@ import (
 
 	"github.com/cilium/cilium/pkg/k8s/resource"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/logging/logfields"
 )
 
 // ciliumNodeSource implements controller-runtime' source.Source and bridges between
@@ -28,10 +32,15 @@ import (
 type ciliumNodeSource struct {
 	resource.Resource[*slim_corev1.Node]
 
+	logger *slog.Logger
+
+	synced bool
+	nodes  map[string]*node
+
 	nodeEvents chan event.GenericEvent
 }
 
-func newNodeSource(config Config, jobGroup job.Group, nodeResource resource.Resource[*slim_corev1.Node]) *ciliumNodeSource {
+func newNodeSource(logger *slog.Logger, config Config, jobGroup job.Group, nodeResource resource.Resource[*slim_corev1.Node]) *ciliumNodeSource {
 	if !config.LoadBalancerCPEnabled {
 		return nil
 	}
@@ -41,6 +50,8 @@ func newNodeSource(config Config, jobGroup job.Group, nodeResource resource.Reso
 	ciliumNodeSource := &ciliumNodeSource{
 		Resource: nodeResource,
 
+		logger:     logger,
+		nodes:      map[string]*node{},
 		nodeEvents: nodeEvents,
 	}
 
@@ -51,10 +62,54 @@ func newNodeSource(config Config, jobGroup job.Group, nodeResource resource.Reso
 
 func (r *ciliumNodeSource) HandleEvent(ctx context.Context, ev resource.Event[*slim_corev1.Node]) error {
 	defer ev.Done(nil)
-	r.nodeEvents <- event.GenericEvent{Object: ev.Object}
+
+	if !r.synced && ev.Kind != resource.Sync {
+		r.logger.Debug("Skipping event - not yet synced")
+		r.nodes[ev.Key.Name] = &node{
+			name:      ev.Key.Name,
+			addresses: ev.Object.Status.Addresses,
+		}
+		return nil
+	}
+
+	if ev.Kind == resource.Sync {
+		r.logger.Debug("Node sync event received - forwarding node event")
+		r.nodeEvents <- event.GenericEvent{Object: ev.Object}
+		r.synced = true
+		return nil
+	}
+
+	if ev.Kind == resource.Delete {
+		r.logger.Debug("Node delete event received - forwarding node event", logfields.NodeName, ev.Key.Name)
+		r.nodeEvents <- event.GenericEvent{Object: ev.Object}
+		delete(r.nodes, ev.Object.Name)
+		return nil
+	}
+
+	// handle upserts -> only trigger when added or addresses or labels changed
+	n, exists := r.nodes[ev.Key.Name]
+
+	if !exists || !slices.Equal(n.addresses, ev.Object.Status.Addresses) || !maps.Equal(n.labels, ev.Object.Labels) {
+		r.logger.Debug("Node upsert event received with changed relevant data - forwarding node event", logfields.NodeName, ev.Key.Name)
+		r.nodes[ev.Key.Name] = &node{
+			name:      ev.Key.Name,
+			labels:    ev.Object.Labels,
+			addresses: ev.Object.Status.Addresses,
+		}
+		r.nodeEvents <- event.GenericEvent{Object: ev.Object}
+		return nil
+	}
+
+	r.logger.Debug("Node upsert event received without changed relevant data - skip forwarding node event", logfields.NodeName, ev.Key.Name)
 	return nil
 }
 
 func (r *ciliumNodeSource) ToSource(handler handler.EventHandler) source.Source {
 	return source.Channel(r.nodeEvents, handler)
+}
+
+type node struct {
+	name      string
+	labels    map[string]string
+	addresses []slim_corev1.NodeAddress
 }
