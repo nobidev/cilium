a91f0bc900d4324a976bf12110008d0d06654d23
Casey Callendrello <cdc@isovalent.com>
2025-09-03 18:07:51 +0200

    fqdnha: proxy: implement bpf ipcache state management
    - react to agent and proxy state changes
    - reload the ipcache as necessary
    - block writes to the ipcache when agent and proxy are up and running
    
    Signed-off-by: Casey Callendrello <cdc@isovalent.com>

--- enterprise/fqdn-proxy/bpf_ipcache.go
+++ enterprise/fqdn-proxy/bpf_ipcache.go
@@ -15,10 +15,18 @@ import (
 	"log/slog"
 	"net/netip"
 
+	"github.com/cilium/cilium/enterprise/fqdn-proxy/api/v1/dnsproxy"
+	"github.com/cilium/cilium/enterprise/pkg/fqdnha/tables"
 	"github.com/cilium/cilium/pkg/identity"
+	"github.com/cilium/cilium/pkg/lock"
 	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/maps/ipcache"
+	"github.com/cilium/cilium/pkg/metrics"
+	"github.com/cilium/cilium/pkg/metrics/metric"
 )
+
+var ErrIPCacheInvalid = fmt.Errorf("agent IPCache map invalid")
+var ErrWriteDisabled = fmt.Errorf("bpf IPCache writes disabled")
 
 type bpfIPCache interface {
 	lookup(netip.Addr) (identity.NumericIdentity, error)
@@ -27,21 +35,135 @@ type bpfIPCache interface {
 
 type bpfIPC struct {
 	logger *slog.Logger
+	sm     *stateManager
+
+	// mapsLock locks access to ipc, start times, and allowWrite
+	mapsLock lock.RWMutex
+	ipc      *ipcache.Map
+	// start time of current and future map
+	// used to detect agent reloads
+	curStartTime, newStartTime int64
+	// whether or not we're allowed to write to the map
+	allowWrite bool
 }
 
-func newBPFIPCache(logger *slog.Logger) bpfIPCache {
-	return &bpfIPC{
+func newBPFIPCache(logger *slog.Logger, sm *stateManager, config Config, reg *metrics.Registry) bpfIPCache {
+	if !config.EnableOfflineMode {
+		return nil
+	}
+
+	b := &bpfIPC{
 		logger: logger,
+		sm:     sm,
+	}
+
+	sm.addOnUpdate(b.onStateChange)
+
+	// We do care about BPF writes and reads
+	metrics.BPFMapOps = metric.NewCounterVec(metric.CounterOpts{
+		ConfigName: metrics.Namespace + "_" + metrics.SubsystemBPF + "_map_ops_total",
+		Namespace:  metrics.Namespace,
+		Subsystem:  metrics.SubsystemBPF,
+		Name:       "map_ops_total",
+		Help:       "Total operations on map, tagged by map name",
+	}, []string{metrics.LabelMapName, metrics.LabelOperation, metrics.LabelOutcome})
+	reg.Register(metrics.BPFMapOps)
+
+	return b
+}
+
+// syncState manages reopening BPF maps as the agent and proxy
+// undergo state transitions.
+//
+// This is called **synchronously** as part of commits to the state table.
+//
+// The basic state machine:
+//   - Once agent transitions from down to any up state, note it's start time
+//   - Once proxy has transitioned to state RPS_LIVE, re-open the bpf map
+//     if the start time has changed
+func (b *bpfIPC) onStateChange(agent tables.AgentState, proxy tables.RemoteProxyState) {
+	b.mapsLock.Lock()
+	defer b.mapsLock.Unlock()
+
+	// Did the agent's start time change? Record it, so we can prepare to reopen.
+	//
+	// We don't re-open immediately, as the proxy continues to write to the "old" BPF map
+	// as well as forwarding to the agent until all endpoints have been regenerated.
+	//
+	// Additionally, handle the case when the map in question has been renamed and thus
+	// is not compatible with this version.
+	if agent.Status != dnsproxy.AgentStatus_AS_UNSPECIFIED && b.newStartTime != agent.StartTime {
+		if agent.IPCacheMapName != ipcache.Name {
+			b.logger.Warn("Cannot use IPCache BPF map: agent has incompatible map name (version skew)!",
+				logfields.BPFMapName, agent.IPCacheMapName,
+				logfields.Want, ipcache.Name)
+			b.newStartTime = 0
+		} else {
+			b.newStartTime = agent.StartTime
+		}
+	}
+
+	// Only permit writing to the ipcache if the agent is down
+	newAllowWrite := (proxy.Status != dnsproxy.RemoteProxyStatus_RPS_LIVE)
+	if newAllowWrite != b.allowWrite {
+		if newAllowWrite {
+			b.logger.Info("Agent is down, enabling BPF IPCache writing")
+		} else {
+			b.logger.Info("Agent handback is complete, disabling BPF IPCache writing")
+		}
+		b.allowWrite = newAllowWrite
+	}
+
+	// Reload the ipcache if the startTime has changed **and** it has gone live
+	if proxy.Status == dnsproxy.RemoteProxyStatus_RPS_LIVE && b.curStartTime != b.newStartTime {
+		// Close the previous ipcache handle
+		if b.ipc != nil {
+			err := b.ipc.Close()
+			if err != nil {
+				b.logger.Warn("Error closing old BPF IPCache",
+					logfields.StartTime, b.curStartTime,
+					logfields.Error, err)
+			} else {
+				b.logger.Info("Closed old BPF IPcache",
+					logfields.StartTime, b.curStartTime)
+			}
+			b.ipc = nil
+		}
+
+		// open the new ipcache (if allowed)
+		b.curStartTime = b.newStartTime
+		if b.curStartTime != 0 {
+			b.logger.Info("Proxy is RPS_LIVE, switching to new BPF IPCache map",
+				logfields.StartTime, b.newStartTime)
+			b.ipc = ipcache.NewMap(nil, ipcache.Name)
+			err := b.ipc.Open()
+			if err != nil {
+				b.logger.Error("Failed to open bpf IPCache map",
+					logfields.Error, err,
+					logfields.StartTime, b.curStartTime)
+				b.ipc = nil
+				b.curStartTime = 0
+			}
+		} else {
+			b.logger.Warn("No open bpf IPCache map. Offline mode will not work!")
+		}
 	}
 }
 
 // looks up the identity for a the given IP address from the BPF ipcache map.
 func (b *bpfIPC) lookup(addr netip.Addr) (identity.NumericIdentity, error) {
+	b.mapsLock.RLock()
+	defer b.mapsLock.RUnlock()
+
+	// If we don't have a working ipcache, return.
+	if b.ipc == nil {
+		return 0, ErrIPCacheInvalid
+	}
+
 	b.logger.Debug("BPF ipcache lookup", logfields.Address, addr)
 
 	key := ipcache.NewKey(addr.Unmap().AsSlice(), nil, 0)
-	// TODO: Add IPCacheMap reload logic, allow to specify name.
-	val, err := ipcache.IPCacheMap(nil).Lookup(&key)
+	val, err := b.ipc.Lookup(&key)
 	if err != nil {
 		return identity.NumericIdentity(0), err
 	}
@@ -60,9 +182,18 @@ func (b *bpfIPC) write(addr netip.Addr, identity identity.NumericIdentity) error
 		logfields.Identity, identity,
 	)
 
+	b.mapsLock.RLock()
+	defer b.mapsLock.RUnlock()
+	if b.ipc == nil {
+		return ErrIPCacheInvalid
+	}
+	if !b.allowWrite {
+		return ErrWriteDisabled
+	}
+
 	key := ipcache.NewKey(addr.Unmap().AsSlice(), nil, 0)
 	val := ipcache.RemoteEndpointInfo{
 		SecurityIdentity: uint32(identity),
 	}
-	return ipcache.IPCacheMap(nil).Update(&key, &val)
+	return b.ipc.Update(&key, &val)
 }
