6f335ac8e4a4dd4dc9aac6178ab7003e9fbc4a07
Sebastian Wicki <sebastian@isovalent.com>
2025-09-17 13:55:22 +0200

    privnet: Add types package
    This commit adds a types package to pkg/privnet. This types package is
    intended to be imported by different packages and is designed to contain
    a small set of dependencies. It currently defines the pod annotation
    name and schema used by PrivNet. These types will also be imported by
    various packages later.
    
    Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
    Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
    Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
    Co-authored-by: Marco Iorio <marco.iorio@isovalent.com>
    Co-authored-by: Tobias Klauser <tobias@isovalent.com>
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

---
+++ enterprise/pkg/privnet/types/k8s.go
@@ -0,0 +1,66 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package types
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/netip"
+	"strconv"
+
+	"github.com/cilium/cilium/pkg/mac"
+)
+
+const (
+	// PrivateNetworkAnnotation is the name of the annotation to attach pods to a particular
+	// private network.
+	PrivateNetworkAnnotation = "network.v1alpha1.isovalent.com/network-attachment"
+
+	// PrivateNetworkInactiveAnnotation is the name of the annotation which marks the pod inactive.
+	PrivateNetworkInactiveAnnotation = "network.v1alpha1.isovalent.com/inactive"
+)
+
+// NetworkAttachment is the value of PrivateNetworkAnnotation (encoded as JSON)
+type NetworkAttachment struct {
+	Network string     `json:"network"`
+	IPv4    netip.Addr `json:"ipv4,omitzero"`
+	IPv6    netip.Addr `json:"ipv6,omitzero"`
+	MAC     mac.MAC    `json:"mac,omitempty"`
+}
+
+func ExtractNetworkAttachmentAnnotation(annotations map[string]string) (*NetworkAttachment, error) {
+	raw, found := annotations[PrivateNetworkAnnotation]
+	if !found {
+		return nil, nil // not found
+	}
+
+	attachment := &NetworkAttachment{}
+	err := json.Unmarshal([]byte(raw), attachment)
+	if err != nil {
+		return nil, fmt.Errorf("invalid value in %q annotation: %w", PrivateNetworkAnnotation, err)
+	}
+
+	return attachment, nil
+}
+
+func ExtractInactiveAnnotation(annotations map[string]string) (inactive bool, err error) {
+	raw, found := annotations[PrivateNetworkInactiveAnnotation]
+	if !found {
+		return false, nil // not found means endpoint is active
+	}
+
+	inactive, err = strconv.ParseBool(raw)
+	if err != nil {
+		return false, fmt.Errorf("invalid value in %q annotation: %w", PrivateNetworkInactiveAnnotation, err)
+	}
+
+	return inactive, nil
+}
