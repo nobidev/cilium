cbc056cc5e05f651414fc091cd49b9f65ef7b3da
Antony Reynaud <antony.reynaud@isovalent.com>
2025-09-19 15:15:47 +0200

    ci: fix svic tag for aws cluster
    svic tool checking for falco and osquery tags run once a day. Since
    cloudstack can take a lot of time to create the clusters/nodegroups,
    svic tool scanning can happen in between the creation and tagging of
    resources.
    
    This commit change the strategy of svic tagging, now
    tags are created at the creation of cluster/nodegroups and
    svic scanning tool will no longer encounter this issue.
    
    Signed-off-by: Antony Reynaud <antony.reynaud@isovalent.com>

--- contrib/scripts/enterprise-meditation.sh
+++ contrib/scripts/enterprise-meditation.sh
@@ -37,6 +37,7 @@ DIFF_EXCL_GLOBS=(
   ':!.github/actionlint.yaml' # We use hosted runners in Enterprise
   ':!.github/ISSUE_TEMPLATE/*' # OSS updates these with renovate
   ':!.github/actions/*/k8s-versions.yaml' # K8s versions often diverge
+  ':!.github/actions/setup-eks-cluster/action.yml' # Enterprise infra tagging for security purposes
   ':!.github/actions/setup-eks-nodegroup/action.yml' # Enterprise infra tagging for security purposes
   ':!.github/actions/setup-aks-cluster/action.yml' # Enterprise infra tagging for security purposes
   ':!.github/pull_request_template.md' # PR templates can differ
