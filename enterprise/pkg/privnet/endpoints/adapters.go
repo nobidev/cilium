nobidev/cilium

commit afabc85bd07a72e7204c7bbc13ebc755f456def0

privnet: Add mock implementations for the endpoint infrastructure
This commit prepares the privnet package for better script testing. This
is achieved by defining a set of abstract endpoint interfaces and
implementing them two times:

The first implementation in adapters.go implements these interfaces on
to of the real endpoint manager and endpoint api.

The second implementation in mock_endpoints.go implements these
interfaces as a mock implementation to be used in script tests. The mock
implementation also simulates the endpoint restoration fences and allows
interaction with the mock endpoint manager via script commands.

The mock implementation is injected in tests with `cell.DecorateAll`,
this will cause Hive to ignore the implementation in adapters.go.

As part of this restructuring, the reconcilers package also gets its
own cell.

Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
Co-authored-by: Marco Iorio <marco.iorio@isovalent.com>
Co-authored-by: Tobias Klauser <tobias@isovalent.com>
Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

enterprise/pkg/privnet/endpoints/adapters.go
@@ -1,0 +1,108 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package endpoints
+
+import (
+	"context"
+	"errors"
+	"iter"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/endpoint"
+	endpointapi "github.com/cilium/cilium/pkg/endpoint/api"
+	"github.com/cilium/cilium/pkg/endpointmanager"
+)
+
+// newEndpointAPIManagerAdapter creates a new endpointAPIManagerAdapter
+func newEndpointAPIManagerAdapter(epam endpointapi.EndpointAPIManager) EndpointCreator {
+	return endpointAPIManagerAdapter{
+		epam: epam,
+	}
+}
+
+// endpointAPIManagerAdapter implements EndpointCreator on top of endpointapi.EndpointAPIManager
+type endpointAPIManagerAdapter struct {
+	epam endpointapi.EndpointAPIManager
+}
+
+// CreateEndpoint implements EndpointCreator.
+func (e endpointAPIManagerAdapter) CreateEndpoint(ctx context.Context, epTemplate *models.EndpointChangeRequest) (Endpoint, error) {
+	ep, _, err := e.epam.CreateEndpoint(ctx, epTemplate)
+	return ep, err
+}
+
+// newEndpointManagerAdapter creates a new endpointManagerAdapter
+func newEndpointManagerAdapter(epm endpointmanager.EndpointManager) (EndpointGetter, EndpointRemover) {
+	a := endpointManagerAdapter{
+		epm: epm,
+	}
+	return a, a
+}
+
+// endpointManagerAdapter implements EndpointGetter and EndpointRemover on top of endpointmanager.EndpointManager
+type endpointManagerAdapter struct {
+	epm endpointmanager.EndpointManager
+}
+
+// RemoveByCEPName implements EndpointRemover.
+func (e endpointManagerAdapter) RemoveByCEPName(nsname string) (Endpoint, error) {
+	ep := e.epm.LookupCEPName(nsname)
+	if ep == nil {
+		return nil, nil
+	}
+	return ep, errors.Join(e.epm.RemoveEndpoint(ep, endpoint.DeleteConfig{
+		NoIPRelease: ep.DatapathConfiguration.ExternalIpam,
+	})...)
+}
+
+// LookupCEPName implements EndpointGetter.
+func (e endpointManagerAdapter) LookupCEPName(nsname string) Endpoint {
+	return e.epm.LookupCEPName(nsname)
+}
+
+// GetEndpointsByPodName implements EndpointGetter.
+func (e endpointManagerAdapter) GetEndpointsByPodName(nsname string) iter.Seq[Endpoint] {
+	return func(yield func(Endpoint) bool) {
+		eps := e.epm.GetEndpointsByPodName(nsname)
+		for _, ep := range eps {
+			if !yield(ep) {
+				break
+			}
+		}
+	}
+}
+
+// Subscribe implements EndpointGetter.
+func (e endpointManagerAdapter) Subscribe(s EndpointSubscriber) {
+	e.epm.Subscribe(endpointSubscriberAdapter{
+		s: s,
+	})
+}
+
+// endpointSubscriberAdapter implements EndpointSubscriber on top of endpointmanager.Subscriber
+type endpointSubscriberAdapter struct {
+	s EndpointSubscriber
+}
+
+// EndpointCreated implements endpointmanager.Subscriber.
+func (e endpointSubscriberAdapter) EndpointCreated(ep *endpoint.Endpoint) {
+	e.s.EndpointCreated(ep)
+}
+
+// EndpointDeleted implements endpointmanager.Subscriber.
+func (e endpointSubscriberAdapter) EndpointDeleted(ep *endpoint.Endpoint, conf endpoint.DeleteConfig) {
+	e.s.EndpointDeleted(ep)
+}
+
+// EndpointRestored implements endpointmanager.Subscriber.
+func (e endpointSubscriberAdapter) EndpointRestored(ep *endpoint.Endpoint) {
+	e.s.EndpointRestored(ep)
+}

enterprise/pkg/privnet/endpoints/cell.go
@@ -8,21 +8,18 @@
 //  or reproduction of this material is strictly forbidden unless prior written
 //  permission is obtained from Isovalent Inc.
 
-package privnet
+package endpoints
 
-import (
-	"github.com/cilium/hive/cell"
+import "github.com/cilium/hive/cell"
 
-	pncfg "github.com/cilium/cilium/enterprise/pkg/privnet/config"
-	"github.com/cilium/cilium/enterprise/pkg/privnet/endpoints"
-	"github.com/cilium/cilium/enterprise/pkg/privnet/reconcilers"
-)
-
-var Cell = cell.Module(
-	"private-networks",
-	"Support for Private Networks",
-
-	pncfg.Cell,
-	reconcilers.Cell,
-	endpoints.Cell,
+// Cell provides the various interfaces defined in this package, implemented as adapters
+// on top of the upstream pkg/endpoint and pkg/endpointmanager packages.
+// The types provided by this cell are usually overwritten via `cell.DecorateAll` in
+// unit tests. As this cell is therefore not covered by script tests, it should be
+// kept as simple possible. Any control-plane logic must be added elsewhere.
+var Cell = cell.Group(
+	cell.Provide(
+		newEndpointManagerAdapter,
+		newEndpointAPIManagerAdapter,
+	),
 )

enterprise/pkg/privnet/endpoints/types.go
@@ -1,0 +1,166 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package endpoints
+
+import (
+	"context"
+	"fmt"
+	"iter"
+	"net/netip"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+const (
+	// PropertyPrivNetNetwork is the name of the network this endpoint is attached to. If unset, then the endpoint is
+	// not attached to a custom private network.
+	PropertyPrivNetNetwork = "isovalent-privnet-network"
+
+	// PropertyPrivNetIPv4 contains the IPv4 address of the endpoint within the network.
+	PropertyPrivNetIPv4 = "isovalent-privnet-ipv4-addr"
+
+	// PropertyPrivNetIPv6 contains the IPv6 address of the endpoint within the network.
+	PropertyPrivNetIPv6 = "isovalent-privnet-ipv6-addr"
+
+	// PropertyPrivNetActivatedAt contains the timestamp when the endpoint became active.
+	PropertyPrivNetActivatedAt = "isovalent-privnet-activated-at"
+)
+
+// EndpointGetter allows read operations on the endpoint manager.
+// This is a custom interface to allow for slim mock implementations.
+type EndpointGetter interface {
+	Subscribe(s EndpointSubscriber)
+
+	LookupCEPName(nsname string) (ep Endpoint)
+	GetEndpointsByPodName(nsname string) iter.Seq[Endpoint]
+}
+
+// EndpointRemover allows the removal of endpoints on the endpoint manager.
+// This is a custom interface to allow for slim mock implementations.
+type EndpointRemover interface {
+	RemoveByCEPName(nsname string) (Endpoint, error)
+}
+
+// EndpointCreator allows the creation of endpoints on the endpoint manager.
+// This is a custom interface to allow for slim mock implementations.
+type EndpointCreator interface {
+	CreateEndpoint(ctx context.Context, epTemplate *models.EndpointChangeRequest) (Endpoint, error)
+}
+
+// Endpoint provides a subset of endpoint methods.
+// This is a custom interface to allow for slim mock implementations.
+type Endpoint interface {
+	GetID16() uint16
+
+	GetPropertyValue(key string) any
+	SetPropertyValue(key string, value any) any
+
+	LXCMac() mac.MAC
+	IPv4Address() netip.Addr
+	GetIPv4Address() string
+	IPv6Address() netip.Addr
+	GetIPv6Address() string
+
+	GetK8sNamespaceAndCEPName() string
+	GetK8sNamespaceAndPodName() string
+
+	SyncEndpointHeaderFile()
+}
+
+// EndpointSubscriber is endpointmanager.Subscriber but using the slim interfaces
+// defined in this package.
+type EndpointSubscriber interface {
+	EndpointCreated(ep Endpoint)
+	EndpointDeleted(ep Endpoint)
+	EndpointRestored(ep Endpoint)
+}
+
+// EndpointPropertyProvider allows access to the endpoint properties.
+// This is a custom interface to allow for slim mock implementations.
+type EndpointPropertyProvider interface {
+	GetPropertyValue(key string) any
+}
+
+// EndpointProperties is a type proxy for accessing the properties of an endpoint.
+type EndpointProperties struct {
+	network string
+	ep      EndpointPropertyProvider
+}
+
+// ExtractEndpointProperties extracts the private network relevant endpoint properties.
+// It returns (nil, false) if the given endpoint is not attached to a private network.
+func ExtractEndpointProperties(ep EndpointPropertyProvider) (*EndpointProperties, bool) {
+	network, ok := ep.GetPropertyValue(PropertyPrivNetNetwork).(string)
+	if !ok || network == "" {
+		return nil, false
+	}
+
+	return &EndpointProperties{
+		network: network,
+		ep:      ep,
+	}, true
+}
+
+// PrivateNetwork returns the name of the private network this endpoint is attached to
+func (p *EndpointProperties) PrivateNetwork() string {
+	return p.network
+}
+
+// NetworkIPv4 returns the IPv4 address of the endpoint within the network.
+func (p *EndpointProperties) NetworkIPv4() (netip.Addr, error) {
+	addr, ok := p.ep.GetPropertyValue(PropertyPrivNetIPv4).(string)
+	if !ok {
+		return netip.Addr{}, nil
+	}
+
+	ipv4, err := netip.ParseAddr(addr)
+	if err != nil {
+		return netip.Addr{}, err
+	} else if !ipv4.Is4() {
+		return netip.Addr{}, fmt.Errorf("expected IPv4 address, got %s", addr)
+	}
+
+	return ipv4, nil
+}
+
+// NetworkIPv6 returns the IPv6 address of the endpoint within the network.
+func (p *EndpointProperties) NetworkIPv6() (netip.Addr, error) {
+	addr, ok := p.ep.GetPropertyValue(PropertyPrivNetIPv6).(string)
+	if !ok {
+		return netip.Addr{}, nil
+	}
+
+	ipv6, err := netip.ParseAddr(addr)
+	if err != nil {
+		return netip.Addr{}, err
+	} else if !ipv6.Is6() {
+		return netip.Addr{}, fmt.Errorf("expected IPv6 address, got %s", addr)
+	}
+
+	return ipv6, nil
+}
+
+// ActivatedAt returns the timestamp when the endpoint became active.
+func (p *EndpointProperties) ActivatedAt() (time.Time, error) {
+	datetime, ok := p.ep.GetPropertyValue(PropertyPrivNetActivatedAt).(string)
+	if !ok {
+		return time.Time{}, nil
+	}
+
+	return time.Parse(time.RFC3339Nano, datetime)
+}
+
+// FormatActivatedAtProperty is a helper to be used to format the PropertyPrivNetActivatedAt property.
+func FormatActivatedAtProperty(t time.Time) string {
+	return t.UTC().Format(time.RFC3339Nano)
+}

enterprise/pkg/privnet/reconcilers/cell.go
@@ -1,0 +1,20 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilers
+
+import "github.com/cilium/hive/cell"
+
+var Cell = cell.Group(
+	PrivateNetworksCell,
+	EndpointsCell,
+	RoutesCell,
+	MapEntriesCell,
+)

enterprise/pkg/privnet/tests/cell.go
@@ -16,6 +16,7 @@ import (
 	"github.com/cilium/hive/cell"
 	"github.com/cilium/statedb"
 
+	daemonk8s "github.com/cilium/cilium/daemon/k8s"
 	"github.com/cilium/cilium/enterprise/pkg/privnet"
 	"github.com/cilium/cilium/enterprise/pkg/privnet/reconcilers"
 	"github.com/cilium/cilium/enterprise/pkg/privnet/tables"
@@ -27,11 +28,18 @@ import (
 	"github.com/cilium/cilium/pkg/promise"
 )
 
+const nodeName = "foobar-worker-1"
+
 func NewTestHive(t testing.TB) *hive.Hive {
 	return hive.New(
 		k8sClient.FakeClientCell(),
 
 		cell.Config(cmtypes.DefaultClusterInfo),
+
+		daemonk8s.ResourcesCell,
+		daemonk8s.TablesCell,
+
+		mockEndpointCell(t),
 
 		cell.Provide(
 			dptables.NewDeviceTable,

enterprise/pkg/privnet/tests/mock_endpoints.go
@@ -1,0 +1,417 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package tests
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"iter"
+	"net/netip"
+	"os"
+	"slices"
+	"testing"
+
+	uhive "github.com/cilium/hive"
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/hive/script"
+
+	"github.com/cilium/cilium/api/v1/models"
+	"github.com/cilium/cilium/enterprise/pkg/privnet/endpoints"
+	"github.com/cilium/cilium/pkg/endpoint"
+	"github.com/cilium/cilium/pkg/endpoint/regeneration"
+	"github.com/cilium/cilium/pkg/endpointstate"
+	"github.com/cilium/cilium/pkg/lock"
+	"github.com/cilium/cilium/pkg/mac"
+	"github.com/cilium/cilium/pkg/promise"
+)
+
+func mockEndpointCell(t testing.TB) cell.Cell {
+	t.Helper()
+
+	return cell.Group(
+		cell.ProvidePrivate(
+			newFakeRestorer,
+			newFakeEPM,
+		),
+		cell.Provide(
+			regeneration.NewFence,
+
+			func(f *fakeEPM) uhive.ScriptCmdsOut { return uhive.NewScriptCmds(f.cmds()) },
+			func(f *fakeRestorer) promise.Promise[endpointstate.Restorer] { return f },
+		),
+		cell.DecorateAll(func(f *fakeEPM) endpoints.EndpointGetter { return f }),
+		cell.DecorateAll(func(f *fakeEPM) endpoints.EndpointCreator { return f }),
+		cell.DecorateAll(func(f *fakeEPM) endpoints.EndpointRemover { return f }),
+	)
+}
+
+type fakeEP struct {
+	mu lock.Mutex
+
+	ID uint16
+
+	IPv4 netip.Addr
+	IPv6 netip.Addr
+	MAC  mac.MAC
+
+	PodName   string
+	Namespace string
+
+	Properties map[string]any
+}
+
+var _ endpoints.Endpoint = &fakeEP{}
+
+// GetID16 implements endpoints.Endpoint.
+func (f *fakeEP) GetID16() uint16 {
+	return f.ID
+}
+
+// IPv4Address implements endpoints.Endpoint.
+func (f *fakeEP) IPv4Address() netip.Addr {
+	return f.IPv4
+}
+
+// GetIPv4Address implements endpoints.Endpoint.
+func (f *fakeEP) GetIPv4Address() string {
+	if f.IPv4.IsValid() {
+		return f.IPv4.String()
+	}
+	return ""
+}
+
+// IPv6Address implements endpoints.Endpoint.
+func (f *fakeEP) IPv6Address() netip.Addr {
+	return f.IPv6
+}
+
+// GetIPv6Address implements endpoints.Endpoint.
+func (f *fakeEP) GetIPv6Address() string {
+	if f.IPv6.IsValid() {
+		return f.IPv6.String()
+	}
+	return ""
+}
+
+// GetK8sCEPName implements endpoints.Endpoint.
+func (f *fakeEP) GetK8sCEPName() string {
+	if cepName, ok := f.Properties[endpoint.PropertyCEPName]; ok {
+		return cepName.(string)
+	}
+	return f.PodName
+}
+
+// GetK8sNamespaceAndCEPName implements endpoints.Endpoint.
+func (f *fakeEP) GetK8sNamespaceAndCEPName() string {
+	return fmt.Sprintf("%s/%s", f.Namespace, f.GetK8sCEPName())
+}
+
+// GetK8sNamespaceAndPodName implements endpoints.Endpoint.
+func (f *fakeEP) GetK8sNamespaceAndPodName() string {
+	return fmt.Sprintf("%s/%s", f.Namespace, f.PodName)
+}
+
+// GetPropertyValue implements endpoints.Endpoint.
+func (f *fakeEP) GetPropertyValue(key string) any {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if f.Properties == nil {
+		return nil
+	}
+	return f.Properties[key]
+}
+
+// SetPropertyValue implements endpoints.Endpoint.
+func (f *fakeEP) SetPropertyValue(key string, value any) any {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	if f.Properties == nil {
+		f.Properties = map[string]any{}
+	}
+	f.Properties[key] = value
+	return value
+}
+
+// LXCMac implements endpoints.Endpoint.
+func (f *fakeEP) LXCMac() mac.MAC {
+	return f.MAC
+}
+
+// SyncEndpointHeaderFile implements endpoints.Endpoint.
+func (f *fakeEP) SyncEndpointHeaderFile() {}
+
+// fakeEPM implements endpoints.Endpoint{Creator,Getter,Remover}
+type fakeEPM struct {
+	mu   lock.Mutex
+	eps  []*fakeEP
+	subs []endpoints.EndpointSubscriber
+
+	restorer *fakeRestorer
+}
+
+func newFakeEPM(restorer *fakeRestorer, fence regeneration.Fence, jg job.Group) *fakeEPM {
+	// wait for regeneration fence to unblock
+	jg.Add(job.OneShot("wait-for-restore", func(ctx context.Context, health cell.Health) error {
+		err := fence.Wait(ctx)
+		if err != nil {
+			return err
+		}
+		restorer.finishRestoration()
+		return nil
+	}))
+
+	return &fakeEPM{
+		subs: []endpoints.EndpointSubscriber{},
+		eps:  []*fakeEP{},
+
+		restorer: restorer,
+	}
+}
+
+// fakeRestorer implements endpointstate.Restorer, and
+// promise.Promise[endpointstate.Restorer] (resolving to itself)
+type fakeRestorer struct {
+	ch chan struct{}
+}
+
+func newFakeRestorer() *fakeRestorer {
+	return &fakeRestorer{
+		ch: make(chan struct{}),
+	}
+}
+
+func (f *fakeRestorer) waitForRestore(ctx context.Context) error {
+	select {
+	case <-f.ch:
+		return nil
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+func (f *fakeRestorer) finishRestoration() {
+	close(f.ch)
+}
+
+func (f *fakeRestorer) isRestored() bool {
+	select {
+	case <-f.ch:
+		return true
+	default:
+		return false
+	}
+}
+
+// WaitForEndpointRestore implements endpointstate.Restorer.
+func (f *fakeRestorer) WaitForEndpointRestore(ctx context.Context) error {
+	return f.waitForRestore(ctx)
+}
+
+// WaitForInitialPolicy implements endpointstate.Restorer.
+func (f *fakeRestorer) WaitForInitialPolicy(ctx context.Context) error {
+	return f.waitForRestore(ctx)
+}
+
+// Await implements promise.Promise.
+func (f *fakeRestorer) Await(context.Context) (endpointstate.Restorer, error) {
+	return f, nil
+}
+
+// CreateEndpoint implements endpoints.EndpointCreator.
+func (f *fakeEPM) createEndpoint(epTemplate *models.EndpointChangeRequest, restored bool) (endpoints.Endpoint, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	var err error
+	ep := fakeEP{
+		ID:         uint16(epTemplate.ID),
+		PodName:    epTemplate.K8sPodName,
+		Namespace:  epTemplate.K8sNamespace,
+		Properties: epTemplate.Properties,
+	}
+	if epTemplate.Addressing.IPV4 != "" {
+		ep.IPv4, err = netip.ParseAddr(epTemplate.Addressing.IPV4)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if epTemplate.Addressing.IPV6 != "" {
+		ep.IPv6, err = netip.ParseAddr(epTemplate.Addressing.IPV6)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if epTemplate.Mac != "" {
+		ep.MAC, err = mac.ParseMAC(epTemplate.Mac)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	f.eps = append(f.eps, &ep)
+	for _, sub := range f.subs {
+		if restored {
+			sub.EndpointRestored(&ep)
+		} else {
+			sub.EndpointCreated(&ep)
+		}
+	}
+	return &ep, nil
+}
+
+// CreateEndpoint implements endpoints.EndpointCreator.
+func (f *fakeEPM) CreateEndpoint(ctx context.Context, epTemplate *models.EndpointChangeRequest) (endpoints.Endpoint, error) {
+	ep, err := f.createEndpoint(epTemplate, false)
+	return ep, err
+}
+
+// RemoveByCEPName implements endpoints.EndpointRemover.
+func (f *fakeEPM) RemoveByCEPName(nsname string) (endpoints.Endpoint, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	i := slices.IndexFunc(f.eps, func(ep *fakeEP) bool {
+		return ep.GetK8sNamespaceAndCEPName() == nsname
+	})
+	if i < 0 {
+		return nil, fmt.Errorf("no endpoint found for CEP %q", nsname)
+	}
+	ep := f.eps[i]
+	f.eps = slices.Delete(f.eps, i, i+1)
+	for _, sub := range f.subs {
+		sub.EndpointDeleted(ep)
+	}
+	return ep, nil
+}
+
+// GetEndpointsByPodName implements endpoints.EndpointGetter.
+func (f *fakeEPM) GetEndpointsByPodName(nsname string) iter.Seq[endpoints.Endpoint] {
+	return func(yield func(endpoints.Endpoint) bool) {
+		f.mu.Lock()
+		defer f.mu.Unlock()
+		for _, ep := range f.eps {
+			if ep.GetK8sNamespaceAndPodName() == nsname {
+				if !yield(ep) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// LookupCEPName implements endpoints.EndpointGetter.
+func (f *fakeEPM) LookupCEPName(nsname string) (ep endpoints.Endpoint) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	for i := range f.eps {
+		if f.eps[i].GetK8sNamespaceAndCEPName() == nsname {
+			return f.eps[i]
+		}
+	}
+	return nil
+}
+
+// Subscribe implements endpoints.EndpointGetter.
+func (f *fakeEPM) Subscribe(s endpoints.EndpointSubscriber) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.subs = append(f.subs, s)
+}
+
+func (f *fakeEPM) cmds() map[string]script.Cmd {
+	return map[string]script.Cmd{
+		"privnet/epm-create":  f.createEPCmd(),
+		"privnet/epm-delete":  f.deleteEPCmd(),
+		"privnet/epm-restore": f.restoreEPCmd(),
+	}
+}
+
+func parseEndpointJSON(s *script.State, args []string) (*models.EndpointChangeRequest, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("%w: expected number of arguments", script.ErrUsage)
+	}
+	b, err := os.ReadFile(s.Path(args[0]))
+	if err != nil {
+		return nil, err
+	}
+	epr := &models.EndpointChangeRequest{}
+	err = json.Unmarshal(b, epr)
+	if err != nil {
+		return nil, err
+	}
+	return epr, nil
+}
+
+func (f *fakeEPM) createEPCmd() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "create a fake endpoint",
+			Args:    "ep-req-json-file",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			epr, err := parseEndpointJSON(s, args)
+			if err != nil {
+				return nil, err
+			}
+			_, err = f.createEndpoint(epr, false)
+			if err != nil {
+				return nil, fmt.Errorf("fake endpoint creation failed: %w", err)
+			}
+
+			return nil, nil
+		},
+	)
+}
+
+func (f *fakeEPM) restoreEPCmd() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "restore a fake endpoint",
+			Args:    "ep-req-json-file",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if f.restorer.isRestored() {
+				return nil, errors.New("cannot restore endpoints after regeneration has already started")
+			}
+			epr, err := parseEndpointJSON(s, args)
+			if err != nil {
+				return nil, err
+			}
+			_, err = f.createEndpoint(epr, true)
+			if err != nil {
+				return nil, fmt.Errorf("fake endpoint creation failed: %w", err)
+			}
+
+			return nil, nil
+		},
+	)
+}
+
+func (f *fakeEPM) deleteEPCmd() script.Cmd {
+	return script.Command(
+		script.CmdUsage{
+			Summary: "delete a fake endpoint",
+			Args:    "cep-name",
+		},
+		func(s *script.State, args ...string) (script.WaitFunc, error) {
+			if len(args) != 1 {
+				return nil, fmt.Errorf("%w: expected number of arguments", script.ErrUsage)
+			}
+			_, err := f.RemoveByCEPName(args[0])
+			if err != nil {
+				return nil, fmt.Errorf("fake endpoint deletion failed: %w", err)
+			}
+
+			return nil, nil
+		},
+	)
+}

enterprise/pkg/privnet/tests/script_test.go
@@ -28,6 +28,7 @@ import (
 	k8sTestutils "github.com/cilium/cilium/pkg/k8s/testutils"
 	"github.com/cilium/cilium/pkg/k8s/version"
 	"github.com/cilium/cilium/pkg/logging"
+	nodeTypes "github.com/cilium/cilium/pkg/node/types"
 	"github.com/cilium/cilium/pkg/testutils"
 )
 
@@ -37,6 +38,7 @@ func TestScript(t *testing.T) {
 	defer testutils.GoleakVerifyNone(t)
 
 	version.Force(k8sTestutils.DefaultVersion)
+	nodeTypes.SetName(nodeName)
 
 	ctx, cancel := context.WithTimeout(t.Context(), 10*time.Second)
 	t.Cleanup(cancel)

commit fce9ba3a61fd9ae853c71735274e8b621f69d60d

privnet: Fix nil-pointer bug in endpoint manager adapter
Without this, callers cannot use `ep == nil` to check if the method has
returned an endpoint or not, since a concrete nil itself is not nil in
Go.

Fixes: afabc85bd07a ("privnet: Add mock implementations for the endpoint infrastructure")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

enterprise/pkg/privnet/endpoints/adapters.go
@@ -65,7 +65,13 @@ func (e endpointManagerAdapter) RemoveByCEPName(nsname string) (Endpoint, error)
 
 // LookupCEPName implements EndpointGetter.
 func (e endpointManagerAdapter) LookupCEPName(nsname string) Endpoint {
-	return e.epm.LookupCEPName(nsname)
+	ep := e.epm.LookupCEPName(nsname)
+	if ep == nil {
+		// This is needed to avoid having the returned interface value
+		// holding a nil concrete value that is itself non-nil.
+		return nil
+	}
+	return ep
 }
 
 // GetEndpointsByPodName implements EndpointGetter.