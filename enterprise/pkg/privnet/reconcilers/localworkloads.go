nobidev/cilium

commit 07a81d11c23d2351d468d6a8a6e35078c057a3c0

privnet: Add local-workloads reconciler
This commit adds the reconciler which watches local endpoint changes (by
subscribing to the endpoint manager and endpoint activation manager) and
popualtes the privnet-local-workloads table accordingly. The local
workloads table contains the private network information of local Cilium
endpoints (for easier consumption by other downstream consumers).

A script test is also added which mocks endpoint restoration, creation
and pod annotation updates.

Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
Co-authored-by: Marco Iorio <marco.iorio@isovalent.com>
Co-authored-by: Tobias Klauser <tobias@isovalent.com>
Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

enterprise/pkg/privnet/reconcilers/localworkloads.go
@@ -1,0 +1,293 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package reconcilers
+
+import (
+	"context"
+	"log/slog"
+	"strings"
+
+	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
+	"github.com/cilium/statedb/reconciler"
+
+	"github.com/cilium/cilium/enterprise/pkg/privnet/config"
+	"github.com/cilium/cilium/enterprise/pkg/privnet/endpoints"
+	"github.com/cilium/cilium/enterprise/pkg/privnet/tables"
+	"github.com/cilium/cilium/pkg/endpointstate"
+	"github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/promise"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+var LocalWorkloadsCell = cell.Group(
+	cell.ProvidePrivate(
+		// Provides the ReadWrite LocalWorkloads table.
+		tables.NewLocalWorkloadsTable,
+
+		// Provides the reconciler handling local private network workloads.
+		newLocalWorkloads,
+	),
+
+	cell.Provide(
+		// Provides the ReadOnly LocalWorkloads table.
+		statedb.RWTable[*tables.LocalWorkload].ToTable,
+	),
+
+	cell.Invoke(
+		// Starts reflecting local endpoints into the local workloads table
+		(*LocalWorkloads).registerReconciler,
+	),
+)
+
+// LocalWorkloads is a reconciler which populates the local workload tables based on local endpoint events
+type LocalWorkloads struct {
+	log *slog.Logger
+	jg  job.Group
+
+	cfg config.Config
+
+	endpointManager           endpoints.EndpointGetter
+	endpointActivationManager *EndpointActivationManager
+	restorerPromise           promise.Promise[endpointstate.Restorer]
+
+	db  *statedb.DB
+	tbl statedb.RWTable[*tables.LocalWorkload]
+}
+
+func newLocalWorkloads(in struct {
+	cell.In
+
+	Log      *slog.Logger
+	JobGroup job.Group
+
+	Config config.Config
+
+	EndpointManager           endpoints.EndpointGetter
+	EndpointActivationManager *EndpointActivationManager
+	RestorerPromise           promise.Promise[endpointstate.Restorer]
+
+	DB    *statedb.DB
+	Table statedb.RWTable[*tables.LocalWorkload]
+}) (*LocalWorkloads, error) {
+	reconciler := &LocalWorkloads{
+		log: in.Log,
+		jg:  in.JobGroup,
+
+		cfg: in.Config,
+
+		endpointManager:           in.EndpointManager,
+		endpointActivationManager: in.EndpointActivationManager,
+		restorerPromise:           in.RestorerPromise,
+
+		db:  in.DB,
+		tbl: in.Table,
+	}
+
+	return reconciler, nil
+}
+
+func (l *LocalWorkloads) registerReconciler() {
+	if !l.cfg.Enabled {
+		return
+	}
+
+	wtx := l.db.WriteTxn(l.tbl)
+	initialized := l.tbl.RegisterInitializer(wtx, "privnet-eps-restored")
+	wtx.Commit()
+
+	// Subscribe to endpoint creation/deletion/activation events
+	l.endpointManager.Subscribe(l)
+	l.endpointActivationManager.Subscribe(l)
+
+	l.jg.Add(job.OneShot("privnet-ep-sync", func(ctx context.Context, health cell.Health) error {
+		defer health.Stopped("Done")
+
+		// Block until all endpoints have been restored (i.e. received by EndpointRestored callback),
+		// as otherwise downstream consumers might publish a partial snapshot.
+		health.OK("Waiting for restorer promise")
+		restorer, err := l.restorerPromise.Await(ctx)
+		if err != nil {
+			health.Degraded("Failed to resolve restorer promise", err)
+			return err
+		}
+
+		health.OK("Waiting for endpoint restoration to finish")
+		err = restorer.WaitForEndpointRestore(ctx)
+		if err != nil {
+			health.Degraded("Failed to wait for endpoint restoration", err)
+			return err
+		}
+
+		// Mark table as initialized
+		health.OK("Initializing local workloads table")
+		wtx := l.db.WriteTxn(l.tbl)
+		initialized(wtx)
+		wtx.Commit()
+
+		return nil
+	}))
+}
+
+// upsertEndpoint upserts the local endpoint into the local workload table if it has
+// private network properties. Endpoints without private network properties or
+// endpoints without a backing CEP are ignored.
+func (l *LocalWorkloads) upsertEndpoint(ep endpoints.Endpoint) {
+	k8sNamespace, k8sName, ok := strings.Cut(ep.GetK8sNamespaceAndCEPName(), "/")
+	if !ok {
+		l.log.Debug("Skipping local endpoint without K8s namespace/name",
+			logfields.EndpointID, ep.GetID16())
+		return
+	}
+
+	privNetAddr, err := extractPrivateNetworkAddressing(ep)
+	if err != nil {
+		l.log.Error("Failed to extract private network endpoint properties",
+			logfields.EndpointID, ep.GetID16(),
+			logfields.CEPName, ep.GetK8sNamespaceAndCEPName(),
+			logfields.Error, err,
+		)
+		return
+	} else if privNetAddr == nil {
+		l.log.Debug("Skipping local endpoint without private network properties",
+			logfields.EndpointID, ep.GetID16(),
+			logfields.CEPName, ep.GetK8sNamespaceAndCEPName(),
+		)
+		return
+	}
+
+	wtx := l.db.WriteTxn(l.tbl)
+	defer wtx.Commit()
+
+	lw := &tables.LocalWorkload{
+		EndpointID: ep.GetID16(),
+		Namespace:  k8sNamespace,
+		Endpoint: v1alpha1.PrivateNetworkEndpointSliceEndpoint{
+			Addressing: v1alpha1.PrivateNetworkEndpointAddressing{
+				IPv4: ep.GetIPv4Address(),
+				IPv6: ep.GetIPv6Address(),
+			},
+			Name: k8sName,
+		},
+		Interface: v1alpha1.PrivateNetworkEndpointSliceInterface{
+			Addressing: v1alpha1.PrivateNetworkEndpointAddressing{
+				IPv4: privNetAddr.ipv4,
+				IPv6: privNetAddr.ipv6,
+			},
+			MAC:     privNetAddr.mac,
+			Network: privNetAddr.network,
+		},
+		ActivatedAt: privNetAddr.activatedAt,
+		Status:      reconciler.StatusPending(),
+	}
+
+	_, _, err = l.tbl.Insert(wtx, lw)
+	if err != nil {
+		l.log.Error("BUG: Failed to insert local endpoint. "+
+			"Please report this bug to Cilium developers.",
+			logfields.EndpointID, ep.GetID16(),
+			logfields.Error, err,
+		)
+		return
+	}
+}
+
+// deleteEndpoint deletes the endpoint from the local workload table (if it exists)
+func (l *LocalWorkloads) deleteEndpoint(ep endpoints.Endpoint) {
+	wtx := l.db.WriteTxn(l.tbl)
+	defer wtx.Commit()
+
+	_, _, err := l.tbl.Delete(wtx, &tables.LocalWorkload{EndpointID: ep.GetID16()})
+	if err != nil {
+		l.log.Error("BUG: Failed to delete local endpoint. "+
+			"Please report this bug to Cilium developers.",
+			logfields.EndpointID, ep.GetID16(),
+			logfields.Error, err,
+		)
+		return
+	}
+}
+
+// EndpointActivationChanged implements endpointActivationSubscriber
+func (l *LocalWorkloads) EndpointActivationChanged(ep endpoints.Endpoint) {
+	l.upsertEndpoint(ep)
+}
+
+// EndpointCreated implements endpoints.EndpointSubscriber.
+func (l *LocalWorkloads) EndpointCreated(ep endpoints.Endpoint) {
+	l.upsertEndpoint(ep)
+}
+
+// EndpointRestored implements endpoints.EndpointSubscriber.
+func (l *LocalWorkloads) EndpointRestored(ep endpoints.Endpoint) {
+	l.upsertEndpoint(ep)
+}
+
+// EndpointDeleted implements endpoints.EndpointSubscriber.
+func (l *LocalWorkloads) EndpointDeleted(ep endpoints.Endpoint) {
+	l.deleteEndpoint(ep)
+}
+
+// privateNetworkAddressing contains the extracted private network endpoint properties
+type privateNetworkAddressing struct {
+	network string
+
+	ipv4 string
+	ipv6 string
+	mac  string
+
+	activatedAt time.Time
+}
+
+// extractPrivateNetworkAddressing extracts private network properties from the endpoint.
+// Returns nil if the endpoint is not attached to a private network or does not have
+// any private network IP.
+func extractPrivateNetworkAddressing(ep endpoints.Endpoint) (*privateNetworkAddressing, error) {
+	properties, ok := endpoints.ExtractEndpointProperties(ep)
+	if !ok {
+		return nil, nil
+	}
+
+	activatedAt, err := properties.ActivatedAt()
+	if err != nil {
+		return nil, err
+	}
+
+	addr := &privateNetworkAddressing{
+		network:     properties.PrivateNetwork(),
+		mac:         ep.LXCMac().String(),
+		activatedAt: activatedAt,
+	}
+
+	ipv4, err := properties.NetworkIPv4()
+	if err != nil {
+		return nil, err
+	}
+	if ipv4.IsValid() {
+		addr.ipv4 = ipv4.String()
+	}
+	ipv6, err := properties.NetworkIPv6()
+	if err != nil {
+		return nil, err
+	}
+	if ipv6.IsValid() {
+		addr.ipv6 = ipv6.String()
+	}
+
+	// ignore endpoints without any valid addresses configured
+	if !(ipv4.IsValid() || ipv6.IsValid()) {
+		return nil, nil
+	}
+
+	return addr, nil
+}

enterprise/pkg/privnet/reconcilers/cell.go
@@ -18,6 +18,7 @@ var Cell = cell.Group(
 	RoutesCell,
 	MapEntriesCell,
 	PodsCell,
+	LocalWorkloadsCell,
 
 	// Infrastructure shared between some of the reconcilers
 	cell.Provide(

enterprise/pkg/privnet/tests/testdata/local-workloads.txtar
@@ -1,0 +1,125 @@
+#! --private-networks-enabled
+
+# Restore endpoint
+privnet/epm-restore ep-curious-koala.json
+db/cmp privnet-local-workloads local-workloads.v1.table
+
+# Ensure the table is not initialized before restoration finishes
+! db/initialized --timeout 0 privnet-local-workloads
+
+# Start hive (this will also finish restoration)
+hive start
+clustermesh/endpoints/sync
+db/initialized
+
+# Create three endpoints - clever-fox is a regular endpoint without privnet
+privnet/epm-create ep-speedy-cheetah.json
+privnet/epm-create ep-sleepy-sloth.json
+privnet/epm-create ep-clever-fox.json
+db/cmp privnet-local-workloads local-workloads.v2.table
+
+# Mark speedy-cheetah as inactive via pod annotation
+k8s/add pod-speedy-cheetah.v1.yaml
+
+# Remove curious-koala
+privnet/epm-delete foo/curious-koala
+
+# Assert that both changes are visible in the table
+db/cmp privnet-local-workloads local-workloads.v3.table
+
+-- local-workloads.v1.table --
+Endpoint             ID   Network         NetworkIPv4     NetworkIPv6           PodIPv4        PodIPv6              ActivatedAt
+foo/curious-koala    15   green-segment   10.20.0.10      fd00:10:20::10        10.244.5.115   fd00:10:244:5::115   2025-09-17T12:40:00Z
+
+-- local-workloads.v2.table --
+Endpoint             ID   Network         NetworkIPv4     NetworkIPv6           PodIPv4        PodIPv6              ActivatedAt
+foo/curious-koala    15   green-segment   10.20.0.10      fd00:10:20::10        10.244.5.115   fd00:10:244:5::115   2025-09-17T12:40:00Z
+bar/speedy-cheetah   16   green-segment   10.20.0.11      fd00:10:20::11        10.244.5.116   fd00:10:244:5::116   2025-09-17T12:41:00Z
+qux/sleepy-sloth     17   blue-segment    192.168.30.10   fd00:192:168:30::10   10.244.5.117   fd00:10:244:5::117   2025-09-17T12:42:00Z
+
+-- local-workloads.v3.table --
+Endpoint             ID   Network         NetworkIPv4     NetworkIPv6           PodIPv4        PodIPv6              ActivatedAt
+bar/speedy-cheetah   16   green-segment   10.20.0.11      fd00:10:20::11        10.244.5.116   fd00:10:244:5::116   <inactive>
+qux/sleepy-sloth     17   blue-segment    192.168.30.10   fd00:192:168:30::10   10.244.5.117   fd00:10:244:5::117   2025-09-17T12:42:00Z
+
+-- ep-curious-koala.json --
+{
+  "ID": 15,
+  "addressing": {
+    "ipv4": "10.244.5.115",
+    "ipv6": "fd00:10:244:5::115"
+  },
+  "k8s-pod-name": "curious-koala",
+  "k8s-namespace": "foo",
+  "mac": "00:50:56:ad:11:01",
+  "properties": {
+    "isovalent-privnet-network": "green-segment",
+    "isovalent-privnet-activated-at": "2025-09-17T12:40:00.000000Z",
+    "isovalent-privnet-ipv4-addr": "10.20.0.10",
+    "isovalent-privnet-ipv6-addr": "fd00:10:20::10"
+  }
+}
+
+-- ep-speedy-cheetah.json --
+{
+  "ID": 16,
+  "addressing": {
+    "ipv4": "10.244.5.116",
+    "ipv6": "fd00:10:244:5::116"
+  },
+  "k8s-pod-name": "speedy-cheetah",
+  "k8s-namespace": "bar",
+  "mac": "00:50:56:ad:11:02",
+  "properties": {
+    "isovalent-privnet-network": "green-segment",
+    "isovalent-privnet-activated-at": "2025-09-17T12:41:00.000000Z",
+    "isovalent-privnet-ipv4-addr": "10.20.0.11",
+    "isovalent-privnet-ipv6-addr": "fd00:10:20::11"
+  }
+}
+
+-- ep-sleepy-sloth.json --
+{
+  "ID": 17,
+  "addressing": {
+    "ipv4": "10.244.5.117",
+    "ipv6": "fd00:10:244:5::117"
+  },
+  "k8s-pod-name": "sleepy-sloth",
+  "k8s-namespace": "qux",
+  "mac": "00:50:56:ad:11:03",
+  "properties": {
+    "isovalent-privnet-network": "blue-segment",
+    "isovalent-privnet-activated-at": "2025-09-17T12:42:00.000000Z",
+    "isovalent-privnet-ipv4-addr": "192.168.30.10",
+    "isovalent-privnet-ipv6-addr": "fd00:192:168:30::10"
+  }
+}
+
+-- ep-clever-fox.json --
+{
+  "ID": 18,
+  "addressing": {
+    "ipv4": "10.244.5.118",
+    "ipv6": "fd00:10:244:5::118"
+  },
+  "k8s-pod-name": "clever-fox",
+  "k8s-namespace": "foo",
+  "mac": "00:50:56:ad:11:04"
+}
+
+-- pod-speedy-cheetah.v1.yaml --
+apiVersion: v1
+kind: Pod
+metadata:
+  name: speedy-cheetah
+  namespace: bar
+  annotations:
+    network.v1alpha1.isovalent.com/inactive: "true"
+    network.v1alpha1.isovalent.com/network-attachment: '{"network":"green-segment","ipv4":"10.20.0.11","ipv6":"fd00:10:244:5::116","mac":"00:50:56:ad:11:02"}'
+spec:
+  containers:
+  - name: sleep
+    image: busybox
+    command: ["sleep", "3600"]
+  nodeName: foobar-worker-1

commit 66b329a09530ba44a4764d7e186ab1e06b2ecd1a

privnet: do not call [health.Stopped] in one-shot job
The job itself already takes care of calling it upon termination, which
causes a log error if it had already been stopped:

> logger.go:256: level=ERROR msg="failed to delete reporter status tree"
>     module=health error="reporting for \"private-networks.job-privnet-ep-sync\" has been stopped"

Fixes: 07a81d11c23d ("privnet: Add local-workloads reconciler")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/localworkloads.go
@@ -110,8 +110,6 @@ func (l *LocalWorkloads) registerReconciler() {
 	l.endpointActivationManager.Subscribe(l)
 
 	l.jg.Add(job.OneShot("privnet-ep-sync", func(ctx context.Context, health cell.Health) error {
-		defer health.Stopped("Done")
-
 		// Block until all endpoints have been restored (i.e. received by EndpointRestored callback),
 		// as otherwise downstream consumers might publish a partial snapshot.
 		health.OK("Waiting for restorer promise")