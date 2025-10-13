nobidev/cilium

commit 16937f40cc631861183a89a0f678a60edcf3ca98

privnet: introduce the private networks reconcilers
Introduce the reconciliation logic to reflect private network
information from Kubernetes into the newly introduced statedb
table, and enrich it with the locally-assigned IDs and the
selected interface information. Additionally, introduce also
a separate logic to reconcile possible devices changes (e.g.,
a previously skipped device is now selected).

Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
Co-authored-by: Sebastian Wicki <sebastian@isovalent.com>
Co-authored-by: Tobias Klauser <tobias@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/privnets.go
@@ -11,13 +11,243 @@
 package reconcilers
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"iter"
+	"log/slog"
+
 	"github.com/cilium/hive/cell"
+	"github.com/cilium/hive/job"
+	"github.com/cilium/statedb"
+
+	"github.com/cilium/cilium/enterprise/pkg/privnet/config"
+	"github.com/cilium/cilium/enterprise/pkg/privnet/tables"
+	dptables "github.com/cilium/cilium/pkg/datapath/tables"
+	"github.com/cilium/cilium/pkg/k8s"
+	iso_v1alpha1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/client"
+	cs_iso_v1alpha1 "github.com/cilium/cilium/pkg/k8s/client/clientset/versioned/typed/isovalent.com/v1alpha1"
+	"github.com/cilium/cilium/pkg/k8s/synced"
+	"github.com/cilium/cilium/pkg/k8s/utils"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/promise"
 )
 
 var PrivateNetworksCell = cell.Group(
+	cell.ProvidePrivate(
+		// Provides the ReadWrite PrivateNetworks table.
+		tables.NewPrivateNetworksTable,
+
+		// Provides the reconciler handling private networks.
+		newPrivateNetworks,
+	),
+
 	cell.Provide(
 		// Provide the IDPool via hive, so that it can be overridden for testing
 		// purposes, as we will want stable IDs there.
 		newDefaultIDPool,
+
+		// Provides the ReadOnly PrivateNetworks table.
+		statedb.RWTable[tables.PrivateNetwork].ToTable,
+	),
+
+	cell.Invoke(
+		// Registers the k8s to table reflector.
+		(*PrivateNetworks).registerK8sReflector,
+
+		// Register the reconciler reacting to device changes.
+		(*PrivateNetworks).registerDeviceChangesReconciler,
 	),
 )
+
+// PrivateNetworks is the reconciler for private networks.
+type PrivateNetworks struct {
+	log *slog.Logger
+	jg  job.Group
+
+	cfg config.Config
+
+	db   *statedb.DB
+	tbl  statedb.RWTable[tables.PrivateNetwork]
+	devs statedb.Table[*dptables.Device]
+
+	client cs_iso_v1alpha1.ClusterwidePrivateNetworkInterface
+}
+
+func newPrivateNetworks(in struct {
+	cell.In
+
+	Log      *slog.Logger
+	JobGroup job.Group
+
+	Config config.Config
+
+	DB      *statedb.DB
+	Table   statedb.RWTable[tables.PrivateNetwork]
+	Devices statedb.Table[*dptables.Device]
+
+	Client client.Clientset
+}) (*PrivateNetworks, error) {
+	reconciler := &PrivateNetworks{
+		log:  in.Log,
+		jg:   in.JobGroup,
+		cfg:  in.Config,
+		db:   in.DB,
+		tbl:  in.Table,
+		devs: in.Devices,
+	}
+
+	if !in.Config.Enabled {
+		return reconciler, nil
+	}
+
+	if !in.Client.IsEnabled() {
+		return nil, errors.New("private networks requires Kubernetes support to be enabled")
+	}
+
+	reconciler.client = in.Client.IsovalentV1alpha1().ClusterwidePrivateNetworks()
+	return reconciler, nil
+}
+
+func (pn *PrivateNetworks) registerK8sReflector(idpool *IDPool, sync promise.Promise[synced.CRDSync]) error {
+	if !pn.cfg.Enabled {
+		return nil
+	}
+
+	cfg := k8s.ReflectorConfig[tables.PrivateNetwork]{
+		Name:          "to-table", // the full name will be "job-k8s-reflector-private-networks-to-table"
+		Table:         pn.tbl,
+		ListerWatcher: utils.ListerWatcherFromTyped(pn.client),
+		MetricScope:   "ClusterwidePrivateNetwork",
+		CRDSync:       sync,
+
+		// Use TransformMany, instead of Transform, to get the signal whether the
+		// entry is being upserted or deleted.
+		TransformMany: func(txn statedb.ReadTxn, deleted bool, obj any) (toInsert, toDelete iter.Seq[tables.PrivateNetwork]) {
+			privnet, ok := obj.(*iso_v1alpha1.ClusterwidePrivateNetwork)
+			if !ok {
+				return nil, nil
+			}
+
+			// Retrieve the current ID, if already assigned.
+			id := tables.NetworkIDReserved
+			if curr, _, found := pn.tbl.Get(txn, tables.PrivateNetworkByName(tables.NetworkName(privnet.Name))); found {
+				id = curr.ID
+			}
+
+			// Attempt to acquire a new ID, if not already assigned.
+			if id == tables.NetworkIDReserved && !deleted {
+				var err error
+				id, err = idpool.acquire()
+				if err != nil {
+					pn.log.Error("Failed to assign network ID to private network",
+						logfields.Error, err,
+						logfields.ClusterwidePrivateNetwork, privnet.Name,
+					)
+					return nil, nil
+				}
+			}
+
+			if deleted {
+				idpool.release(id)
+				return nil, func(yield func(tables.PrivateNetwork) bool) {
+					yield(tables.PrivateNetwork{Name: tables.NetworkName(privnet.Name)})
+				}
+			}
+
+			var iface tables.PrivateNetworkInterface
+			// Set the interface name only if we are not running in bridge mode,
+			// to prevent surprises in case the same manifests get reused.
+			if ifname := privnet.Spec.Interface.Name; pn.cfg.EnabledAsBridge() && ifname != "" {
+				// The devices controller, which provides the devices table, has
+				// a start hook that returns only after that the first initialization
+				// completed. Hence, here we are guaranteed that the devices table
+				// is already initialized. We configure the interface parameters
+				// at this point, in addition to via the dedicated job, to ensure
+				// that they are correctly populated on startup, when the table
+				// is marked as initialized.
+				dev, _, _ := pn.devs.Get(txn, dptables.DeviceNameIndex.Query(ifname))
+				iface = pn.newInterface(ifname, dev)
+			}
+
+			return func(yield func(tables.PrivateNetwork) bool) {
+				yield(tables.PrivateNetwork{
+					Name:      tables.NetworkName(privnet.Name),
+					ID:        id,
+					INBs:      privnet.Spec.INBs,
+					Interface: iface,
+					Routes:    privnet.Spec.Routes,
+					Subnets:   privnet.Spec.Subnets,
+				})
+			}, nil
+		},
+	}
+
+	return k8s.RegisterReflector(pn.jg, pn.db, cfg)
+}
+
+func (pn *PrivateNetworks) registerDeviceChangesReconciler() {
+	if !pn.cfg.Enabled {
+		return
+	}
+
+	pn.jg.Add(
+		job.OneShot(
+			"private-networks-device-changes",
+			pn.reconcileDeviceChanges,
+		),
+	)
+}
+
+func (pn *PrivateNetworks) reconcileDeviceChanges(ctx context.Context, health cell.Health) error {
+	health.OK("Starting")
+	for {
+		txn := pn.db.WriteTxn(pn.tbl)
+
+		devs := make(map[string]*dptables.Device)
+		devsIter, devsWatch := pn.devs.AllWatch(txn)
+		for dev := range devsIter {
+			devs[dev.Name] = dev
+		}
+
+		privnets := pn.tbl.All(txn)
+		for privnet := range privnets {
+			ifname := privnet.Interface.Name
+			if ifname == "" {
+				continue
+			}
+
+			iface := pn.newInterface(ifname, devs[ifname])
+			if privnet.Interface != iface {
+				copy := privnet
+				copy.Interface = iface
+				pn.tbl.Insert(txn, copy)
+			}
+		}
+
+		txn.Commit()
+		health.OK("Reconciliation completed")
+
+		select {
+		case <-devsWatch:
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (pn *PrivateNetworks) newInterface(name string, dev *dptables.Device) tables.PrivateNetworkInterface {
+	iface := tables.PrivateNetworkInterface{Name: name}
+
+	switch {
+	case dev == nil:
+		iface.Error = fmt.Sprintf("Interface %q not found", name)
+	case !dev.Selected:
+		iface.Error = fmt.Sprintf("Interface %q not selected by Cilium: %v", name, dev.NotSelectedReason)
+	default:
+		iface.Index = dev.Index
+	}
+
+	return iface
+}

install/kubernetes/cilium/templates/cilium-agent/enterprise-clusterrole.yaml
@@ -91,4 +91,14 @@ rules:
     - watch
     - get
 {{- end }}
+{{- if .Values.enterprise.privateNetworks.enabled }}
+- apiGroups:
+    - isovalent.com
+  resources:
+    - clusterwideprivatenetworks
+  verbs:
+    - list
+    - watch
+    - get
+{{- end }}
 {{- end }}

pkg/logging/logfields/enterprise_logfields.go
@@ -108,4 +108,6 @@ const (
 	ToRemove = "toRemove"
 
 	WorldIdentity = "worldIdentity"
+
+	ClusterwidePrivateNetwork = "ClusterwidePrivateNetwork"
 )

commit d592b7e2573f4c0d99e27d53b70088505fb60217

privnet: fix release of private network IDs
Currently, the release of no longer used private network IDs is
performed as part of the [TransformMany] reconciler function.
However, this function is not invoked on old entries upon a full
relist, hence possibly leaking IDs. While this is expected to
be never a problem in practice, given the huge ID space, let's
get it fixed, moving the release task to a dedicated job. While
being there, let's also change the reconciler back to [Transform].

Fixes: 16937f40cc63 ("privnet: introduce the private networks reconcilers")
Reported-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/privnets.go
@@ -14,7 +14,6 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"iter"
 	"log/slog"
 	"net/netip"
 
@@ -59,6 +58,9 @@ var PrivateNetworksCell = cell.Group(
 
 		// Register the reconciler reacting to device changes.
 		(*PrivateNetworks).registerDeviceChangesReconciler,
+
+		// Register the reconciler to release stale network IDs.
+		(*PrivateNetworks).registerIDsReleaser,
 	),
 )
 
@@ -123,12 +125,10 @@ func (pn *PrivateNetworks) registerK8sReflector(idpool *IDPool, sync promise.Pro
 		MetricScope:   "ClusterwidePrivateNetwork",
 		CRDSync:       sync,
 
-		// Use TransformMany, instead of Transform, to get the signal whether the
-		// entry is being upserted or deleted.
-		TransformMany: func(txn statedb.ReadTxn, deleted bool, obj any) (toInsert, toDelete iter.Seq[tables.PrivateNetwork]) {
+		Transform: func(txn statedb.ReadTxn, obj any) (tables.PrivateNetwork, bool) {
 			privnet, ok := obj.(*iso_v1alpha1.ClusterwidePrivateNetwork)
 			if !ok {
-				return nil, nil
+				return tables.PrivateNetwork{}, false
 			}
 
 			// Retrieve the current ID, if already assigned.
@@ -138,7 +138,7 @@ func (pn *PrivateNetworks) registerK8sReflector(idpool *IDPool, sync promise.Pro
 			}
 
 			// Attempt to acquire a new ID, if not already assigned.
-			if id == tables.NetworkIDReserved && !deleted {
+			if id == tables.NetworkIDReserved {
 				var err error
 				id, err = idpool.acquire()
 				if err != nil {
@@ -146,14 +146,7 @@ func (pn *PrivateNetworks) registerK8sReflector(idpool *IDPool, sync promise.Pro
 						logfields.Error, err,
 						logfields.ClusterwidePrivateNetwork, privnet.Name,
 					)
-					return nil, nil
-				}
-			}
-
-			if deleted {
-				idpool.release(id)
-				return nil, func(yield func(tables.PrivateNetwork) bool) {
-					yield(tables.PrivateNetwork{Name: tables.NetworkName(privnet.Name)})
+					return tables.PrivateNetwork{}, false
 				}
 			}
 
@@ -176,16 +169,14 @@ func (pn *PrivateNetworks) registerK8sReflector(idpool *IDPool, sync promise.Pro
 			routes := pn.extractRoutes(privnet)
 			subnets := pn.extractSubnets(privnet)
 
-			return func(yield func(tables.PrivateNetwork) bool) {
-				yield(tables.PrivateNetwork{
-					Name:      tables.NetworkName(privnet.Name),
-					ID:        id,
-					INBs:      inbs,
-					Interface: iface,
-					Routes:    routes,
-					Subnets:   subnets,
-				})
-			}, nil
+			return tables.PrivateNetwork{
+				Name:      tables.NetworkName(privnet.Name),
+				ID:        id,
+				INBs:      inbs,
+				Interface: iface,
+				Routes:    routes,
+				Subnets:   subnets,
+			}, true
 		},
 	}
 
@@ -309,6 +300,46 @@ func (pn *PrivateNetworks) reconcileDeviceChanges(ctx context.Context, health ce
 			return nil
 		}
 	}
+}
+
+func (pn *PrivateNetworks) registerIDsReleaser(idpool *IDPool) {
+	if !pn.cfg.Enabled {
+		return
+	}
+
+	pn.jg.Add(
+		job.OneShot(
+			"private-networks-release-ids",
+			func(ctx context.Context, health cell.Health) error {
+				wtx := pn.db.WriteTxn(pn.tbl)
+				changeIter, _ := pn.tbl.Changes(wtx)
+				wtx.Commit()
+
+				health.OK("Primed")
+				for {
+					var count uint
+					changes, watch := changeIter.Next(pn.db.ReadTxn())
+
+					for change := range changes {
+						if change.Deleted && change.Object.ID != tables.NetworkIDReserved {
+							idpool.release(change.Object.ID)
+							count++
+						}
+					}
+
+					if count > 0 {
+						health.OK(fmt.Sprintf("%d IDs released", count))
+					}
+
+					select {
+					case <-watch:
+					case <-ctx.Done():
+						return nil
+					}
+				}
+			},
+		),
+	)
 }
 
 func (pn *PrivateNetworks) newInterface(name string, dev *dptables.Device) tables.PrivateNetworkInterface {