c7f4b37b2913010b7b9c6f4cc20d485c7df70e37
Marco Iorio <marco.iorio@isovalent.com>
2025-10-24 09:52:14 +0200

    privnet/health/test: script tests to validate checker implementation
    Building on top of the ServerPool and CheckerPool utilities, let's
    introduce a script check to validate E2E the correct functioning of
    the checker implementation, including health probing, retrieval and
    update of served networks, as well as activation and deactivation
    of networks.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

---
+++ enterprise/pkg/privnet/health/grpc/tests/script_test.go
@@ -0,0 +1,78 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package tests
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"maps"
+	"testing"
+	"time"
+
+	"github.com/cilium/hive/hivetest"
+	"github.com/cilium/hive/script"
+	"github.com/cilium/hive/script/scripttest"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/cilium/cilium/enterprise/pkg/privnet/health/grpc"
+	"github.com/cilium/cilium/pkg/hive"
+	"github.com/cilium/cilium/pkg/logging"
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+var debug = flag.Bool("debug", false, "Enable debug logging")
+
+func TestScript(t *testing.T) {
+	defer testutils.GoleakVerifyNone(t)
+
+	ctx, cancel := context.WithTimeout(t.Context(), 10*time.Second)
+	t.Cleanup(cancel)
+
+	scripttest.Test(t,
+		ctx,
+		func(t testing.TB, args []string) *script.Engine {
+			var opts []hivetest.LogOption
+			if *debug {
+				opts = append(opts, hivetest.LogLevel(slog.LevelDebug))
+				logging.SetLogLevelToDebug()
+			}
+			log := hivetest.Logger(t, opts...)
+
+			h := hive.New(
+				grpc.Cell,
+
+				ConnFactoryCell(t.TempDir()),
+				ServerPoolCell,
+				CheckerPoolCell,
+			)
+
+			t.Cleanup(func() {
+				assert.NoError(t, h.Stop(log, context.Background()))
+			})
+
+			flags := pflag.NewFlagSet("", pflag.ContinueOnError)
+			h.RegisterFlags(flags)
+
+			require.NoError(t, flags.Parse(args), "flags.Parse")
+
+			cmds, err := h.ScriptCommands(log)
+			require.NoError(t, err, "ScriptCommands")
+			maps.Insert(cmds, maps.All(script.DefaultCmds()))
+
+			return &script.Engine{
+				Cmds:          cmds,
+				RetryInterval: 10 * time.Millisecond,
+			}
+		}, []string{}, "testdata/*.txtar")
+}
