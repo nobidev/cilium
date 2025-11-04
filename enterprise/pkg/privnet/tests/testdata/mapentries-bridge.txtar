nobidev/cilium

commit 685c3c30228a13e5a2b7ef038e9f958c21752534

privnet: extend mapentries reconciler to account for external endpoints
Extend the mapentries reconciler to make use of the newly introduced
properties of endpoints to appropriately filter out the external
endpoints advertised by non-active INB nodes, so that traffic is
correctly directed to the active INB. The script test is also adapted
accordingly to validate the new behavior.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/mapentries.go
@@ -297,8 +297,16 @@ func (m *MapEntries) handleEndpointChange(txn statedb.WriteTxn, ep tables.Endpoi
 		return nil
 	}
 
+	// We do not short-circuit the processing of external endpoints advertised by
+	// non-active INBs at this point, as it is technically possible to mutate the
+	// node name and/or the external flag of the endpoint, which would lead to
+	// incorrect entries in the mapentries table. Instead, let's go through the
+	// normal processing, short-circuiting below if the desired entry matches the
+	// one already currently present. This also avoids the need for a special
+	// logic to handle the change of the active INB.
+
 	// Try to find a new active endpoint for the given (net, netIP) pair
-	activeEp := m.determineActiveEndpointForNetworkIP(txn, tables.NetworkName(ep.Network.Name), ep.Network.IP)
+	activeEp := m.determineActiveEndpointForNetworkIP(txn, privNet, ep.Network.IP)
 
 	// Check if there already is an existing entry in the table for the endpoint to be upserted
 	current, _, found := m.tbl.Get(txn, tables.MapEntryByKey(ep.ToMapEntryKey()))
@@ -344,11 +352,16 @@ func (m *MapEntries) handleEndpointChange(txn statedb.WriteTxn, ep tables.Endpoi
 }
 
 // determineActiveEndpointForNetworkIP checks the endpoint table to find the active endpoint for the given
-// network IP. The active endpoint is the one with the most recent "activatedAt" timestamp.
-func (m *MapEntries) determineActiveEndpointForNetworkIP(txn statedb.ReadTxn, network tables.NetworkName, networkIP netip.Addr) (active *tables.Endpoint) {
-	for ep := range m.endpoints.List(txn, tables.EndpointsByNetworkIP(network, networkIP)) {
+// network IP. The active endpoint is the one with the most recent "activatedAt" timestamp, filtering out
+// the ones advertised by non-active INBs.
+func (m *MapEntries) determineActiveEndpointForNetworkIP(txn statedb.ReadTxn, network tables.SlimPrivateNetwork, networkIP netip.Addr) (active *tables.Endpoint) {
+	for ep := range m.endpoints.List(txn, tables.EndpointsByNetworkIP(network.Name, networkIP)) {
 		if ep.ActivatedAt.IsZero() {
 			continue // skip inactive endpoints
+		}
+
+		if m.shouldSkipExternalEndpoint(ep, network) {
+			continue // skip external endpoints advertised by non-active INBs.
 		}
 
 		if active == nil || ep.ActivatedAt.After(active.ActivatedAt) {
@@ -357,6 +370,16 @@ func (m *MapEntries) determineActiveEndpointForNetworkIP(txn statedb.ReadTxn, ne
 	}
 
 	return active
+}
+
+// shouldSkipExternalEndpointSkip returns whether a given external endpoint should
+// be skipped, that is if either this network has no active INB, or the endpoint
+// is not advertised by the currently active INB. This function always returns
+// false if the endpoint is not external.
+func (m *MapEntries) shouldSkipExternalEndpoint(ep tables.Endpoint, privnet tables.SlimPrivateNetwork) bool {
+	return ep.Flags.External && (!privnet.ActiveINB.IP.IsValid() ||
+		tables.ClusterName(ep.Source.Cluster) != privnet.ActiveINB.Cluster ||
+		tables.NodeName(ep.NodeName) != privnet.ActiveINB.Name)
 }
 
 // findConflictingNATEntriesForActiveEP checks the endpoint table to find all endpoints

enterprise/pkg/privnet/tables/mapentries.go
@@ -87,6 +87,9 @@ type MapEntryType uint8
 const (
 	// MapEntryTypeEndpoint represents an endpoint.
 	MapEntryTypeEndpoint MapEntryType = iota
+	// MapEntryTypeExternalEndpoint represents an expoint external to the
+	// cluster(mesh), accessible via the INB.
+	MapEntryTypeExternalEndpoint
 	// MapEntryTypeDCNRoute represents an L2 route.
 	MapEntryTypeDCNRoute
 	// MapEntryTypeStaticRoute represents an L3 static route.
@@ -97,6 +100,8 @@ func (typ MapEntryType) String() string {
 	switch typ {
 	case MapEntryTypeEndpoint:
 		return "E"
+	case MapEntryTypeExternalEndpoint:
+		return "X"
 	case MapEntryTypeDCNRoute:
 		return "D"
 	case MapEntryTypeStaticRoute:

enterprise/pkg/privnet/tables/endpoints.go
@@ -43,6 +43,14 @@ func (ep Endpoint) Key() EndpointKey {
 	return newEndpointKey(ep.Source, NetworkName(ep.Name), ep.IP)
 }
 
+// MapEntryType returns the MapEntry type of this endpoint.
+func (ep Endpoint) MapEntryType() MapEntryType {
+	if ep.Flags.External {
+		return MapEntryTypeExternalEndpoint
+	}
+	return MapEntryTypeEndpoint
+}
+
 // ToMapEntry returns the MapEntry object created from the Endpoint and
 // PrivateNetwork information.
 func (ep Endpoint) ToMapEntry(privnet SlimPrivateNetwork, bridgeMode bool) *MapEntry {
@@ -54,7 +62,7 @@ func (ep Endpoint) ToMapEntry(privnet SlimPrivateNetwork, bridgeMode bool) *MapE
 	}
 
 	return &MapEntry{
-		Type: MapEntryTypeEndpoint,
+		Type: ep.MapEntryType(),
 
 		Target: MapEntryTarget{
 			NetworkName: privnet.Name,
@@ -77,7 +85,7 @@ func (ep Endpoint) ToMapEntry(privnet SlimPrivateNetwork, bridgeMode bool) *MapE
 // ToMapEntryKey returns the key uniquely identifying the corresponding entry in
 // the map entries table.
 func (ep Endpoint) ToMapEntryKey() MapEntryKey {
-	return newMapEntryKey(NetworkName(ep.Network.Name), MapEntryTypeEndpoint,
+	return newMapEntryKey(NetworkName(ep.Network.Name), ep.MapEntryType(),
 		netip.PrefixFrom(ep.Network.IP, ep.Network.IP.BitLen()))
 }
 

enterprise/pkg/privnet/tests/testdata/mapentries.txtar
@@ -6,6 +6,11 @@ hive start
 k8s/add icpn-blue.v1.yaml
 k8s/add privnet-eps.v1.yaml
 
+# Ingest the endpoints from clustermesh.
+clustermesh/endpoints/upsert blue-network/10.245.8.21 endpoint-felidae-cougar-1.json
+clustermesh/endpoints/upsert blue-network/10.245.8.87 endpoint-felidae-cougar-2.json
+clustermesh/endpoints/upsert blue-network/10.245.9.41 endpoint-camelidae-alpaca-1.json
+
 # Observe INB node information from clustermesh (the etcd key is omitted as unused).
 clustermesh/nodes/upsert _ nodes-felidae-cougar.json
 clustermesh/nodes/upsert _ nodes-camelidae-alpaca.json
@@ -36,7 +41,11 @@ db/cmp privnet-mapentries mapentries.v3.table
 # - green:10.0.100.30 is removed (this bubbles up the newer endpoint from above)
 # - green:10.0.100.31 becomes inactive
 # - blue:192.168.250.{20,21} are removed (both endpoints were already shadowed)
+# - blue:192.168.250.251 is removed
+# - blue:192.168.250.252 is added
 k8s/add privnet-eps.v2.yaml
+clustermesh/endpoints/delete blue-network/10.245.8.87 endpoint-felidae-cougar-2.json
+clustermesh/endpoints/upsert blue-network/10.245.8.92 endpoint-felidae-cougar-3.json
 db/cmp privnet-mapentries mapentries.v4.table
 
 # Delete original slice. Since its only endpoint is inactive, there should be no changes
@@ -282,6 +291,90 @@ nodeName: crayfish
     ]
 }
 
+-- endpoint-felidae-cougar-1.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.435Z",
+  "flags": {
+    "external": true
+  },
+  "ip": "10.245.8.21",
+  "name": "valid-monkey",
+  "network": {
+    "name": "blue-network",
+    "ip": "192.168.250.200",
+    "mac": "19:60:b8:48:84:b9"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-felidae-cougar-2.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.435Z",
+  "flags": {
+    "external": true
+  },
+  "ip": "10.245.8.87",
+  "name": "resolved-monster",
+  "network": {
+    "name": "blue-network",
+    "ip": "192.168.250.201",
+    "mac": "ad:e7:f2:bf:0b:58"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-felidae-cougar-3.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.435Z",
+  "flags": {
+    "external": true
+  },
+  "ip": "10.245.8.92",
+  "name": "superb-kite",
+  "network": {
+    "name": "blue-network",
+    "ip": "192.168.250.202",
+    "mac": "cd:77:2b:8b:45:a1"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-camelidae-alpaca-1.json --
+{
+  "activatedAt": "2025-06-20T15:53:01.435Z",
+  "flags": {
+    "external": true
+  },
+  "ip": "10.245.9.41",
+  "name": "resolved-monster",
+  "network": {
+    "name": "blue-network",
+    "ip": "192.168.250.201",
+    "mac": "ad:e7:f2:bf:0b:58"
+  },
+  "nodeName": "alpaca",
+  "source": {
+    "cluster": "camelidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
 
 -- mapentries.v1.table --
 Type   Network        CIDR               Nexthop
@@ -292,6 +385,8 @@ E      blue-network   192.168.250.20/32  10.244.5.108
 E      blue-network   192.168.250.21/32  10.244.5.109
 E      blue-network   fd10:0:250::20/128 fd00:10:244:5::108
 E      blue-network   fd10:0:250::21/128 fd00:10:244:5::109
+X      blue-network   192.168.250.200/32 10.245.8.21
+X      blue-network   192.168.250.201/32 10.245.8.87
 
 -- mapentries.v2.table --
 Type   Network        CIDR                  Nexthop
@@ -302,6 +397,8 @@ E      blue-network   192.168.250.20/32     10.244.5.108
 E      blue-network   192.168.250.21/32     10.244.5.109
 E      blue-network   fd10:0:250::20/128    fd00:10:244:5::108
 E      blue-network   fd10:0:250::21/128    fd00:10:244:5::109
+X      blue-network   192.168.250.200/32    10.245.8.21
+X      blue-network   192.168.250.201/32    10.245.8.87
 D      green-network  10.0.100.0/24         172.128.0.7
 D      green-network  fd00:10:100::/64      172.128.0.7
 E      green-network  10.0.100.30/32        10.244.5.110
@@ -319,6 +416,8 @@ E      blue-network   192.168.250.24/32     10.244.5.107
 E      blue-network   fd10:0:250::20/128    fd00:10:244:6::150
 E      blue-network   fd10:0:250::23/128    fd00:10:244:5::109
 E      blue-network   fd10:0:250::24/128    fd00:10:244:5::107
+X      blue-network   192.168.250.200/32    10.245.8.21
+X      blue-network   192.168.250.201/32    10.245.8.87
 D      green-network  10.0.100.0/24         172.128.0.7
 D      green-network  fd00:10:100::/64      172.128.0.7
 E      green-network  10.0.100.30/32        10.244.5.110
@@ -336,6 +435,8 @@ E      blue-network   192.168.250.24/32     10.244.5.107
 E      blue-network   fd10:0:250::20/128    fd00:10:244:6::150
 E      blue-network   fd10:0:250::23/128    fd00:10:244:5::109
 E      blue-network   fd10:0:250::24/128    fd00:10:244:5::107
+X      blue-network   192.168.250.200/32    10.245.8.21
+X      blue-network   192.168.250.202/32    10.245.8.92
 D      green-network  10.0.100.0/24         172.128.0.7
 D      green-network  fd00:10:100::/64      172.128.0.7
 E      green-network  10.0.100.30/32        10.244.6.151
@@ -351,6 +452,8 @@ E      blue-network   192.168.250.24/32     10.244.5.107
 E      blue-network   fd10:0:250::20/128    fd00:10:244:6::150
 E      blue-network   fd10:0:250::23/128    fd00:10:244:5::109
 E      blue-network   fd10:0:250::24/128    fd00:10:244:5::107
+X      blue-network   192.168.250.200/32    10.245.8.21
+X      blue-network   192.168.250.202/32    10.245.8.92
 
 -- mapentries.v6.table --
 Type   Network        CIDR                 Nexthop
@@ -364,6 +467,8 @@ E      blue-network   fd10:0:250::23/128   fd00:10:244:5::109
 E      blue-network   fd10:0:250::24/128   fd00:10:244:5::107
 S      blue-network   0.0.0.0/0            172.128.0.6
 S      blue-network   192.168.252.0/24     172.128.0.6
+X      blue-network   192.168.250.200/32   10.245.8.21
+X      blue-network   192.168.250.202/32   10.245.8.92
 
 -- mapentries.v7.table --
 Type   Network        CIDR                 Nexthop
@@ -372,6 +477,8 @@ D      blue-network   192.168.250.24/32    172.128.0.6
 D      blue-network   fd10:0:250::/64      172.128.0.6
 S      blue-network   0.0.0.0/0            172.128.0.6
 S      blue-network   192.168.252.0/24     172.128.0.6
+X      blue-network   192.168.250.200/32   10.245.8.21
+X      blue-network   192.168.250.202/32   10.245.8.92
 
 -- mapentries.v8.table --
 Type   Network        CIDR                 Nexthop
@@ -380,3 +487,4 @@ D      blue-network   192.168.250.24/32    172.128.0.7
 D      blue-network   fd10:0:250::/64      172.128.0.7
 S      blue-network   0.0.0.0/0            172.128.0.7
 S      blue-network   192.168.252.0/24     172.128.0.7
+X      blue-network   192.168.250.201/32   10.245.9.41

commit 8bfe5bd8752428af423cd94f3553210833db95d3

privnet: do not filter local endpoints in mapentries reconciler
The blamed commit extended the mapentries reconciler to filter out the
external endpoints advertised by non-active INB nodes. However, it shall
not exclude the ones advertised by the local node itself. Let's get that
fixed, and add a new script test to cover that case as well.

Fixes: 685c3c30228a ("privnet: extend mapentries reconciler to account for external endpoints")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

enterprise/pkg/privnet/reconcilers/mapentries.go
@@ -23,7 +23,9 @@ import (
 
 	"github.com/cilium/cilium/enterprise/pkg/privnet/config"
 	"github.com/cilium/cilium/enterprise/pkg/privnet/tables"
+	cmtypes "github.com/cilium/cilium/pkg/clustermesh/types"
 	"github.com/cilium/cilium/pkg/logging/logfields"
+	notypes "github.com/cilium/cilium/pkg/node/types"
 )
 
 var MapEntriesCell = cell.Group(
@@ -52,7 +54,8 @@ type MapEntries struct {
 	log *slog.Logger
 	jg  job.Group
 
-	cfg config.Config
+	cfg   config.Config
+	local tables.INBNode
 
 	db        *statedb.DB
 	networks  statedb.Table[tables.PrivateNetwork]
@@ -75,7 +78,8 @@ func newMapEntries(in struct {
 	Log      *slog.Logger
 	JobGroup job.Group
 
-	Config config.Config
+	Config      config.Config
+	ClusterInfo cmtypes.ClusterInfo
 
 	DB        *statedb.DB
 	Networks  statedb.Table[tables.PrivateNetwork]
@@ -89,6 +93,10 @@ func newMapEntries(in struct {
 		jg:  in.JobGroup,
 
 		cfg: in.Config,
+		local: tables.INBNode{
+			Cluster: tables.ClusterName(in.ClusterInfo.Name),
+			Name:    tables.NodeName(notypes.GetName()),
+		},
 
 		db:        in.DB,
 		networks:  in.Networks,
@@ -375,11 +383,15 @@ func (m *MapEntries) determineActiveEndpointForNetworkIP(txn statedb.ReadTxn, ne
 // shouldSkipExternalEndpointSkip returns whether a given external endpoint should
 // be skipped, that is if either this network has no active INB, or the endpoint
 // is not advertised by the currently active INB. This function always returns
-// false if the endpoint is not external.
+// false if the endpoint is not external, as well as for the external endpoints
+// advertised by the local node.
 func (m *MapEntries) shouldSkipExternalEndpoint(ep tables.Endpoint, privnet tables.SlimPrivateNetwork) bool {
-	return ep.Flags.External && (!privnet.ActiveINB.IP.IsValid() ||
-		tables.ClusterName(ep.Source.Cluster) != privnet.ActiveINB.Cluster ||
-		tables.NodeName(ep.NodeName) != privnet.ActiveINB.Name)
+	return ep.Flags.External &&
+		(!privnet.ActiveINB.IP.IsValid() ||
+			tables.ClusterName(ep.Source.Cluster) != privnet.ActiveINB.Cluster ||
+			tables.NodeName(ep.NodeName) != privnet.ActiveINB.Name) &&
+		(tables.ClusterName(ep.Source.Cluster) != m.local.Cluster ||
+			tables.NodeName(ep.NodeName) != m.local.Name)
 }
 
 // findConflictingNATEntriesForActiveEP checks the endpoint table to find all endpoints

enterprise/pkg/privnet/tests/testdata/mapentries-bridge.txtar
@@ -1,0 +1,255 @@
+#! --private-networks-enabled --private-networks-mode=bridge
+
+hive start
+
+# Populate the initial state.
+db/insert devices device.eth1.yaml
+db/insert devices device.eth2.yaml
+k8s/add icpn-1.yaml icpn-2.yaml
+
+# Create a few local workloads.
+db/insert privnet-local-workloads local-workload-optimum-mouse.yaml
+db/insert privnet-local-workloads local-workload-shining-burro.yaml
+
+# Ingest the endpoints from endpointslices and clustermesh.
+k8s/add privnet-eps.yaml
+clustermesh/endpoints/upsert maggot/10.245.8.21 endpoint-maggot-cougar-1.json
+clustermesh/endpoints/upsert maggot/10.245.8.22 endpoint-maggot-cougar-2.json
+clustermesh/endpoints/upsert firefly/10.245.8.31 endpoint-firefly-cougar-1.json
+clustermesh/endpoints/upsert firefly/10.245.8.32 endpoint-firefly-lion-1.json
+clustermesh/endpoints/sync
+
+# Assert that the mapentries table got properly reconciled. The only external
+# endpoint present should be the one advertised by the local node.
+db/cmp privnet-mapentries mapentries.v1.table
+
+-- device.eth1.yaml --
+index: 10
+name: eth1
+type: veth
+selected: true
+
+-- device.eth1.v2.yaml --
+index: 10
+name: eth1
+type: veth
+selected: false
+notselectedreason: not matching user filter
+
+-- device.eth2.yaml --
+index: 11
+name: eth2
+type: veth
+selected: true
+
+-- icpn-1.yaml --
+apiVersion: isovalent.com/v1alpha1
+kind: ClusterwidePrivateNetwork
+metadata:
+  name: maggot
+spec:
+  interface:
+    name: eth1
+  routes: []
+  subnets:
+  - cidr: 192.168.250.0/24
+
+-- icpn-2.yaml --
+apiVersion: isovalent.com/v1alpha1
+kind: ClusterwidePrivateNetwork
+metadata:
+  name: firefly
+spec:
+  interface:
+    name: eth2
+  routes: []
+  subnets:
+  - cidr: 192.168.251.0/24
+
+-- local-workload-optimum-mouse.yaml --
+endpointid: 11
+namespace: foo
+endpoint:
+    addressing:
+      ipv4: 10.245.8.20
+    name: optimum-mouse
+interface:
+    addressing:
+      ipv4: 192.168.250.20
+    mac: 19:60:b8:48:84:20
+    network: maggot
+activatedat: 2025-06-20T12:40:00Z
+flags:
+    external: false
+
+-- local-workload-shining-burro.yaml --
+endpointid: 16
+namespace: foo
+endpoint:
+    addressing:
+      ipv4: 10.245.8.39
+    name: shining-burro
+interface:
+    addressing:
+      ipv4: 192.168.251.39
+    mac: 19:60:b8:48:84:39
+    network: firefly
+activatedat: 2025-06-20T12:40:00Z
+flags:
+    external: true
+
+-- privnet-eps.yaml --
+apiVersion: isovalent.com/v1alpha1
+kind: PrivateNetworkEndpointSlice
+metadata:
+  namespace: default
+  name: privnet-eps-1
+endpoints:
+- activatedAt: "2025-06-20T15:54:04.765000Z"
+  endpoint:
+    addressing:
+      ipv4: 10.245.8.30
+    name: moved-racer
+  interface:
+    addressing:
+      ipv4: 192.168.251.30
+    mac: 19:60:b8:48:84:30
+    network: firefly
+- activatedAt: "2025-06-20T15:54:03.234000Z"
+  endpoint:
+    addressing:
+      ipv4: 10.245.8.39
+    name: destined-bluejay
+  flags:
+    external: true
+  interface:
+    addressing:
+      ipv4: 192.168.251.39
+    mac: 19:60:b8:48:84:39
+    network: maggot
+nodeName: foobar-worker-2
+
+-- endpoint-maggot-cougar-1.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.421Z",
+  "ip": "10.245.8.21",
+  "name": "valid-monkey",
+  "network": {
+    "name": "maggot",
+    "ip": "192.168.250.21",
+    "mac": "19:60:b8:48:84:21"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-maggot-cougar-2.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.422Z",
+  "ip": "10.245.8.22",
+  "name": "intense-phoenix",
+  "network": {
+    "name": "maggot",
+    "ip": "192.168.250.22",
+    "mac": "19:60:b8:48:84:22"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-maggot-cougar-3.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.423Z",
+  "ip": "10.245.8.23",
+  "name": "charming-finch",
+  "network": {
+    "name": "maggot",
+    "ip": "192.168.250.23",
+    "mac": "19:60:b8:48:84:23"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-firefly-cougar-1.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.431Z",
+  "ip": "10.245.8.31",
+  "name": "eminent-griffon",
+  "network": {
+    "name": "firefly",
+    "ip": "192.168.251.31",
+    "mac": "19:60:b8:48:84:31"
+  },
+  "nodeName": "cougar",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-firefly-lion-1.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.432Z",
+  "ip": "10.245.8.32",
+  "name": "grateful-raccoon",
+  "network": {
+    "name": "firefly",
+    "ip": "192.168.251.32",
+    "mac": "19:60:b8:48:84:32"
+  },
+  "nodeName": "lion",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- endpoint-firefly-lion-2.json --
+{
+  "activatedAt": "2025-06-20T15:52:01.433Z",
+  "ip": "10.245.8.33",
+  "name": "quality-bass",
+  "network": {
+    "name": "firefly",
+    "ip": "192.168.251.33",
+    "mac": "19:60:b8:48:84:33"
+  },
+  "nodeName": "lion",
+  "source": {
+    "cluster": "felidae",
+    "namespace": "gopher",
+    "name": "privnet-eps-1"
+  }
+}
+
+-- health.v1.table --
+Instance                  Network   Health
+default/foobar-worker-1   firefly   Healthy, Confirmed
+default/foobar-worker-1   maggot    Healthy, Confirmed
+
+-- mapentries.v1.table --
+Type   Network   CIDR                Nexthop
+D      firefly   192.168.251.0/24    0.0.0.0
+E      firefly   192.168.251.30/32   10.245.8.30
+E      firefly   192.168.251.31/32   10.245.8.31
+E      firefly   192.168.251.32/32   10.245.8.32
+X      firefly   192.168.251.39/32   10.245.8.39
+D      maggot    192.168.250.0/24    0.0.0.0
+E      maggot    192.168.250.20/32   10.245.8.20
+E      maggot    192.168.250.21/32   10.245.8.21
+E      maggot    192.168.250.22/32   10.245.8.22