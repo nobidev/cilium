d11a68798ba848656874c5d606ab6c6915bad4ab
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2025-11-04 07:56:57 -0800

    bgp: Add conversions/validations/defaulting for IsovalentBGPPolicy
    Add conversion functions for CRD => internal policy and validation and
    defaulting function. Conversion is an error-free conversion that ignores
    invalid fields. To make it right, users must validate and default the
    object beforehand.
    
    The validation and defauting only covers the one that cannot be
    performed in the API server.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

---
+++ enterprise/pkg/bgpv1/types/conversions.go
@@ -0,0 +1,216 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package types
+
+import (
+	"errors"
+	"net/netip"
+
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	v1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1"
+)
+
+func ToRoutePolicy(p *v1.BGPImportPolicy, name string, neighbor netip.Addr, family types.Family) *types.RoutePolicy {
+	ret := &types.RoutePolicy{
+		Name:       name,
+		Type:       types.RoutePolicyTypeImport,
+		Statements: make([]*types.RoutePolicyStatement, 0, len(p.Statements)),
+	}
+	for _, statement := range p.Statements {
+		ret.Statements = append(ret.Statements, ToRoutePolicyStatement(&statement, neighbor, family))
+	}
+	return ret
+}
+
+func ValidateAndDefaultImportPolicy(p *v1.BGPImportPolicy, family v2.CiliumBGPFamily) error {
+	var errs error
+	for i, stmt := range p.Statements {
+		err := ValidateAndDefaultPolicyStatement(&stmt, family)
+		if err != nil {
+			errs = errors.Join(errs, err)
+			continue
+		}
+		p.Statements[i] = stmt
+	}
+	return errs
+}
+
+func ToRoutePolicyStatement(s *v1.BGPPolicyStatement, neighbor netip.Addr, family types.Family) *types.RoutePolicyStatement {
+	return &types.RoutePolicyStatement{
+		Conditions: ToRoutePolicyConditions(&s.Conditions, neighbor, family),
+		Actions:    ToRoutePolicyActions(&s.Actions),
+	}
+}
+
+func ValidateAndDefaultPolicyStatement(s *v1.BGPPolicyStatement, family v2.CiliumBGPFamily) error {
+	var errs error
+	if err := ValidateAndDefaultPolicyConditions(&s.Conditions, family); err != nil {
+		errs = errors.Join(errs, err)
+	}
+	return errs
+}
+
+func ToRoutePolicyConditions(c *v1.BGPPolicyConditions, neighbor netip.Addr, family types.Family) types.RoutePolicyConditions {
+	prefixMatches := []*types.RoutePolicyPrefixMatch{}
+
+	// We only render PrefixesV4 for ipv4-unicast
+	if c.PrefixesV4 != nil && (family == types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast}) {
+		for _, match := range c.PrefixesV4.Matches {
+			p, err := netip.ParsePrefix(match.Prefix)
+			if err != nil {
+				// Impossible as long as the k8s validation is in place
+				continue
+			}
+			if match.MaxLen == nil {
+				// Don't render incomplete match. Should call defaulting before conversion.
+				continue
+			}
+			if match.MinLen == nil {
+				// Don't render incomplete match. Should call defaulting before conversion.
+				continue
+			}
+			prefixMatches = append(prefixMatches, &types.RoutePolicyPrefixMatch{
+				CIDR:         p,
+				PrefixLenMax: int(*match.MaxLen),
+				PrefixLenMin: int(*match.MinLen),
+			})
+		}
+	}
+
+	// We only render PrefixesV6 if the families include IPv6 unicast
+	if c.PrefixesV6 != nil && (family == types.Family{Afi: types.AfiIPv6, Safi: types.SafiUnicast}) {
+		for _, match := range c.PrefixesV6.Matches {
+			p, err := netip.ParsePrefix(match.Prefix)
+			if err != nil {
+				// Impossible as long as the k8s validation is in place
+				continue
+			}
+			if match.MaxLen == nil {
+				// Don't render incomplete match. Should call defaulting before conversion.
+				continue
+			}
+			if match.MinLen == nil {
+				// Don't render incomplete match. Should call defaulting before conversion.
+				continue
+			}
+			prefixMatches = append(prefixMatches, &types.RoutePolicyPrefixMatch{
+				CIDR:         p,
+				PrefixLenMax: int(*match.MaxLen),
+				PrefixLenMin: int(*match.MinLen),
+			})
+		}
+	}
+
+	return types.RoutePolicyConditions{
+		MatchNeighbors: []netip.Addr{neighbor},
+		MatchPrefixes:  prefixMatches,
+		MatchFamilies:  []types.Family{family},
+	}
+}
+
+func ValidateAndDefaultPolicyConditions(c *v1.BGPPolicyConditions, family v2.CiliumBGPFamily) error {
+	var (
+		errs     error
+		hasMatch bool
+	)
+	if c.PrefixesV4 != nil && (family.Afi == "ipv4" && family.Safi == "unicast") {
+		hasMatch = true
+		if err := ValidateAndDefaultPrefixesV4Condition(c.PrefixesV4); err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+	if c.PrefixesV6 != nil && (family.Afi == "ipv6" && family.Safi == "unicast") {
+		hasMatch = true
+		if err := ValidateAndDefaultPrefixesV6Condition(c.PrefixesV6); err != nil {
+			errs = errors.Join(errs, err)
+		}
+	}
+	if !hasMatch {
+		errs = errors.Join(errs, errors.New("no usable match in the conditions"))
+	}
+	return errs
+}
+
+func ValidateAndDefaultPrefixesV4Condition(c *v1.PrefixesV4Condition) error {
+	var errs error
+	for i, match := range c.Matches {
+		if err := ValidateAndDefaultPrefixV4Match(&match); err != nil {
+			errs = errors.Join(errs, err)
+			continue
+		}
+		c.Matches[i] = match
+	}
+	return errs
+}
+
+func ValidateAndDefaultPrefixV4Match(m *v1.PrefixV4Match) error {
+	p, err := netip.ParsePrefix(m.Prefix)
+	if err != nil {
+		return err
+	}
+	pLen := uint8(p.Bits())
+	if m.MaxLen == nil {
+		m.MaxLen = &pLen
+	}
+	if m.MinLen == nil {
+		m.MinLen = m.MaxLen
+	}
+	if *m.MinLen > *m.MaxLen {
+		return errors.New("minLen must be less than or equal to maxLen")
+	}
+	return nil
+}
+
+func ValidateAndDefaultPrefixesV6Condition(c *v1.PrefixesV6Condition) error {
+	var errs error
+	for i, match := range c.Matches {
+		if err := ValidateAndDefaultPrefixV6Match(&match); err != nil {
+			errs = errors.Join(errs, err)
+			continue
+		}
+		c.Matches[i] = match
+	}
+	return errs
+}
+
+func ValidateAndDefaultPrefixV6Match(m *v1.PrefixV6Match) error {
+	p, err := netip.ParsePrefix(m.Prefix)
+	if err != nil {
+		return err
+	}
+	pLen := uint8(p.Bits())
+	if m.MaxLen == nil {
+		m.MaxLen = &pLen
+	}
+	if m.MinLen == nil {
+		m.MinLen = m.MaxLen
+	}
+	if *m.MinLen > *m.MaxLen {
+		return errors.New("minLen must be less than or equal to maxLen")
+	}
+	return nil
+}
+
+func ToRoutePolicyActions(a *v1.BGPPolicyActions) types.RoutePolicyActions {
+	return types.RoutePolicyActions{
+		RouteAction: ToRoutePolicyAction(a.RouteAction),
+	}
+}
+
+func ToRoutePolicyAction(a v1.BGPRouteAction) types.RoutePolicyAction {
+	switch a {
+	case v1.BGPRouteActionAccept:
+		return types.RoutePolicyActionAccept
+	default:
+		return types.RoutePolicyActionNone
+	}
+}
---
+++ enterprise/pkg/bgpv1/types/conversions_test.go
@@ -0,0 +1,432 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package types
+
+import (
+	"net/netip"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"k8s.io/utils/ptr"
+
+	"github.com/cilium/cilium/pkg/bgpv1/types"
+	v2 "github.com/cilium/cilium/pkg/k8s/apis/cilium.io/v2"
+	v1 "github.com/cilium/cilium/pkg/k8s/apis/isovalent.com/v1"
+)
+
+func TestToRoutePolicy(t *testing.T) {
+	policy := &v1.BGPImportPolicy{
+		Statements: []v1.BGPPolicyStatement{
+			{
+				Conditions: v1.BGPPolicyConditions{},
+				Actions:    v1.BGPPolicyActions{},
+			},
+		},
+	}
+	rp := ToRoutePolicy(
+		policy,
+		"test",
+		netip.MustParseAddr("10.0.0.1"),
+		types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast},
+	)
+	require.NotNil(t, rp)
+	require.Equal(t, "test", rp.Name)
+	require.Equal(t, types.RoutePolicyTypeImport, rp.Type)
+	require.Len(t, rp.Statements, 1)
+}
+
+func TestToRoutePolicyStatement(t *testing.T) {
+	statement := v1.BGPPolicyStatement{
+		Conditions: v1.BGPPolicyConditions{},
+		Actions:    v1.BGPPolicyActions{},
+	}
+	rps := ToRoutePolicyStatement(
+		&statement,
+		netip.MustParseAddr("10.0.0.1"),
+		types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast},
+	)
+	require.NotNil(t, rps)
+}
+
+func TestToRoutePolicyConditions(t *testing.T) {
+	t.Run("Empty", func(t *testing.T) {
+		conditions := v1.BGPPolicyConditions{}
+		rpc := ToRoutePolicyConditions(
+			&conditions,
+			netip.MustParseAddr("10.0.0.1"),
+			types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast},
+		)
+		require.NotNil(t, rpc)
+
+		// Ensure the neighbors and families are copied without modification
+		require.Equal(t, []netip.Addr{netip.MustParseAddr("10.0.0.1")}, rpc.MatchNeighbors)
+		require.Equal(t, []types.Family{{Afi: types.AfiIPv4, Safi: types.SafiUnicast}}, rpc.MatchFamilies)
+	})
+
+	t.Run("PrefixesV4", func(t *testing.T) {
+		conditions := v1.BGPPolicyConditions{
+			PrefixesV4: &v1.PrefixesV4Condition{
+				MatchType: v1.BGPPolicyMatchTypeOr,
+				Matches: []v1.PrefixV4Match{
+					{
+						Prefix: "10.0.0.0/24",
+						MaxLen: ptr.To(uint8(32)),
+						MinLen: ptr.To(uint8(24)),
+					},
+				},
+			},
+		}
+		rpc := ToRoutePolicyConditions(
+			&conditions,
+			netip.MustParseAddr("10.0.0.1"),
+			types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast},
+		)
+		require.NotNil(t, rpc)
+		require.Len(t, rpc.MatchPrefixes, 1)
+		require.Equal(t, netip.MustParsePrefix("10.0.0.0/24"), rpc.MatchPrefixes[0].CIDR)
+		require.Equal(t, 32, rpc.MatchPrefixes[0].PrefixLenMax)
+		require.Equal(t, 24, rpc.MatchPrefixes[0].PrefixLenMin)
+	})
+
+	t.Run("PrefixesV6", func(t *testing.T) {
+		conditions := v1.BGPPolicyConditions{
+			PrefixesV6: &v1.PrefixesV6Condition{
+				MatchType: v1.BGPPolicyMatchTypeOr,
+				Matches: []v1.PrefixV6Match{
+					{
+						Prefix: "fd00::/64",
+						MaxLen: ptr.To(uint8(128)),
+						MinLen: ptr.To(uint8(64)),
+					},
+				},
+			},
+		}
+		rpc := ToRoutePolicyConditions(
+			&conditions,
+			netip.MustParseAddr("2001:db8::1"),
+			types.Family{Afi: types.AfiIPv6, Safi: types.SafiUnicast},
+		)
+		require.NotNil(t, rpc)
+		require.Len(t, rpc.MatchPrefixes, 1)
+		require.Equal(t, netip.MustParsePrefix("fd00::/64"), rpc.MatchPrefixes[0].CIDR)
+		require.Equal(t, 128, rpc.MatchPrefixes[0].PrefixLenMax)
+		require.Equal(t, 64, rpc.MatchPrefixes[0].PrefixLenMin)
+	})
+
+	t.Run("DualStackPolicy", func(t *testing.T) {
+		conditions := v1.BGPPolicyConditions{
+			PrefixesV4: &v1.PrefixesV4Condition{
+				MatchType: v1.BGPPolicyMatchTypeOr,
+				Matches: []v1.PrefixV4Match{
+					{
+						Prefix: "10.0.0.0/24",
+						MaxLen: ptr.To(uint8(32)),
+						MinLen: ptr.To(uint8(24)),
+					},
+				},
+			},
+			PrefixesV6: &v1.PrefixesV6Condition{
+				MatchType: v1.BGPPolicyMatchTypeOr,
+				Matches: []v1.PrefixV6Match{
+					{
+						Prefix: "fd00::/64",
+						MaxLen: ptr.To(uint8(128)),
+						MinLen: ptr.To(uint8(64)),
+					},
+				},
+			},
+		}
+
+		// Test for IPv4 family
+		rpc := ToRoutePolicyConditions(
+			&conditions,
+			netip.MustParseAddr("10.0.0.1"),
+			types.Family{Afi: types.AfiIPv4, Safi: types.SafiUnicast},
+		)
+		require.NotNil(t, rpc)
+		require.Len(t, rpc.MatchPrefixes, 1)
+		require.Equal(t, netip.MustParsePrefix("10.0.0.0/24"), rpc.MatchPrefixes[0].CIDR)
+		require.Equal(t, 32, rpc.MatchPrefixes[0].PrefixLenMax)
+		require.Equal(t, 24, rpc.MatchPrefixes[0].PrefixLenMin)
+
+		// Test for IPv6 family
+		rpc = ToRoutePolicyConditions(
+			&conditions,
+			netip.MustParseAddr("fd00::1"),
+			types.Family{Afi: types.AfiIPv6, Safi: types.SafiUnicast},
+		)
+		require.NotNil(t, rpc)
+		require.Len(t, rpc.MatchPrefixes, 1)
+		require.Equal(t, netip.MustParsePrefix("fd00::/64"), rpc.MatchPrefixes[0].CIDR)
+		require.Equal(t, 128, rpc.MatchPrefixes[0].PrefixLenMax)
+		require.Equal(t, 64, rpc.MatchPrefixes[0].PrefixLenMin)
+	})
+}
+
+func TestToRoutePolicyActions(t *testing.T) {
+	cases := []struct {
+		name           string
+		action         v1.BGPRouteAction
+		expectedAction types.RoutePolicyAction
+	}{
+		{
+			name:           "Empty => None",
+			action:         "",
+			expectedAction: types.RoutePolicyActionNone,
+		},
+		{
+			name:           "Unexpected => None",
+			action:         "foo",
+			expectedAction: types.RoutePolicyActionNone,
+		},
+		{
+			name:           "Accept",
+			action:         v1.BGPRouteActionAccept,
+			expectedAction: types.RoutePolicyActionAccept,
+		},
+	}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			actions := v1.BGPPolicyActions{
+				RouteAction: tt.action,
+			}
+			rpa := ToRoutePolicyActions(&actions)
+			require.NotNil(t, rpa)
+			require.Equal(t, tt.expectedAction, rpa.RouteAction)
+		})
+	}
+}
+
+func TestBGPConditionsValidateAndDefault(t *testing.T) {
+	tests := []struct {
+		name       string
+		conditions v1.BGPPolicyConditions
+		family     v2.CiliumBGPFamily
+		isInvalid  bool
+	}{
+		{
+			name: "Valid IPv4",
+			conditions: v1.BGPPolicyConditions{
+				PrefixesV4: &v1.PrefixesV4Condition{
+					MatchType: v1.BGPPolicyMatchTypeOr,
+					Matches: []v1.PrefixV4Match{
+						{
+							Prefix: "10.0.0.0/24",
+						},
+					},
+				},
+			},
+			family:    v2.CiliumBGPFamily{Afi: "ipv4", Safi: "unicast"},
+			isInvalid: false,
+		},
+		{
+			name: "Valid IPv6",
+			conditions: v1.BGPPolicyConditions{
+				PrefixesV6: &v1.PrefixesV6Condition{
+					MatchType: v1.BGPPolicyMatchTypeOr,
+					Matches: []v1.PrefixV6Match{
+						{
+							Prefix: "fd00::/64",
+						},
+					},
+				},
+			},
+			family:    v2.CiliumBGPFamily{Afi: "ipv6", Safi: "unicast"},
+			isInvalid: false,
+		},
+		{
+			name:       "Invalid no match",
+			conditions: v1.BGPPolicyConditions{},
+			family:     v2.CiliumBGPFamily{Afi: "ipv4", Safi: "unicast"},
+			isInvalid:  true,
+		},
+		{
+			name: "Invalid IPv4 prefix in IPv6 family, no matching criteria",
+			conditions: v1.BGPPolicyConditions{
+				PrefixesV4: &v1.PrefixesV4Condition{
+					MatchType: v1.BGPPolicyMatchTypeOr,
+					Matches: []v1.PrefixV4Match{
+						{
+							Prefix: "10.0.0.0/24",
+						},
+					},
+				},
+			},
+			family:    v2.CiliumBGPFamily{Afi: "ipv6", Safi: "unicast"},
+			isInvalid: true,
+		},
+		{
+			name: "Invalid IPv6 prefix in IPv4 family, no matching criteria",
+			conditions: v1.BGPPolicyConditions{
+				PrefixesV6: &v1.PrefixesV6Condition{
+					MatchType: v1.BGPPolicyMatchTypeOr,
+					Matches: []v1.PrefixV6Match{
+						{
+							Prefix: "fd00::/64",
+						},
+					},
+				},
+			},
+			family:    v2.CiliumBGPFamily{Afi: "ipv4", Safi: "unicast"},
+			isInvalid: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ValidateAndDefaultPolicyConditions(&tt.conditions, tt.family)
+			if tt.isInvalid {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
+
+func TestPrefixV4MatchValidateAndDefault(t *testing.T) {
+	tests := []struct {
+		name       string
+		conditions v1.PrefixV4Match
+		expected   v1.PrefixV4Match
+		isInvalid  bool
+	}{
+		{
+			name: "Valid prefix no min/max",
+			conditions: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+			},
+			expected: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+				MaxLen: ptr.To(uint8(24)),
+				MinLen: ptr.To(uint8(24)),
+			},
+			isInvalid: false,
+		},
+		{
+			name: "Valid prefix no min",
+			conditions: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+				MaxLen: ptr.To(uint8(32)),
+			},
+			expected: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+				MinLen: ptr.To(uint8(32)),
+				MaxLen: ptr.To(uint8(32)),
+			},
+			isInvalid: false,
+		},
+		{
+			name:       "Invalid no prefixes",
+			conditions: v1.PrefixV4Match{},
+			isInvalid:  true,
+		},
+		{
+			name: "Invalid min greater than max",
+			conditions: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+				MaxLen: ptr.To(uint8(26)),
+				MinLen: ptr.To(uint8(28)),
+			},
+			isInvalid: true,
+		},
+		{
+			name: "Invalid min greater than max by defaulting",
+			conditions: v1.PrefixV4Match{
+				Prefix: "10.0.0.0/24",
+				// This will become larger than the maxLen
+				// which defaults to the prefix length.
+				MinLen: ptr.To(uint8(26)),
+			},
+			isInvalid: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ValidateAndDefaultPrefixV4Match(&tt.conditions)
+			if tt.isInvalid {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expected, tt.conditions)
+			}
+		})
+	}
+}
+
+func TestPrefixV6MatchValidateAndDefault(t *testing.T) {
+	tests := []struct {
+		name       string
+		conditions v1.PrefixV6Match
+		expected   v1.PrefixV6Match
+		isInvalid  bool
+	}{
+		{
+			name: "Valid prefix no min/max",
+			conditions: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+			},
+			expected: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+				MaxLen: ptr.To(uint8(64)),
+				MinLen: ptr.To(uint8(64)),
+			},
+			isInvalid: false,
+		},
+		{
+			name: "Valid prefix no min",
+			conditions: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+				MaxLen: ptr.To(uint8(96)),
+			},
+			expected: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+				MinLen: ptr.To(uint8(96)),
+				MaxLen: ptr.To(uint8(96)),
+			},
+			isInvalid: false,
+		},
+		{
+			name:       "Invalid no prefixes",
+			conditions: v1.PrefixV6Match{},
+			isInvalid:  true,
+		},
+		{
+			name: "Invalid min greater than max",
+			conditions: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+				MaxLen: ptr.To(uint8(96)),
+				MinLen: ptr.To(uint8(128)),
+			},
+			isInvalid: true,
+		},
+		{
+			name: "Invalid min greater than max by defaulting",
+			conditions: v1.PrefixV6Match{
+				Prefix: "fd00::/64",
+				// This will become larger than the maxLen
+				// which defaults to the prefix length.
+				MinLen: ptr.To(uint8(96)),
+			},
+			isInvalid: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ValidateAndDefaultPrefixV6Match(&tt.conditions)
+			if tt.isInvalid {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tt.expected, tt.conditions)
+			}
+		})
+	}
+}
