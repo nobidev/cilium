nobidev/cilium

commit a48c7d4276b7e3c9bb3792321e9e1e83b7d41e68

Remove CiliumMesh
The main motivation for the removal that it's going to be replaced by
INB + PrivNet, and that we still keep spending resources in reviewing
PRs relevant to CiliumMesh and investigating CiliumMesh CI flakes.

After ther removal, some datapath hooks are no longer used. We will
consider removing them in OSS separately from this PR.

I did not sync enterprise/olm/manifests, as there is a scheduled job to
do so, as noted in enterprise/olm/README.md:

    - A scheduled workflow, [OLM
      Sync](../../.github/workflows/enterprise-olm-sync.yaml) runs
      weekly to synchronize the manifest files used by CLife with
      changes made to the helm manifests. This workflow creates a pull
      request with the required changes.

Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

CODEOWNERS
@@ -698,8 +698,6 @@ Makefile* @cilium/build
 # - @isovalent/backporters
 #   Responsible for maintaining tools, workflows and processes for managing
 #   backports.
-# - @isovalent/cilium-mesh
-#   Responsible for maintaining the enterprise-specific Cilium Mesh feature
 # - @isovalent/cli:
 #   Provide user experience feedback on changes to Command-Line Interfaces.
 #   These owners are a stand-in for the user community to bring a user
@@ -826,14 +824,12 @@ Makefile* @cilium/build
 /.github/workflows/enterprise-panda-scale.yaml @isovalent/scalability-performance
 /.github/workflows/enterprise-release-cilium-cli.yaml @isovalent/cli
 /.github/workflows/enterprise-*srv6*.yaml @isovalent/traffic-engineering
-/.github/workflows/enterprise-*cilium-mesh*.yaml @isovalent/cilium-mesh
 /.github/workflows/enterprise-fqdn-ha-perf.yaml @isovalent/dns-proxy @isovalent/scalability-performance
 /.github/workflows/enterprise-scale-encryption-policy.yaml @isovalent/encryption
 /.github/workflows/release-hubble-cli.yaml @isovalent/hubble
 /.github/workflows/enterprise-encryption-policy.yaml @isovalent/encryption
 /bpf/enterprise_* @isovalent/cilium-datapath
 /bpf/lib/enterprise_* @isovalent/cilium-datapath
-/bpf/lib/enterprise_cilium_mesh.h @isovalent/cilium-mesh
 /bpf/lib/enterprise_encrypt.h @isovalent/encryption
 /bpf/lib/enterprise_ext_eps_policy.h @isovalent/private-networking
 /bpf/complexity-tests/**/**/*enterprise_*.txt @isovalent/cilium-datapath
@@ -862,13 +858,10 @@ Makefile* @cilium/build
 /cilium-cli/enterprise/hooks/connectivity_ingress.go @isovalent/service-mesh
 /cilium-cli/enterprise/hooks/manifests/ingress-policy/ @isovalent/service-mesh
 /cilium-cli/enterprise/hooks/sysdump/timescape*.go @isovalent/timescape
-/contrib/scripts/enterprise-cilium-mesh-smoke-test.sh @isovalent/cilium-mesh
 /contrib/scripts/enterprise-codeowners.sh @isovalent/core-structure
 /contrib/scripts/enterprise-k8s-manifests-gen.sh @isovalent/sig-k8s
-/contrib/scripts/enterprise-kind-cilium-mesh.sh @isovalent/cilium-mesh
 /contrib/scripts/enterprise-meditation.sh @isovalent/core-structure
 /contrib/scripts/enterprise-testowners.sh @isovalent/core-structure
-/contrib/testing/enterprise-cilium-install-cluster*.yaml @isovalent/cilium-mesh
 /contrib/testing/enterprise-kind.yaml @isovalent/infrastructure
 /contrib/testing/enterprise-kind-loadbalancer.yaml @isovalent/isovalent-loadbalancer
 /contrib/testing/kind-egressgwha-values.yaml @isovalent/egress-gateway
@@ -904,7 +897,6 @@ Makefile* @cilium/build
 /enterprise/operator/k8s/ @isovalent/sig-k8s
 /enterprise/operator/pkg/bfd/ @isovalent/traffic-engineering
 /enterprise/operator/pkg/bgpv2/ @isovalent/traffic-engineering
-/enterprise/operator/pkg/ciliummesh @isovalent/cilium-mesh
 /enterprise/operator/pkg/lb/ @isovalent/isovalent-loadbalancer
 /enterprise/operator/pkg/multinetwork/ @isovalent/multi-network
 /enterprise/operator/pkg/networkpolicy/ @isovalent/policy-identity-networking
@@ -914,7 +906,6 @@ Makefile* @cilium/build
 /enterprise/pkg/bfd/ @isovalent/traffic-engineering
 /enterprise/pkg/bgpv1/ @isovalent/traffic-engineering
 /enterprise/pkg/config/cell.go @isovalent/cilium-agent
-/enterprise/pkg/ciliummesh @isovalent/cilium-mesh
 /enterprise/pkg/client @isovalent/cilium-agent @isovalent/cni
 /enterprise/pkg/clustermesh/ @isovalent/clustermesh
 /enterprise/pkg/egressgatewayha/ @isovalent/egress-gateway
@@ -972,10 +963,8 @@ Makefile* @cilium/build
 /pkg/clustermesh/**/enterprise_* @isovalent/clustermesh
 /pkg/datapath/**/enterprise*.go @isovalent/cilium-datapath
 /pkg/datapath/ipcache/enterprise_listener.go @isovalent/clustermesh
-/pkg/endpoint/enterprise_endpoint.go @isovalent/cilium-agent
 /pkg/fqdn/**/*enterprise* @isovalent/dns-proxy
 /pkg/maps/srv6map/*enterprise* @isovalent/traffic-engineering
-/pkg/labels/*enterprise* @isovalent/policy-identity-networking
 /pkg/metrics/enterprise_* @isovalent/cni
 /pkg/metrics/features/*enterprise*.go @isovalent/metrics
 /pkg/metrics/features/**/*enterprise*.go @isovalent/metrics

TESTOWNERS.enterprise
@@ -374,14 +374,12 @@
 /.github/workflows/enterprise-panda-scale.yaml @isovalent/scalability-performance
 /.github/workflows/enterprise-release-cilium-cli.yaml @isovalent/cli
 /.github/workflows/enterprise-*srv6*.yaml @isovalent/traffic-engineering
-/.github/workflows/enterprise-*cilium-mesh*.yaml @isovalent/cilium-mesh
 /.github/workflows/enterprise-fqdn-ha-perf.yaml @isovalent/dns-proxy @isovalent/scalability-performance
 /.github/workflows/enterprise-scale-encryption-policy.yaml @isovalent/encryption
 /.github/workflows/release-hubble-cli.yaml @isovalent/hubble
 /.github/workflows/enterprise-encryption-policy.yaml @isovalent/encryption
 /bpf/enterprise_* @isovalent/cilium-datapath
 /bpf/lib/enterprise_* @isovalent/cilium-datapath
-/bpf/lib/enterprise_cilium_mesh.h @isovalent/cilium-mesh
 /bpf/lib/enterprise_encrypt.h @isovalent/encryption
 /bpf/lib/enterprise_ext_eps_policy.h @isovalent/private-networking
 /bpf/complexity-tests/**/**/*enterprise_*.txt @isovalent/cilium-datapath
@@ -410,10 +408,7 @@
 /cilium-cli/enterprise/hooks/connectivity_ingress.go @isovalent/service-mesh
 /cilium-cli/enterprise/hooks/manifests/ingress-policy/ @isovalent/service-mesh
 /cilium-cli/enterprise/hooks/sysdump/timescape*.go @isovalent/timescape
-/contrib/scripts/enterprise-cilium-mesh-smoke-test.sh @isovalent/cilium-mesh
 /contrib/scripts/enterprise-k8s-manifests-gen.sh @isovalent/sig-k8s
-/contrib/scripts/enterprise-kind-cilium-mesh.sh @isovalent/cilium-mesh
-/contrib/testing/enterprise-cilium-install-cluster*.yaml @isovalent/cilium-mesh
 /contrib/testing/enterprise-kind.yaml @isovalent/infrastructure
 /contrib/testing/enterprise-kind-loadbalancer.yaml @isovalent/isovalent-loadbalancer
 /contrib/testing/kind-egressgwha-values.yaml @isovalent/egress-gateway
@@ -447,7 +442,6 @@
 /enterprise/operator/k8s/ @isovalent/sig-k8s
 /enterprise/operator/pkg/bfd/ @isovalent/traffic-engineering
 /enterprise/operator/pkg/bgpv2/ @isovalent/traffic-engineering
-/enterprise/operator/pkg/ciliummesh @isovalent/cilium-mesh
 /enterprise/operator/pkg/lb/ @isovalent/isovalent-loadbalancer
 /enterprise/operator/pkg/multinetwork/ @isovalent/multi-network
 /enterprise/operator/pkg/networkpolicy/ @isovalent/policy-identity-networking
@@ -457,7 +451,6 @@
 /enterprise/pkg/bfd/ @isovalent/traffic-engineering
 /enterprise/pkg/bgpv1/ @isovalent/traffic-engineering
 /enterprise/pkg/config/cell.go @isovalent/cilium-agent
-/enterprise/pkg/ciliummesh @isovalent/cilium-mesh
 /enterprise/pkg/client @isovalent/cilium-agent @isovalent/cni
 /enterprise/pkg/clustermesh/ @isovalent/clustermesh
 /enterprise/pkg/egressgatewayha/ @isovalent/egress-gateway
@@ -513,10 +506,8 @@
 /pkg/clustermesh/**/enterprise_* @isovalent/clustermesh
 /pkg/datapath/**/enterprise*.go @isovalent/cilium-datapath
 /pkg/datapath/ipcache/enterprise_listener.go @isovalent/clustermesh
-/pkg/endpoint/enterprise_endpoint.go @isovalent/cilium-agent
 /pkg/fqdn/**/*enterprise* @isovalent/dns-proxy
 /pkg/maps/srv6map/*enterprise* @isovalent/traffic-engineering
-/pkg/labels/*enterprise* @isovalent/policy-identity-networking
 /pkg/metrics/enterprise_* @isovalent/cni
 /pkg/metrics/features/*enterprise*.go @isovalent/metrics
 /pkg/metrics/features/**/*enterprise*.go @isovalent/metrics

pkg/endpoint/enterprise_endpoint.go
@@ -1,49 +1,0 @@
-//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
-//
-//  NOTICE: All information contained herein is, and remains the property of
-//  Isovalent Inc and its suppliers, if any. The intellectual and technical
-//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
-//  and may be covered by U.S. and Foreign Patents, patents in process, and are
-//  protected by trade secret or copyright law.  Dissemination of this information
-//  or reproduction of this material is strictly forbidden unless prior written
-//  permission is obtained from Isovalent Inc.
-
-package endpoint
-
-import (
-	"fmt"
-
-	"github.com/cilium/cilium/pkg/maps/policymap"
-)
-
-const (
-	// PropertyMeshEndpoint represents Cilium endpoints that are Cilium Mesh
-	// endpoints.
-	PropertyMeshEndpoint = "isovalent-metadata-mesh-endpoint"
-
-	// PropertyIsovalentMeshEndpointName is the key for the CEP name.
-	PropertyIsovalentMeshEndpointName = "isovalent-metadata-mesh-endpoint-name"
-
-	// PropertyIsovalentMeshEndpointNamespace is the key for the CEP namespace.
-	PropertyIsovalentMeshEndpointNamespace = "isovalent-metadata-mesh-endpoint-namespace"
-
-	// PropertyIsovalentMeshEndpointUID is the key for the CEP UID.
-	PropertyIsovalentMeshEndpointUID = "isovalent-metadata-mesh-endpoint-uid"
-
-	// PropertyIsovalentMeshEndpoint is the key for the CEP UID.
-	PropertyIsovalentMeshEndpoint = "isovalent-metadata-mesh-endpoint-object"
-)
-
-func (e *Endpoint) GetPolicyMap() (*policymap.PolicyMap, error) {
-	var err error
-
-	if e.policyMap != nil {
-		return e.policyMap, nil
-	}
-
-	if e.policyMapFactory == nil {
-		return nil, fmt.Errorf("endpoint has nil policyMapFactory")
-	}
-	e.policyMap, err = e.policyMapFactory.OpenEndpoint(e.ID)
-	return e.policyMap, err
-}

commit 6ffe2e98dd12b0e57425d567e554b17325630710

endpoint: Bring back GetPolicyMap()
[GetPolicyMap] method is used by the PrivNet feature (e.g. [1]).

[1]: https://github.com/isovalent/cilium/blob/f1ced34408c525602fcd36a22173b59d34e75272/enterprise/pkg/privnet/externaleps.go#L500

Reported-by: Tobias Klauser <tobias@isovalent.com>
Fixes: a48c7d427 ("Remove CiliumMesh")
Signed-off-by: Martynas Pumputis <martynas@isovalent.com>

CODEOWNERS
@@ -968,6 +968,7 @@ Makefile* @cilium/build
 /pkg/clustermesh/**/enterprise_* @isovalent/clustermesh
 /pkg/datapath/**/enterprise*.go @isovalent/cilium-datapath
 /pkg/datapath/ipcache/enterprise_listener.go @isovalent/clustermesh
+/pkg/endpoint/enterprise_endpoint.go @isovalent/private-networking
 /pkg/fqdn/**/*enterprise* @isovalent/dns-proxy
 /pkg/maps/srv6map/*enterprise* @isovalent/traffic-engineering
 /pkg/metrics/enterprise_* @isovalent/cni

TESTOWNERS.enterprise
@@ -513,6 +513,7 @@
 /pkg/clustermesh/**/enterprise_* @isovalent/clustermesh
 /pkg/datapath/**/enterprise*.go @isovalent/cilium-datapath
 /pkg/datapath/ipcache/enterprise_listener.go @isovalent/clustermesh
+/pkg/endpoint/enterprise_endpoint.go @isovalent/private-networking
 /pkg/fqdn/**/*enterprise* @isovalent/dns-proxy
 /pkg/maps/srv6map/*enterprise* @isovalent/traffic-engineering
 /pkg/metrics/enterprise_* @isovalent/cni

pkg/endpoint/enterprise_endpoint.go
@@ -1,0 +1,31 @@
+//  Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+//  NOTICE: All information contained herein is, and remains the property of
+//  Isovalent Inc and its suppliers, if any. The intellectual and technical
+//  concepts contained herein are proprietary to Isovalent Inc and its suppliers
+//  and may be covered by U.S. and Foreign Patents, patents in process, and are
+//  protected by trade secret or copyright law.  Dissemination of this information
+//  or reproduction of this material is strictly forbidden unless prior written
+//  permission is obtained from Isovalent Inc.
+
+package endpoint
+
+import (
+	"fmt"
+
+	"github.com/cilium/cilium/pkg/maps/policymap"
+)
+
+func (e *Endpoint) GetPolicyMap() (*policymap.PolicyMap, error) {
+	var err error
+
+	if e.policyMap != nil {
+		return e.policyMap, nil
+	}
+
+	if e.policyMapFactory == nil {
+		return nil, fmt.Errorf("endpoint has nil policyMapFactory")
+	}
+	e.policyMap, err = e.policyMapFactory.OpenEndpoint(e.ID)
+	return e.policyMap, err
+}