nobidev/cilium

commit 5ea43bf2940ea7ab9119fdbf78f0ebf5153b35e1

gha/scale-egw: Enable health server without active checks
Disable full mesh Cilium agent health checking
for scalability reasons.
Set the flag that enables the Cilium health server
without periodic active checks so that the EGW(HA)
health checking can work.

Signed-off-by: Aditi Ghag <aditi@cilium.io>

.github/actions/set-env-variables/enterprise-scalability-overrides.sh
@@ -9,3 +9,5 @@ echo "CL2_MEDIAN_MEM_USAGE_THRESHOLD=270" >> "$GITHUB_ENV"
 echo "CL2_EGW_POLICY_TEMPLATE=manifests/enterprise-egw-policy.yaml" >> "$GITHUB_ENV"
 echo "CL2_EGW_MASQ_CPU_USAGE_THRESHOLD=0.17" >> "$GITHUB_ENV"
 echo "CL2_EGW_MASQ_MEM_USAGE_THRESHOLD=320" >> "$GITHUB_ENV"
+echo "EGRESS_GATEWAY_SCALE_HELM_VALUES=--helm-set=healthChecking=false --helm-set=endpointHealthChecking.enabled=false \
+    --helm-set=enterprise.healthServerWithoutActiveChecks.enabled=true" >> "$GITHUB_ENV"

.github/workflows/scale-test-egw.yaml
@@ -164,6 +164,7 @@ jobs:
             --set=prometheus.enabled=true \
             --set=cluster.name=${{ env.cluster_name }} \
             ${{ matrix.test_type == 'egw' && env.EGRESS_GATEWAY_HELM_VALUES || '' }} \
+            ${{ matrix.test_type == 'egw' && env.EGRESS_GATEWAY_SCALE_HELM_VALUES || '' }} \
             --set=enableIPv4Masquerade=true \
             --set=bpf.masquerade=true \
             --set=kubeProxyReplacement=true \

commit d458e4a9eeb5c9dc514af078631b4f1e2f4e2869

gha/scale-egw: make configuration of extra options mindful
The blamed commit extended the egress gateway scale test to configure
extra options. However, it did so touching the upstream workflow file,
that caused complaints from the mindfulness script. Let's slightly
rework the approach, so that the script is happy again.

Fixes: 5ea43bf2940e ("gha/scale-egw: Enable health server without active checks")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

.github/actions/set-env-variables/enterprise-scalability-overrides.sh
@@ -9,5 +9,9 @@ echo "CL2_MEDIAN_MEM_USAGE_THRESHOLD=270" >> "$GITHUB_ENV"
 echo "CL2_EGW_POLICY_TEMPLATE=manifests/enterprise-egw-policy.yaml" >> "$GITHUB_ENV"
 echo "CL2_EGW_MASQ_CPU_USAGE_THRESHOLD=0.17" >> "$GITHUB_ENV"
 echo "CL2_EGW_MASQ_MEM_USAGE_THRESHOLD=320" >> "$GITHUB_ENV"
-echo "EGRESS_GATEWAY_SCALE_HELM_VALUES=--helm-set=healthChecking=false --helm-set=endpointHealthChecking.enabled=false \
-    --helm-set=enterprise.healthServerWithoutActiveChecks.enabled=true" >> "$GITHUB_ENV"
+
+if [[ "$GITHUB_WORKFLOW_REF" =~ scale-test-egw.yaml ]]; then
+    echo EGRESS_GATEWAY_HELM_VALUES="$(grep -oP '^EGRESS_GATEWAY_HELM_VALUES=\K.*' "$GITHUB_ENV") \
+        --helm-set=healthChecking=false --helm-set=endpointHealthChecking.enabled=false \
+        --helm-set=enterprise.healthServerWithoutActiveChecks.enabled=true" >> "$GITHUB_ENV"
+fi

.github/workflows/scale-test-egw.yaml
@@ -167,7 +167,6 @@ jobs:
             --set=prometheus.enabled=true \
             --set=cluster.name=${{ env.cluster_name }} \
             ${{ matrix.test_type == 'egw' && env.EGRESS_GATEWAY_HELM_VALUES || '' }} \
-            ${{ matrix.test_type == 'egw' && env.EGRESS_GATEWAY_SCALE_HELM_VALUES || '' }} \
             --set=enableIPv4Masquerade=true \
             --set=bpf.masquerade=true \
             --set=kubeProxyReplacement=true \