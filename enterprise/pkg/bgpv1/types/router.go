c16dc0c0ed1c06e33771141c230a16024adc59e0
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2025-11-13 22:06:56 -0800

    bgp: Introduce EnterpriseRouter interface
    As our enterprise implementation grows, it is getting harder to
    introduce a new types or struct fields into the OSS types package which
    is useless from OSS's standpoint. Also, it takes time to propagate OSS
    changes into CEE which significantly affects feature velocity.
    
    This commit introduces EnterpriseRouter interface. This is a superset of
    the OSS Router interface and supports "extended" functions. The idea is
    we can implement these functions to the OSS GoBGPServer type within
    enterprise_xxx.go file and on the enterprise side, we "upgrade" OSS
    Router to the EnterpriseRouter type using paramUpgrader. In this way, we
    can introduce enterprise-specific logic without modifying OSS.
    
    As a first target, we introduce GetRoutesExtended which returns
    Neighbor's Address that advertised the route on top of the existing
    GetRoutes response which is required for the BGP Unnumbered support for
    BGP Route Import.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

---
+++ enterprise/pkg/bgpv1/types/router.go
@@ -0,0 +1,56 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package types
+
+import (
+	"context"
+	"net/netip"
+
+	ossTypes "github.com/cilium/cilium/pkg/bgp/types"
+)
+
+// EnterpriseRouter is an extension of the ossTypes.Router interface that adds
+// enterprise-specific functionality. This is a superset of the OSS Router
+// interface. We can add support for these enterprise-specific methods in the
+// OSS Router implementations through enterprise_*.go file and on the
+// enterprise side, we can upgrade the OSS Router to an EnterpriseRouter when
+// needed (most likely through upgrader).
+type EnterpriseRouter interface {
+	ossTypes.Router
+
+	// GetRoutesExtended retrieves routes from the RIB of underlying router
+	// implementation. The reply contains extended enterprise-specific
+	// route information.
+	GetRoutesExtended(ctx context.Context, r *GetRoutesExtendedRequest) (*GetRoutesExtendedResponse, error)
+}
+
+type GetRoutesExtendedRequest struct {
+	ossTypes.GetRoutesRequest
+}
+
+type GetRoutesExtendedResponse struct {
+	Routes []*ExtendedRoute
+}
+
+type ExtendedRoute struct {
+	Prefix string
+	Paths  []*ExtendedPath
+}
+
+type ExtendedPath struct {
+	ossTypes.Path
+
+	// NeighborAddr is the address of the neighbor that advertised this
+	// path. When the neighbor is a BGP Unnumbered Peer, the netip.Addr
+	// will contain the zone information to identify the neighbor
+	// interface.
+	NeighborAddr netip.Addr
+}
