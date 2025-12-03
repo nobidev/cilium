60bd611286b2fd60555bfd8fec2a9c3ed4a8ec2c
Tobias Klauser <tobias@cilium.io>
2025-12-03 11:13:44 +0100

    plugins/cilium-cni/cmd: add single CNI main entry point
    [ upstream commit 32fb61a1e74b6a80089ee12ec572bda3275f2884 ]
    
    [ tk: follow the change in the enterprise CNI plugin and convert it to
      use the newly added (*Cmd).PluginMain with enterprise-specific
      overrides. ]
    
    Rather than having the caller construct the *Cmd and invoke
    skel.PluginMainFuncs, provide a convenience function which can be called
    from the CNI plugin's main func. It optionally takes options which can
    be used by downstream implementations of the Cilium CNI to configure
    certain aspects of the CNI plugin.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- enterprise/plugins/cilium-cni/main.go
+++ enterprise/plugins/cilium-cni/main.go
@@ -13,13 +13,8 @@ package main
 import (
 	"runtime"
 
-	"github.com/containernetworking/cni/pkg/skel"
-	cniVersion "github.com/containernetworking/cni/pkg/version"
-
 	_ "github.com/cilium/cilium/enterprise/fips"
 	"github.com/cilium/cilium/enterprise/plugins/cilium-cni/pkg/multinetwork"
-	"github.com/cilium/cilium/pkg/logging"
-	"github.com/cilium/cilium/pkg/logging/logfields"
 	"github.com/cilium/cilium/pkg/version"
 	"github.com/cilium/cilium/plugins/cilium-cni/cmd"
 )
@@ -29,10 +24,8 @@ func init() {
 }
 
 func main() {
-	// slogloggercheck: the logger has been initialized with default settings
-	logger := logging.DefaultSlogLogger.With(logfields.LogSubsys, "cilium-cni")
-	c := cmd.NewCmd(logger, cmd.WithEPConfigurator(multinetwork.NewEndpointConfigurator()))
-	skel.PluginMainFuncs(c.CNIFuncs(),
-		cniVersion.PluginSupports("0.1.0", "0.2.0", "0.3.0", "0.3.1", "0.4.0", "1.0.0", "1.1.0"),
-		"Cilium CNI plugin (enterprise) "+version.Version)
+	cmd.PluginMain(
+		cmd.WithVersion("Cilium CNI plugin (enterprise) "+version.Version),
+		cmd.WithEPConfigurator(multinetwork.NewEndpointConfigurator()),
+	)
 }
