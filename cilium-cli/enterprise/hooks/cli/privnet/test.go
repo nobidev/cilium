nobidev/cilium

commit 256ea75390dcd6a11282adac130410bf1b4adeda

privnet/cli: explicitly trigger cleanup at the end of the test
In preparation for introducing the INB failover tests, let's explicitly
remove all existing policies at the end, as they would interfere.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cilium-cli/enterprise/hooks/cli/privnet/test.go
@@ -545,6 +545,10 @@ func (t *TestRun) Cleanup(ctx context.Context) {
 			}
 		}
 	}
+
+	// Reset the map, so that a subsequent call to Cleanup doesn't attempt
+	// to remove the same policies again.
+	t.policies = make(map[string]k8s.Object)
 }
 
 var networks = []NetworkName{NetworkA, NetworkB, NetworkC, NetworkD}

cilium-cli/enterprise/hooks/cli/privnet_tests.go
@@ -153,6 +153,9 @@ func newCmdPrivNetTest() *cobra.Command {
 				}
 			}
 
+			// Remove all policies before proceeding with the INB failover tests.
+			t.Cleanup(ctx)
+
 			if t.Failed() {
 				return errors.New("one or more tests failed")
 			}

commit 7fe51b547b9a0fb6d572fcfa22e38453c0bd8504

privnet/cli: fix policy cleanup in case of test failure
The blamed commit introduced an explicit step to delete all existing
policies before performing the INB failover tests. However, this turned
out being problematic in case any of the tests executed after applying
the last version of the policies fails (or is canceled), given that the
explicit cleanup step would fail as well due to the context having been
canceled. Yet, the deferred execution of the cleanup, which makes use
of a separate context, would be also skipped, as the map of policies is
incorrectly reset upon the first execution.

Let's get this fixed, skipping the execution of the explicit cleanup in
case the context got canceled, so that the deferred one can take care
of it. Additionally, let's avoid resetting the policies map if any of
the deletions fails, so that the last resort cleanup can still attempt
to fix it.

Fixes: 256ea75390dc ("privnet/cli: explicitly trigger cleanup at the end of the test")
Reported-by: Sebastian Wicki <sebastian@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

cilium-cli/enterprise/hooks/cli/privnet/test.go
@@ -576,6 +576,8 @@ func (t *TestRun) Failed() bool {
 }
 
 func (t *TestRun) Cleanup(ctx context.Context) {
+	var success = true
+
 	for _, client := range t.inbClients {
 		for _, obj := range t.policies {
 			kind := obj.GetObjectKind().GroupVersionKind().Kind
@@ -588,13 +590,17 @@ func (t *TestRun) Cleanup(ctx context.Context) {
 			err := client.DeleteGeneric(ctx, obj)
 			if err != nil && !k8serrors.IsNotFound(err) {
 				t.log.Warn(fmt.Sprintf("⚠️ Failed removing %s %s: %v", kind, name, err))
+				success = false
 			}
 		}
 	}
 
 	// Reset the map, so that a subsequent call to Cleanup doesn't attempt
-	// to remove the same policies again.
-	t.policies = make(map[string]k8s.Object)
+	// to remove the same policies again. Unless something went wrong in the
+	// cleanup, in which case we keep the map content intact.
+	if success {
+		t.policies = make(map[string]k8s.Object)
+	}
 }
 
 var networks = []NetworkName{NetworkA, NetworkB, NetworkC, NetworkD}

cilium-cli/enterprise/hooks/cli/privnet_tests.go
@@ -153,8 +153,12 @@ func newCmdPrivNetTest() *cobra.Command {
 				}
 			}
 
-			// Remove all policies before proceeding with the INB failover tests.
-			t.Cleanup(ctx)
+			// Remove all policies before proceeding with the INB failover tests,
+			// unless the context got canceled, in which case we let the deferred
+			// function take care of it.
+			if ctx.Err() == nil {
+				t.Cleanup(ctx)
+			}
 
 			// Trigger a bunch of failovers.
 			for inb := range t.INBNodeNames() {