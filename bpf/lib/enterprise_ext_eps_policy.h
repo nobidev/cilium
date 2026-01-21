nobidev/cilium

commit b9a93bfa7c2da1e3f790dfb5d06c048a65a33df3

ent/bpf: add external_eps policy helpers for IPv6
In preparation for reusing the external_eps helpers in the context of
private networking, let's also introduce the IPv6 variant, mimicking
the already existing ones.

Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

bpf/lib/enterprise_ext_eps_policy.h
@@ -95,3 +95,32 @@ ext_eps_policy_can_ingress4(struct __ctx_buff *ctx, __be32 ip, __u32 dst_id,
 			l4_off, match_type, INGRESS_POLICY, is_untracked_fragment, audited,
 			ext_err, proxy_port);
 }
+
+static __always_inline int
+ext_eps_policy_can_egress6(struct __ctx_buff *ctx, union v6addr ip6, __u32 dst_id,
+			   __be16 dport, __u8 proto, int l4_off, __u8 *match_type,
+			   __u8 *audited, __s8 *ext_err, __u16 *proxy_port)
+{
+	struct endpoint_key key = {
+		.ip6 = ip6,
+		.family = ENDPOINT_KEY_IPV6,
+	};
+
+	return __ext_eps_policy_can_access(ctx, &key, dst_id, ETH_P_IP, dport, proto,
+			l4_off, match_type, EGRESS_POLICY, false, audited, ext_err, proxy_port);
+}
+
+static __always_inline int
+ext_eps_policy_can_ingress6(struct __ctx_buff *ctx, union v6addr ip6, __u32 dst_id,
+			    __be16 dport, __u8 proto, int l4_off, bool is_untracked_fragment,
+			    __u8 *match_type, __u8 *audited, __s8 *ext_err, __u16 *proxy_port)
+{
+	struct endpoint_key key = {
+		.ip6 = ip6,
+		.family = ENDPOINT_KEY_IPV6,
+	};
+
+	return __ext_eps_policy_can_access(ctx, &key, dst_id, ETH_P_IP, dport, proto,
+			l4_off, match_type, INGRESS_POLICY, is_untracked_fragment, audited,
+			ext_err, proxy_port);
+}

commit b726b39034f6bf5c2d38a394441cd136220aa2e4

ent/bpf: fix wrong ethertype in IPv6 external endpoints policy helpers
Cure53 reported that the IPv6 variants of the external endpoints policy
helpers propagate the incorrect ethertype. These helpers are currently
used by private networks to implement network policies for endpoints
whose reachablility is provided via the INB, such as in vSphere/NSX.
Differently, the now removed Cilium Mesh did not support IPv6 at all.

To the best of my knowledge, this mismatch does not have any security
implication, because the ethertype field is not used to determine the
set of policy rules to apply (which is based on the policy map and
identities, as usual, nor to determine the target policy map).
However, it could cause certain ICMPv6 packets intended to be allowed
to be incorrectly dropped, as the logic in [1] would not take effect.

Let's get if fixed, propagating the correct ethertype.

[1]: https://github.com/cilium/cilium/blob/56c67c5d754348d18b80dbbfdaecd7aca7cf84ef/bpf/lib/policy.h#L237-L274

Fixes: b9a93bfa7c2d ("ent/bpf: add external_eps policy helpers for IPv6")
Reported-by: Vikram <vikram@cure53.de>
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

bpf/lib/enterprise_ext_eps_policy.h
@@ -49,7 +49,7 @@ ext_eps_policy_can_egress6(struct __ctx_buff *ctx, union v6addr ip6, __u32 dst_i
 		.family = ENDPOINT_KEY_IPV6,
 	};
 
-	return ext_ep_policy_verdict(ctx, &key, dst_id, ETH_P_IP, dport, proto,
+	return ext_ep_policy_verdict(ctx, &key, dst_id, ETH_P_IPV6, dport, proto,
 				     l4_off, match_type, EGRESS_POLICY, false,
 				     audited, ext_err, proxy_port, cookie);
 }
@@ -65,7 +65,7 @@ ext_eps_policy_can_ingress6(struct __ctx_buff *ctx, union v6addr ip6, __u32 dst_
 		.family = ENDPOINT_KEY_IPV6,
 	};
 
-	return ext_ep_policy_verdict(ctx, &key, dst_id, ETH_P_IP, dport, proto,
+	return ext_ep_policy_verdict(ctx, &key, dst_id, ETH_P_IPV6, dport, proto,
 				     l4_off, match_type, INGRESS_POLICY, is_untracked_fragment,
 				     audited, ext_err, proxy_port, cookie);
 }