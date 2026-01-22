38a6e9285216e86c5cde765db74196138affddab
Tobias Klauser <tobias@isovalent.com>
2026-01-22 13:40:52 +0100

    identity: add enterprise-only numeric identity for privnet unknown flows
    This the identity used for unknown flows between the cluster and the
    INB.
    
    Co-authored-by: David Bimmler <david.bimmler@isovalent.com>
    Co-authored-by: Fabian Fischer <fabian.fischer@isovalent.com>
    Co-authored-by: Harsimran Pabla <hpabla@isovalent.com>
    Co-authored-by: Martynas Pumputis <martynas@isovalent.com>
    Co-authored-by: Sebastian Wicki <sebastian@isovalent.com>
    Co-authored-by: Marco Iorio <marco.iorio@isovalent.com>
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

---
+++ pkg/identity/enterprise_numericidentity.go
@@ -0,0 +1,17 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package identity
+
+const (
+	// ReservedPrivnetUnknownFlow is the identity used for unknown flows
+	// between the cluster and the INB.
+	ReservedPrivnetUnknownFlow NumericIdentity = 99
+)
