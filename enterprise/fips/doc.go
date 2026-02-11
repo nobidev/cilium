ffeeda49b2b9ab5bc6d1881b6662b9cb853c51f8
Sekhar Sankaramanchi <sekhar@isovalent.com>
2026-02-11 02:08:39 +0000

    chore: fips-140 compliance for cilium
    Signed-off-by: Sekhar Sankaramanchi <sekhar@isovalent.com>

--- enterprise/fips/doc.go
+++ enterprise/fips/doc.go
@@ -7,11 +7,29 @@
 //  protected by trade secret or copyright law.  Dissemination of this information
 //  or reproduction of this material is strictly forbidden unless prior written
 //  permission is obtained from Isovalent Inc.
+
+// Package fips enables FIPS 140-3 compliant cryptography using Go's native
+// crypto/fips140 module (Go 1.24+).
 //
-// Package fips can be used to enable the usage of FIPs compliant crypto
-// algorithms for a binary, with the following steps:
-// 1. Import this package somewhere in the target binary's project.
-// 2. Set the environment variable GOEXPERIMENT=boringcrypto
-// 3. Build your binary with the build tag 'fips'.
+// To build with FIPS support:
+//
+//	GOFIPS140=v1.0.0 go build -tags fips ./...
+//
+// Or using make:
+//
+//	FIPS=1 make
+//
+// To run in FIPS mode:
+//
+//	GODEBUG=fips140=on ./cilium-agent
+//
+// For strict mode (non-FIPS algorithms return errors):
+//
+//	GODEBUG=fips140=only ./cilium-agent
+//
+// The crypto/fips140.Enabled() function can be used to check if FIPS mode
+// is active at runtime.
+//
+// See https://go.dev/doc/security/fips140 for more details.
 
 package fips
--- enterprise/fips/fips.go
+++ enterprise/fips/fips.go
@@ -10,26 +10,40 @@
 //  or reproduction of this material is strictly forbidden unless prior written
 //  permission is obtained from Isovalent Inc.
 
+// Package fips enables FIPS 140-3 compliant cryptography using Go's native
+// crypto/fips140 module (Go 1.24+). This replaces the previous BoringCrypto
+// implementation.
+//
+// To enable FIPS mode:
+//   - Build with: GOFIPS140=v1.0.0 go build -tags fips
+//   - Run with: GODEBUG=fips140=on ./binary
+//
+// See https://go.dev/doc/security/fips140 for details.
 package fips
 
 import (
+	"crypto/fips140"
 	"crypto/tls"
-	_ "crypto/tls/fipsonly"
+	"log"
 
 	"github.com/cilium/cilium/pkg/hubble/relay/server"
 	"github.com/cilium/cilium/pkg/hubble/server/serveroption"
 )
 
 func init() {
-	// As of 2024/11/22, boringcrypto only supports `tls.Config.MinVersion == TLS1.2`. An issue in the
-	// Go repository details the work toward allowing more recent FIPS-approved protocols/algorithms
-	// (including TLS 1.3) but was closed because of missing algorithm in the latest version of
-	// BoringCrypto.
-	// https://github.com/golang/go/issues/62372
+	// Native Go FIPS 140-3 mode (Go 1.24+) supports TLS 1.3, unlike BoringCrypto
+	// which was limited to TLS 1.2. However, we still set TLS 1.2 as minimum
+	// for broad compatibility.
 	//
-	// An issue was opened in the Go repository on 2024/09/19 to add a FIPS 140-3 compliant package in
-	// the standard library targeted for Go 1.24.
-	// https://github.com/golang/go/issues/69536
+	// FIPS mode is controlled at runtime via GODEBUG=fips140=on (or =only).
+	// When enabled, crypto/tls automatically restricts to FIPS-approved
+	// cipher suites and algorithms.
+	//
+	// See: https://go.dev/doc/security/fips140
 	server.MinTLSVersion = tls.VersionTLS12
 	serveroption.MinTLSVersion = tls.VersionTLS12
+
+	if fips140.Enabled() {
+		log.Println("FIPS 140-3 mode is enabled")
+	}
 }
