53a16a22cc5355624ad18a0473f120ab9d619de6
Jussi Maki <jussi.maki@isovalent.com>
2026-02-11 12:47:38 +0000

    privnet: dhcp: add unicast relay
    Unicast relay relays DHCP requests to the DHCP server via unicast.
    This uses a normal UDP socket instead of a raw socket and thus requires
    the NET_BIND_SERVICE capabilities to bind to port 67.
    
    Signed-off-by: Jussi Maki <jussi.maki@isovalent.com>

---
+++ enterprise/pkg/privnet/dhcp/unicast_relay.go
@@ -0,0 +1,182 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package dhcp
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+
+	"github.com/insomniacslk/dhcp/dhcpv4"
+
+	"github.com/cilium/cilium/enterprise/pkg/privnet/tables"
+	"github.com/cilium/cilium/pkg/logging/logfields"
+	"github.com/cilium/cilium/pkg/netns"
+	"github.com/cilium/cilium/pkg/time"
+)
+
+// unicastRelay forwards DHCP requests to a configured DHCP server via unicast
+// using UDP source port 67.
+type unicastRelay struct {
+	serverAddr *net.UDPAddr
+	log        *slog.Logger
+	netns      *netns.NetNS
+}
+
+// unicastRelayFactory returns a unicastRelay for each workload.
+type unicastRelayFactory struct {
+	serverAddr *net.UDPAddr
+	log        *slog.Logger
+	netns      *netns.NetNS
+}
+
+// RelayFor implements RelayFactory.
+func (f *unicastRelayFactory) RelayFor(*tables.LocalWorkload) (Relayer, error) {
+	return &unicastRelay{
+		serverAddr: f.serverAddr,
+		log:        f.log,
+		netns:      f.netns,
+	}, nil
+}
+
+// Relay forwards the DHCP request and returns responses received within waitTime.
+func (r *unicastRelay) Relay(ctx context.Context, waitTime time.Duration, req *dhcpv4.DHCPv4) ([]*dhcpv4.DHCPv4, error) {
+	if req == nil {
+		return nil, errors.New("dhcp request is nil")
+	}
+	if waitTime <= 0 {
+		return nil, fmt.Errorf("wait time is required")
+	}
+
+	conn, err := r.dialConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	giaddr := net.IP(nil)
+	if udpAddr, ok := conn.LocalAddr().(*net.UDPAddr); ok {
+		giaddr = udpAddr.IP
+	}
+
+	sendReq, err := r.prepare(req, giaddr)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := conn.Write(sendReq.ToBytes()); err != nil {
+		if r.log != nil {
+			r.log.Error("Failed to send unicast DHCP request", logfields.Error, err)
+		}
+		return nil, fmt.Errorf("send unicast request: %w", err)
+	}
+
+	waitCtx, cancel := context.WithTimeout(ctx, waitTime)
+	defer cancel()
+
+	buf := make([]byte, 1500)
+	for {
+		select {
+		case <-waitCtx.Done():
+			if r.log != nil {
+				r.log.Error("DHCP relay context done", logfields.Error, waitCtx.Err())
+			}
+			if errors.Is(waitCtx.Err(), context.DeadlineExceeded) {
+				if r.log != nil {
+					r.log.Error("Timed out waiting for DHCP response")
+				}
+				return nil, fmt.Errorf("timed out waiting for DHCP response")
+			}
+			return nil, waitCtx.Err()
+		default:
+			if err := conn.SetReadDeadline(time.Now().Add(100 * time.Millisecond)); err != nil {
+				return nil, err
+			}
+			n, err := conn.Read(buf)
+			if err != nil {
+				var ne net.Error
+				if errors.As(err, &ne) && ne.Timeout() {
+					if _, err := conn.Write(sendReq.ToBytes()); err != nil {
+						if r.log != nil {
+							r.log.Error("Failed to resend unicast DHCP request", logfields.Error, err)
+						}
+						return nil, fmt.Errorf("resend unicast request: %w", err)
+					}
+					continue
+				}
+				return nil, err
+			}
+			resp, err := dhcpv4.FromBytes(buf[:n])
+			if err != nil {
+				r.log.Info("Parse failure", logfields.Error, err)
+				continue
+			}
+
+			if resp.TransactionID != req.TransactionID {
+				continue
+			}
+			return []*dhcpv4.DHCPv4{resp}, nil
+		}
+	}
+}
+
+func (r *unicastRelay) prepare(req *dhcpv4.DHCPv4, giaddr net.IP) (*dhcpv4.DHCPv4, error) {
+	copyReq, err := dhcpv4.FromBytes(req.ToBytes())
+	if err != nil {
+		return nil, err
+	}
+	if copyReq.HopCount < 255 {
+		copyReq.HopCount++
+	}
+	if giaddr != nil && !giaddr.IsUnspecified() {
+		copyReq.GatewayIPAddr = giaddr
+	}
+	return copyReq, nil
+}
+
+func (r *unicastRelay) dialConn(ctx context.Context) (*net.UDPConn, error) {
+	if r.serverAddr == nil || r.serverAddr.IP == nil {
+		return nil, errors.New("server address is required for unicast relay")
+	}
+	dialer := net.Dialer{
+		LocalAddr: &net.UDPAddr{Port: dhcpv4.ServerPort},
+	}
+
+	ns := r.netns
+	if ns == nil {
+		var err error
+		ns, err = netns.Current()
+		if err != nil {
+			return nil, err
+		}
+		defer ns.Close()
+	}
+
+	var udpConn *net.UDPConn
+	if err := ns.Do(func() error {
+		conn, err := dialer.DialContext(ctx, "udp4", r.serverAddr.String())
+		if err != nil {
+			return fmt.Errorf("dial unicast relay socket to %s: %w", r.serverAddr, err)
+		}
+		c, ok := conn.(*net.UDPConn)
+		if !ok {
+			_ = conn.Close()
+			return fmt.Errorf("unexpected conn type %T", conn)
+		}
+		udpConn = c
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return udpConn, nil
+}
---
+++ enterprise/pkg/privnet/dhcp/unicast_relay_test.go
@@ -0,0 +1,296 @@
+// Copyright (C) Isovalent, Inc. - All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Isovalent Inc and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Isovalent Inc and its suppliers
+// and may be covered by U.S. and Foreign Patents, patents in process, and are
+// protected by trade secret or copyright law.  Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Isovalent Inc.
+
+package dhcp
+
+import (
+	"errors"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/cilium/hive/hivetest"
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/stretchr/testify/require"
+	"github.com/vishvananda/netlink"
+
+	"github.com/cilium/cilium/pkg/datapath/linux/safenetlink"
+	"github.com/cilium/cilium/pkg/netns"
+	"github.com/cilium/cilium/pkg/testutils"
+)
+
+func TestPrivilegedUnicastRelaySendUnicastSetsGIAddrAndHop(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	relayNS, err := netns.New()
+	require.NoError(t, err)
+	defer relayNS.Close()
+
+	serverNS, err := netns.New()
+	require.NoError(t, err)
+	defer serverNS.Close()
+
+	relayIP := net.IPv4(192, 0, 2, 10)
+	serverIP := net.IPv4(192, 0, 2, 20)
+
+	setupUnicastRelayNamespaces(t, relayNS, serverNS, relayIP, serverIP)
+	recvCh, stopServer := runSimpleDHCPServer(serverNS, serverIP, net.IPv4(192, 0, 2, 100))
+	defer stopServer()
+
+	relayFactory := &unicastRelayFactory{
+		serverAddr: &net.UDPAddr{IP: serverIP, Port: dhcpv4.ServerPort},
+		netns:      relayNS,
+	}
+	relay, err := relayFactory.RelayFor(nil)
+	require.NoError(t, err)
+	require.NotNil(t, relay)
+	req, err := dhcpv4.NewDiscovery(net.HardwareAddr{0x00, 0x11, 0x22, 0x33, 0x44, 0x55})
+	require.NoError(t, err)
+	require.Equal(t, uint8(0), req.HopCount)
+
+	require.Eventually(
+		t,
+		func() bool {
+			_, err = relay.Relay(t.Context(), 50*time.Millisecond, req)
+			if err != nil {
+				t.Logf("Relay(): %s", err)
+				return false
+			}
+			select {
+			case info := <-recvCh:
+				require.NotNil(t, info.msg)
+				require.NotNil(t, info.addr)
+				require.Equal(t, info.addr.IP.To4(), info.msg.GatewayIPAddr.To4())
+				require.Equal(t, uint8(1), info.msg.HopCount)
+				require.Equal(t, req.TransactionID, info.msg.TransactionID)
+			case <-time.After(100 * time.Millisecond):
+				return false
+			}
+			return true
+		},
+		2*time.Second,
+		50*time.Millisecond,
+	)
+}
+
+func TestPrivilegedUnicastRelaySendUnicastMaxHop(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	relayNS, err := netns.New()
+	require.NoError(t, err)
+	defer relayNS.Close()
+
+	serverNS, err := netns.New()
+	require.NoError(t, err)
+	defer serverNS.Close()
+
+	relayIP := net.IPv4(192, 0, 2, 30)
+	serverIP := net.IPv4(192, 0, 2, 40)
+
+	setupUnicastRelayNamespaces(t, relayNS, serverNS, relayIP, serverIP)
+	recvCh, stopServer := runSimpleDHCPServer(serverNS, serverIP, net.IPv4(192, 0, 2, 101))
+	defer stopServer()
+
+	relayFactory := &unicastRelayFactory{
+		serverAddr: &net.UDPAddr{IP: serverIP, Port: dhcpv4.ServerPort},
+		netns:      relayNS,
+	}
+	relay, err := relayFactory.RelayFor(nil)
+	require.NoError(t, err)
+	require.NotNil(t, relay)
+
+	req, err := dhcpv4.NewDiscovery(net.HardwareAddr{0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff})
+	require.NoError(t, err)
+	req.HopCount = 255
+
+	require.Eventually(
+		t,
+		func() bool {
+			_, err = relay.Relay(t.Context(), 50*time.Millisecond, req)
+			if err != nil {
+				return false
+			}
+			select {
+			case info := <-recvCh:
+				require.NotNil(t, info.msg)
+				require.Equal(t, uint8(255), info.msg.HopCount)
+				require.Equal(t, req.TransactionID, info.msg.TransactionID)
+			case <-time.After(50 * time.Millisecond):
+				return false
+			}
+			return true
+
+		},
+		2*time.Second,
+		50*time.Millisecond,
+	)
+}
+
+func TestPrivilegedUnicastRelayRelayReturnsResponse(t *testing.T) {
+	testutils.PrivilegedTest(t)
+
+	relayNS, err := netns.New()
+	require.NoError(t, err)
+	defer relayNS.Close()
+
+	serverNS, err := netns.New()
+	require.NoError(t, err)
+	defer serverNS.Close()
+
+	relayIP := net.IPv4(192, 0, 2, 50)
+	serverIP := net.IPv4(192, 0, 2, 60)
+
+	setupUnicastRelayNamespaces(t, relayNS, serverNS, relayIP, serverIP)
+	_, stopServer := runSimpleDHCPServer(serverNS, serverIP, net.IPv4(192, 0, 2, 100))
+	defer stopServer()
+
+	relayFactory := &unicastRelayFactory{
+		serverAddr: &net.UDPAddr{IP: serverIP, Port: dhcpv4.ServerPort},
+		log:        hivetest.Logger(t),
+		netns:      relayNS,
+	}
+	relay, err := relayFactory.RelayFor(nil)
+	require.NoError(t, err)
+	require.NotNil(t, relay)
+	req, err := dhcpv4.NewDiscovery(net.HardwareAddr{0xde, 0xad, 0xbe, 0xef, 0x00, 0x01})
+	require.NoError(t, err)
+
+	require.Eventually(
+		t,
+		func() bool {
+			resps, err := relay.Relay(t.Context(), 100*time.Millisecond, req)
+			if err != nil || len(resps) != 1 {
+				return false
+			}
+			resp := resps[0]
+			require.NotNil(t, resp)
+			require.Equal(t, dhcpv4.MessageTypeAck, resp.MessageType())
+			return true
+		},
+		time.Second,
+		50*time.Millisecond)
+}
+
+type recvInfo struct {
+	msg  *dhcpv4.DHCPv4
+	addr *net.UDPAddr
+}
+
+func setupUnicastRelayNamespaces(t *testing.T, relayNS, serverNS *netns.NetNS, relayIP, serverIP net.IP) {
+	t.Helper()
+
+	veth0, veth1 := setupVethPair(t, relayNS)
+	veth0MAC := append(net.HardwareAddr(nil), veth0.Attrs().HardwareAddr...)
+	veth1MAC := append(net.HardwareAddr(nil), veth1.Attrs().HardwareAddr...)
+
+	require.NoError(t, relayNS.Do(func() error {
+		link, err := safenetlink.LinkByName(veth1.Attrs().Name)
+		if err != nil {
+			return err
+		}
+		return netlink.LinkSetNsFd(link, serverNS.FD())
+	}))
+
+	require.NoError(t, relayNS.Do(func() error {
+		link, err := safenetlink.LinkByName(veth0.Attrs().Name)
+		if err != nil {
+			return err
+		}
+		if err := netlink.AddrAdd(link, &netlink.Addr{IPNet: &net.IPNet{IP: relayIP, Mask: net.CIDRMask(24, 32)}}); err != nil {
+			return err
+		}
+		return netlink.NeighSet(&netlink.Neigh{
+			LinkIndex:    link.Attrs().Index,
+			IP:           serverIP,
+			HardwareAddr: veth1MAC,
+			State:        netlink.NUD_PERMANENT,
+		})
+	}))
+
+	require.NoError(t, serverNS.Do(func() error {
+		link, err := safenetlink.LinkByName(veth1.Attrs().Name)
+		if err != nil {
+			return err
+		}
+		if err := netlink.LinkSetUp(link); err != nil {
+			return err
+		}
+		if err := netlink.AddrAdd(link, &netlink.Addr{IPNet: &net.IPNet{IP: serverIP, Mask: net.CIDRMask(24, 32)}}); err != nil {
+			return err
+		}
+		return netlink.NeighSet(&netlink.Neigh{
+			LinkIndex:    link.Attrs().Index,
+			IP:           relayIP,
+			HardwareAddr: veth0MAC,
+			State:        netlink.NUD_PERMANENT,
+		})
+	}))
+}
+
+func runSimpleDHCPServer(serverNS *netns.NetNS, serverIP, offeredIP net.IP) (<-chan recvInfo, func()) {
+	recvCh := make(chan recvInfo, 1)
+	stopCh := make(chan struct{})
+	doneCh := make(chan struct{})
+
+	go func() {
+		defer close(doneCh)
+		serverNS.Do(func() error {
+			conn, err := net.ListenUDP("udp4", &net.UDPAddr{IP: serverIP, Port: dhcpv4.ServerPort})
+			if err != nil {
+				return err
+			}
+			defer conn.Close()
+
+			buf := make([]byte, 2048)
+			for {
+				select {
+				case <-stopCh:
+					return nil
+				default:
+				}
+				_ = conn.SetReadDeadline(time.Now().Add(50 * time.Millisecond))
+				n, addr, err := conn.ReadFromUDP(buf)
+				if err != nil {
+					var ne net.Error
+					if errors.As(err, &ne) && ne.Timeout() {
+						continue
+					}
+					return err
+				}
+				msg, err := dhcpv4.FromBytes(buf[:n])
+				if err != nil {
+					continue
+				}
+				select {
+				case recvCh <- recvInfo{msg: msg, addr: addr}:
+				default:
+				}
+
+				resp, err := dhcpv4.NewReplyFromRequest(msg)
+				if err != nil {
+					continue
+				}
+				resp.YourIPAddr = offeredIP
+				resp.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeAck))
+				_, err = conn.WriteToUDP(resp.ToBytes(), addr)
+				if err != nil {
+					return err
+				}
+			}
+		})
+	}()
+
+	stop := func() {
+		close(stopCh)
+		<-doneCh
+	}
+
+	return recvCh, stop
+}
