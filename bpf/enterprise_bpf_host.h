36e9076207ce84c2727399828ace4c547f4bb52c
Marco Iorio <marco.iorio@isovalent.com>
2026-02-12 15:02:42 +0100

    ent/bpf: remove empty host_egress_policy_hook implementation
    The enterprise implementation of the hook is always returning
    CTX_ACT_OK since a48c7d4276b7 ("Remove CiliumMesh"), and can
    thus be removed.
    
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- bpf/enterprise_bpf_host.h
+++ bpf/enterprise_bpf_host.h
@@ -11,15 +11,6 @@
 
 #include "lib/enterprise_privnet.h"
 
-#undef host_egress_policy_hook
-static __always_inline int
-host_egress_policy_hook(struct __ctx_buff *ctx __maybe_unused,
-			__u32 src_sec_identity __maybe_unused,
-			__s8 *ext_err __maybe_unused)
-{
-       return CTX_ACT_OK;
-}
-
 static __always_inline int
 enterprise_privnet_do_netdev(struct __ctx_buff *ctx, __u16 proto, __u32 __maybe_unused identity,
 			     enum trace_point obs_point,  const bool __maybe_unused from_host)
