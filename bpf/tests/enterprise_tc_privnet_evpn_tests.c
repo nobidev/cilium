06e06f9dd6021ab0a956298b3f1a26297a3f2e0c
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-02-13 01:23:29 -0800

    privnet,bpf: Add unit tests for __privnet_evpn_ingress
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

---
+++ bpf/tests/enterprise_tc_privnet_evpn_tests.c
@@ -0,0 +1,223 @@
+// SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+/* Copyright Authors of Cilium */
+
+#include <bpf/ctx/skb.h>
+#include <bpf/config/global.h> /* Needed for interface_mac config */
+#include "bpf/helpers.h"
+#include "common.h"
+#include "pktgen.h"
+#include "scapy.h"
+
+/* Datapath dummy config for tests */
+#define ENABLE_IPV4
+#define ENABLE_IPV6
+#define privnet_tunnel_id 99
+
+/* Enable debug output */
+#define DEBUG
+
+#include <bpf/config/node.h>
+
+#include <lib/enterprise_privnet.h>
+#include "tests/lib/endpoint.h"
+#include "enterprise_privnet_common.h"
+#include "tests/lib/enterprise_privnet.h"
+
+/* Enable configurations */
+ASSIGN_CONFIG(bool, privnet_enable, true)
+ASSIGN_CONFIG(union macaddr, interface_mac, {.addr = mac_one_addr }) /* set device mac */
+
+PKTGEN("tc", "01_privnet_evpn_ingress_v4")
+int privnet_evpn_ingress_v4_pktgen(struct __ctx_buff *ctx)
+{
+	build_privnet_packet(ctx, NETIP_ICMP_REQ);
+	return 0;
+}
+
+CHECK("tc", "01_privnet_evpn_ingress_v4")
+int privnet_evpn_ingress_v4_check(struct __ctx_buff *ctx)
+{
+	test_init();
+
+	TEST("baseline", {
+		__u32 status_code;
+
+		privnet_v4_add_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN, SUBNET_ID);
+		privnet_v4_add_endpoint_entry(NET_ID, SUBNET_ID, V4_NET_IP_2, V4_POD_IP_1);
+		endpoint_v4_add_entry(V4_POD_IP_1, 1, 0, 0, 0, 0, (const __u8 *)mac_four,
+				      (const __u8 *)mac_five);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v4_del_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN);
+		privnet_v4_del_endpoint_entry(NET_ID, SUBNET_ID, V4_NET_IP_2, V4_POD_IP_1);
+		endpoint_v4_del_entry(V4_POD_IP_1);
+
+		if (status_code != TC_ACT_REDIRECT)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   TC_ACT_REDIRECT, status_code);
+	});
+
+	TEST("drop_fib_miss", {
+		__u32 status_code;
+
+		privnet_v4_add_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN, SUBNET_ID);
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+		privnet_v4_del_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	TEST("drop_route_entry", {
+		__u32 status_code;
+
+		privnet_v4_add_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN, SUBNET_ID);
+		privnet_v4_add_subnet_route(NET_ID, SUBNET_ID, V4_NET_IP_2, INB_IP, 0);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v4_del_route(NET_ID, SUBNET_ID, V4_NET_IP_2);
+		privnet_v4_del_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	TEST("drop_endpoint_miss", {
+		__u32 status_code;
+
+		privnet_v4_add_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN, SUBNET_ID);
+		privnet_v4_add_endpoint_entry(NET_ID, SUBNET_ID, V4_NET_IP_2, V4_POD_IP_1);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v4_del_endpoint_entry(NET_ID, SUBNET_ID, V4_NET_IP_2, V4_POD_IP_1);
+		privnet_v4_del_subnet_entry(NET_ID, SUBNET_V4, SUBNET_V4_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	test_finish();
+}
+
+PKTGEN("tc", "02_privnet_evpn_ingress_v6")
+int privnet_evpn_ingress_v6_pktgen(struct __ctx_buff *ctx)
+{
+	build_privnet_packet(ctx, LXC_ICMP6_NA_LL);
+	return 0;
+}
+
+CHECK("tc", "02_privnet_evpn_ingress_v6")
+int privnet_evpn_ingress_v6_check(struct __ctx_buff *ctx)
+{
+	struct endpoint_key ep_key = {
+		.family = ENDPOINT_KEY_IPV6,
+	};
+
+	test_init();
+
+	TEST("baseline", {
+		__u32 status_code;
+
+		privnet_v6_add_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN, SUBNET_ID);
+		privnet_v6_add_endpoint_entry(NET_ID, SUBNET_ID,
+					      (const union v6addr *)V6_NET_IP_1,
+					      (const union v6addr *)V6_POD_IP_1);
+		endpoint_v6_add_entry((const union v6addr *)V6_POD_IP_1, 1, 0, 0, 0,
+				      (const __u8 *)mac_four, (const __u8 *)mac_five);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v6_del_endpoint_entry(NET_ID, SUBNET_ID,
+					      (const union v6addr *)V6_NET_IP_1,
+					      (const union v6addr *)V6_POD_IP_1);
+		privnet_v6_del_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN);
+		memcpy(&ep_key.ip6, (const union v6addr *)V6_POD_IP_1, sizeof(ep_key.ip6));
+		map_delete_elem(&cilium_lxc, &ep_key);
+
+		if (status_code != TC_ACT_REDIRECT)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   TC_ACT_REDIRECT, status_code);
+	});
+
+	TEST("drop_fib_miss", {
+		__u32 status_code;
+
+		privnet_v6_add_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN, SUBNET_ID);
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+		privnet_v6_del_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	TEST("drop_route_entry", {
+		__u32 status_code;
+
+		privnet_v6_add_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN, SUBNET_ID);
+		privnet_v6_add_subnet_route(NET_ID, SUBNET_ID,
+					    (const union v6addr *)V6_NET_IP_1,
+					    (const union v6addr *)V6_POD_IP_2, 0);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v6_del_route(NET_ID, SUBNET_ID, (const union v6addr *)V6_NET_IP_1);
+		privnet_v6_del_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	TEST("drop_endpoint_miss", {
+		__u32 status_code;
+
+		privnet_v6_add_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN, SUBNET_ID);
+		privnet_v6_add_endpoint_entry(NET_ID, SUBNET_ID,
+					      (const union v6addr *)V6_NET_IP_1,
+					      (const union v6addr *)V6_POD_IP_1);
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+
+		privnet_v6_del_endpoint_entry(NET_ID, SUBNET_ID,
+					      (const union v6addr *)V6_NET_IP_1,
+					      (const union v6addr *)V6_POD_IP_1);
+		privnet_v6_del_subnet_entry(NET_ID, SUBNET_V6, SUBNET_V6_LEN);
+
+		if (status_code != (__u32)DROP_UNROUTABLE)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNROUTABLE, status_code);
+	});
+
+	test_finish();
+}
+
+PKTGEN("tc", "03_privnet_evpn_ingress_non_ip")
+int privnet_evpn_ingress_non_ip_pktgen(struct __ctx_buff *ctx)
+{
+	build_privnet_packet(ctx, NETIP_ARP_REQ);
+	return 0;
+}
+
+CHECK("tc", "03_privnet_evpn_ingress_non_ip")
+int privnet_evpn_ingress_non_ip_check(struct __ctx_buff *ctx)
+{
+	test_init();
+
+	TEST("drop_unknown_l3", {
+		__u32 status_code;
+
+		status_code = __privnet_evpn_ingress(ctx, NET_ID);
+		if (status_code != (__u32)DROP_UNKNOWN_L3)
+			test_fatal("unexpected status code (expected %d, got %d)",
+				   DROP_UNKNOWN_L3, status_code);
+	});
+
+	test_finish();
+}
