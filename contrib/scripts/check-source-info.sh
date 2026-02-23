883d307ade7dd3150665413aeec06acac779f84a
Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>
2026-02-23 15:25:03 -0800

    contrib: Auto-find source files in check-source-info.sh
    This is a follow-up for the 6c968d14d49. Instead of injecting the source
    information files manually, automatically find source files by matching
    the magic marker "@@ source files list begin". This makes it easier to
    run the script stand-alone and also makes it resillient against file
    name changes.
    
    Signed-off-by: Yutaro Hayakawa <yutaro.hayakawa@isovalent.com>

--- contrib/scripts/check-source-info.sh
+++ contrib/scripts/check-source-info.sh
@@ -2,34 +2,35 @@
 
 set -euo pipefail
 
+bpf_source_info=$(grep --exclude-dir=vendor/ --include='*.h' -Rl "@@ source files list begin" | tr '\n' ' ')
+go_source_info=$(grep --exclude-dir=vendor/ --include='*.go' -Rl "@@ source files list begin" | tr '\n' ' ')
+
 #
 # This simple awk command extracts file names from the definition of the
 # __id_for_file function. This is the list of file names which
 # are currently assigned a numerical ID.
 #
 defined_files=$(
-	echo ${BPF_SOURCE_INFO} |
-	xargs -n1 awk -F: '/@@ source files list begin/{found=1; next}
-	/@@ source files list end/{exit}
+	awk -F: '/@@ source files list begin/{found=1; next}
+	/@@ source files list end/{found=0; nextfile}
 	{if (!found || !/_strcase_/) next}
-	{gsub(/.* |"|\)|;/, "", $1); print $1}' |
-	sort -u
+	{gsub(/.* |"|\)|;/, "", $1); print $1}
+	' ${bpf_source_info} | sort -u
 )
 
 #
 # Now let's find all the names defined inside the go source file
 #
 defined_files_go=$(
-	echo ${GO_SOURCE_INFO} |
-	xargs -n1 awk '/@@ source files list begin/{found=1; next}
+	awk '/@@ source files list begin/{found=1; next}
 	{if (!found) next}
-	/@@ source files list end/{exit}
+	/@@ source files list end/{found=0; nextfile}
 	{if (!/: /) next}
-	{gsub(/"|,/, "", $2); print $2}' |
-	sort -u
+	{gsub(/"|,/, "", $2); print $2}
+	' ${go_source_info} | sort -u
 )
 if [ "$defined_files" != "$defined_files_go" ]; then
-	echo "File lists in ${BPF_SOURCE_INFO} and ${GO_SOURCE_INFO} aren't same, please sync" >&2
+	echo "File lists in ${bpf_source_info} and ${go_source_info} aren't same, please sync" >&2
 	exit 1
 fi
 
@@ -56,7 +57,7 @@ required_files=$(
 retval=0
 for f in $required_files; do
 	if ! grep --silent -w "$f" <<<"$defined_files"; then
-		echo "$0: $f is not defined, please add its mapping to ${BPF_SOURCE_INFO}" >&2
+		echo "$0: $f is not defined, please add its mapping to ${bpf_source_info}" >&2
 		retval=1
 	fi
 done
@@ -66,7 +67,7 @@ done
 #
 for f in $defined_files; do
 	if ! grep --silent -w "$f" <<<"$required_files"; then
-		echo "$0: $f is not using send_drop_notify*, update_(trace_)metrics or send_trace_notify, please remove it from ${BPF_SOURCE_INFO}" >&2
+		echo "$0: $f is not using send_drop_notify*, update_(trace_)metrics or send_trace_notify, please remove it from ${bpf_source_info}" >&2
 		retval=1
 	fi
 done
