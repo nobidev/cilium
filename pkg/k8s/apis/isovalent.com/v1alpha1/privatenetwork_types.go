nobidev/cilium

commit 05b0fd4a1cac05e460372943c40a9f1ec8436a95

privnet: introducing privatenetworknodeattachment CRD
Introducing new CRD for setting up devices in the private-networks. This
allows users to dynamically attach private-network subnets to egress via
specific devices.

In addition, users can specify vlan-id which can be used by Cilium to
dynamically create dot1q devices.

Signed-off-by: harsimran pabla <hpabla@isovalent.com>

pkg/k8s/apis/isovalent.com/client/crds/v1alpha1/privatenetworknodeattachments.yaml
@@ -1,0 +1,168 @@
+---
+apiVersion: apiextensions.k8s.io/v1
+kind: CustomResourceDefinition
+metadata:
+  annotations:
+    controller-gen.kubebuilder.io/version: v0.19.0
+  name: privatenetworknodeattachments.isovalent.com
+spec:
+  group: isovalent.com
+  names:
+    categories:
+    - cilium
+    - isovalent
+    kind: PrivateNetworkNodeAttachment
+    listKind: PrivateNetworkNodeAttachmentList
+    plural: privatenetworknodeattachments
+    shortNames:
+    - pnna
+    singular: privatenetworknodeattachment
+  scope: Cluster
+  versions:
+  - name: v1alpha1
+    schema:
+      openAPIV3Schema:
+        description: PrivateNetworkNodeAttachment defines a node device management
+          of private networks.
+        properties:
+          apiVersion:
+            description: |-
+              APIVersion defines the versioned schema of this representation of an object.
+              Servers should convert recognized schemas to the latest internal value, and
+              may reject unrecognized values.
+              More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#resources
+            type: string
+          kind:
+            description: |-
+              Kind is a string value representing the REST resource this object represents.
+              Servers may infer this from the endpoint the client submits requests to.
+              Cannot be updated.
+              In CamelCase.
+              More info: https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#types-kinds
+            type: string
+          metadata:
+            type: object
+          spec:
+            description: The private network specification.
+            properties:
+              attachments:
+                description: Attachments is a list of egress devices to be configured
+                  on the selected nodes.
+                items:
+                  properties:
+                    interface:
+                      description: |-
+                        Interface specifies the network interface used for private network
+                        traffic ingress and egress on the selected nodes. This interface must be
+                        present on the node and appropriately connected to underlying network
+                        infrastructure.
+                      type: string
+                    subnetRefs:
+                      description: |-
+                        Subnets is a list of subnets reachable via this attachment.
+
+                        If empty / nil means all subnets configured in the private-network resource
+                        are selected.
+                      items:
+                        properties:
+                          name:
+                            description: Name of the Subnet specified in the private-network
+                              resource.
+                            type: string
+                        required:
+                        - name
+                        type: object
+                      type: array
+                    vlanID:
+                      description: |-
+                        VlanID, when specified, result in Cilium to create a DOT1Q VLAN subinterface
+                        with parent device specified in Interface.
+                      maximum: 4094
+                      minimum: 1
+                      type: integer
+                  required:
+                  - interface
+                  type: object
+                minItems: 1
+                type: array
+                x-kubernetes-list-map-keys:
+                - interface
+                x-kubernetes-list-type: map
+              nodeSelector:
+                description: |-
+                  NodeSelector selects the nodes to which this configuration applies.
+
+                  If empty / omitted then this config will apply to all nodes.
+                properties:
+                  matchExpressions:
+                    description: matchExpressions is a list of label selector requirements.
+                      The requirements are ANDed.
+                    items:
+                      description: |-
+                        A label selector requirement is a selector that contains values, a key, and an operator that
+                        relates the key and values.
+                      properties:
+                        key:
+                          description: key is the label key that the selector applies
+                            to.
+                          type: string
+                        operator:
+                          description: |-
+                            operator represents a key's relationship to a set of values.
+                            Valid operators are In, NotIn, Exists and DoesNotExist.
+                          enum:
+                          - In
+                          - NotIn
+                          - Exists
+                          - DoesNotExist
+                          type: string
+                        values:
+                          description: |-
+                            values is an array of string values. If the operator is In or NotIn,
+                            the values array must be non-empty. If the operator is Exists or DoesNotExist,
+                            the values array must be empty. This array is replaced during a strategic
+                            merge patch.
+                          items:
+                            type: string
+                          type: array
+                          x-kubernetes-list-type: atomic
+                      required:
+                      - key
+                      - operator
+                      type: object
+                    type: array
+                    x-kubernetes-list-type: atomic
+                  matchLabels:
+                    additionalProperties:
+                      description: MatchLabelsValue represents the value from the
+                        MatchLabels {key,value} pair.
+                      maxLength: 63
+                      pattern: ^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$
+                      type: string
+                    description: |-
+                      matchLabels is a map of {key,value} pairs. A single {key,value} in the matchLabels
+                      map is equivalent to an element of matchExpressions, whose key field is "key", the
+                      operator is "In", and the values array contains only "value". The requirements are ANDed.
+                    type: object
+                type: object
+                x-kubernetes-map-type: atomic
+              privateNetworkRef:
+                description: |-
+                  PrivateNetworkRef identifies the private-network resource that this configuration
+                  is linked to.
+                properties:
+                  name:
+                    description: Name of the ClusterwidePrivateNetwork resource.
+                    type: string
+                required:
+                - name
+                type: object
+            required:
+            - attachments
+            - privateNetworkRef
+            type: object
+        required:
+        - spec
+        type: object
+    served: true
+    storage: true

pkg/k8s/apis/isovalent.com/v1alpha1/privatenetwork_types.go
@@ -89,6 +89,11 @@ type PrivateNetworkSpec struct {
 }
 
 type SubnetSpec struct {
+	// The name of the subnet.
+	//
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+
 	// The CIDR (either v4 or v6) associated with the private network.
 	CIDR NetworkCIDR `json:"cidr"`
 }
@@ -353,4 +358,100 @@ func (in *PrivateNetworkExternalEndpointStatus) DeepEqual(other *PrivateNetworkE
 	}
 
 	return true
+}
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:categories={cilium,isovalent},singular="privatenetworknodeattachment",path="privatenetworknodeattachments",scope="Cluster",shortName={pnna}
+// +kubebuilder:object:root=true
+// +kubebuilder:storageversion
+// +deepequal-gen=false
+
+// PrivateNetworkNodeAttachment defines a node device management of private networks.
+type PrivateNetworkNodeAttachment struct {
+	// +deepequal-gen=false
+	metav1.TypeMeta `json:",inline"`
+	// +deepequal-gen=false
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// The private network specification.
+	//
+	// +kubebuilder:validation:Required
+	Spec PrivateNetworkNodeAttachmentSpec `json:"spec"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +deepequal-gen=false
+
+// PrivateNetworkNodeAttachmentList is a list of PrivateNetworkNodeAttachment objects.
+type PrivateNetworkNodeAttachmentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+
+	// Items is a list of PrivateNetworkNodeAttachment.
+	Items []PrivateNetworkNodeAttachment `json:"items"`
+}
+
+type PrivateNetworkNodeAttachmentSpec struct {
+	// PrivateNetworkRef identifies the private-network resource that this configuration
+	// is linked to.
+	//
+	// +kubebuilder:validation:Required
+	PrivateNetworkRef PrivateNetworkRef `json:"privateNetworkRef"`
+
+	// NodeSelector selects the nodes to which this configuration applies.
+	//
+	// If empty / omitted then this config will apply to all nodes.
+	//
+	// +kubebuilder:validation:Optional
+	NodeSelector slim_metav1.LabelSelector `json:"nodeSelector,omitempty"`
+
+	// Attachments is a list of egress devices to be configured on the selected nodes.
+	//
+	// +kubebuilder:validation:Required
+	// +listType=map
+	// +listMapKey=interface
+	// +kubebuilder:validation:MinItems=1
+	Attachments []PrivateNetworkAttachment `json:"attachments"`
+}
+
+type PrivateNetworkRef struct {
+	// Name of the ClusterwidePrivateNetwork resource.
+	//
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+}
+
+type PrivateNetworkSubnetRef struct {
+	// Name of the Subnet specified in the private-network resource.
+	//
+	// +kubebuilder:validation:Required
+	Name string `json:"name"`
+}
+
+type PrivateNetworkAttachment struct {
+	// Interface specifies the network interface used for private network
+	// traffic ingress and egress on the selected nodes. This interface must be
+	// present on the node and appropriately connected to underlying network
+	// infrastructure.
+	//
+	// +kubebuilder:validation:Required
+	Interface string `json:"interface"`
+
+	// Subnets is a list of subnets reachable via this attachment.
+	//
+	// If empty / nil means all subnets configured in the private-network resource
+	// are selected.
+	//
+	// +kubebuilder:validation:Optional
+	SubnetRefs []PrivateNetworkSubnetRef `json:"subnetRefs,omitempty"`
+
+	// VlanID, when specified, result in Cilium to create a DOT1Q VLAN subinterface
+	// with parent device specified in Interface.
+	//
+	// +kubebuilder:validation:Optional
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=4094
+	VlanID *int `json:"vlanID,omitempty"`
 }

contrib/scripts/enterprise-k8s-manifests-gen.sh
@@ -47,7 +47,8 @@ CRDS_ISOVALENT_V1ALPHA1="isovalentfqdngroups \
                          isovalentnetworkpolicies \
                          clusterwideprivatenetworks \
                          privatenetworkendpointslices \
-                         privatenetworkexternalendpoints"
+                         privatenetworkexternalendpoints \
+                         privatenetworknodeattachments"
 
 TMPDIR=$(mktemp -d -t cilium.tmpXXXXXXXX)
 go run sigs.k8s.io/controller-tools/cmd/controller-gen ${CRD_OPTIONS} paths="${CRD_PATHS}" output:crd:artifacts:config="${TMPDIR}"

pkg/k8s/apis/isovalent.com/client/crds/v1alpha1/clusterwideprivatenetworks.yaml
@@ -165,8 +165,12 @@ spec:
                         private network.
                       format: cidr
                       type: string
+                    name:
+                      description: The name of the subnet.
+                      type: string
                   required:
                   - cidr
+                  - name
                   type: object
                 minItems: 1
                 type: array

pkg/k8s/apis/isovalent.com/client/register.go
@@ -113,6 +113,9 @@ const (
 
 	// PrivateNetworkExternalEndpointCRDName is the full name of the PrivateNetworkExternalEndpoint CRD.
 	PrivateNetworkExternalEndpointCRDName = k8sconstv1alpha1.PrivateNetworkExternalEndpointKindDefinition + "/" + k8sconstv1alpha1.CustomResourceDefinitionVersion
+
+	// PrivateNetworkNodeAttachmentCRDName is the full name of the PrivateNetworkNodeAttachment CRD.
+	PrivateNetworkNodeAttachmentCRDName = k8sconstv1alpha1.PrivateNetworkNodeAttachmentKindDefinition + "/" + k8sconstv1alpha1.CustomResourceDefinitionVersion
 )
 
 type CRDList struct {
@@ -239,6 +242,10 @@ func CustomResourceDefinitionList() map[string]*CRDList {
 			Name:     PrivateNetworkExternalEndpointCRDName,
 			FullName: k8sconstv1alpha1.PrivateNetworkExternalEndpointName,
 		},
+		synced.CRDResourceName(k8sconstv1alpha1.PrivateNetworkNodeAttachmentName): {
+			Name:     PrivateNetworkNodeAttachmentCRDName,
+			FullName: k8sconstv1alpha1.PrivateNetworkNodeAttachmentName,
+		},
 	}
 }
 
@@ -348,6 +355,9 @@ var (
 
 	//go:embed crds/v1alpha1/privatenetworkexternalendpoints.yaml
 	crdsv1Alpha1PrivateNetworkExternalEndpoints []byte
+
+	//go:embed crds/v1alpha1/privatenetworknodeattachments.yaml
+	crdsv1Alpha1PrivateNetworkNodeAttachments []byte
 )
 
 // GetPregeneratedCRD returns the pregenerated CRD based on the requested CRD
@@ -421,6 +431,8 @@ func GetPregeneratedCRD(logger *slog.Logger, crdName string) apiextensionsv1.Cus
 		crdBytes = crdsv1Alpha1PrivateNetworkEndpointSlices
 	case PrivateNetworkExternalEndpointCRDName:
 		crdBytes = crdsv1Alpha1PrivateNetworkExternalEndpoints
+	case PrivateNetworkNodeAttachmentCRDName:
+		crdBytes = crdsv1Alpha1PrivateNetworkNodeAttachments
 	default:
 		logging.Fatal(scopedLog, "Pregenerated CRD does not exist")
 	}

pkg/k8s/apis/isovalent.com/register.go
@@ -12,7 +12,7 @@ const (
 	//
 	// Maintainers: Run ./Documentation/check-crd-compat-table.sh for each release
 	// Developers: Bump patch for each change in the CRD schema.
-	CustomResourceDefinitionSchemaVersion = "1.30.25"
+	CustomResourceDefinitionSchemaVersion = "1.30.26"
 
 	// CustomResourceDefinitionSchemaVersionKey is key to label which holds the CRD schema version
 	CustomResourceDefinitionSchemaVersionKey = "com.isovalent.k8s.crd.schema.version"

pkg/k8s/apis/isovalent.com/v1alpha1/register.go
@@ -125,6 +125,11 @@ const (
 	PrivateNetworkExternalEndpointPluralName     = "privatenetworkexternalendpoints"
 	PrivateNetworkExternalEndpointKindDefinition = "PrivateNetworkExternalEndpoint"
 	PrivateNetworkExternalEndpointName           = PrivateNetworkExternalEndpointPluralName + "." + CustomResourceDefinitionGroup
+
+	// PrivateNetworkNodeAttachment
+	PrivateNetworkNodeAttachmentPluralName     = "privatenetworknodeattachments"
+	PrivateNetworkNodeAttachmentKindDefinition = "PrivateNetworkNodeAttachment"
+	PrivateNetworkNodeAttachmentName           = PrivateNetworkNodeAttachmentPluralName + "." + CustomResourceDefinitionGroup
 )
 
 // SchemeGroupVersion is group version used to register these objects
@@ -221,6 +226,8 @@ func addKnownTypes(scheme *runtime.Scheme) error {
 		&PrivateNetworkEndpointSliceList{},
 		&PrivateNetworkExternalEndpoint{},
 		&PrivateNetworkExternalEndpointList{},
+		&PrivateNetworkNodeAttachment{},
+		&PrivateNetworkNodeAttachmentList{},
 	)
 
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)

pkg/k8s/apis/isovalent.com/v1alpha1/zz_generated.deepcopy.go
@@ -5831,6 +5831,32 @@ func (in *PortProtocol) DeepCopy() *PortProtocol {
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkAttachment) DeepCopyInto(out *PrivateNetworkAttachment) {
+	*out = *in
+	if in.SubnetRefs != nil {
+		in, out := &in.SubnetRefs, &out.SubnetRefs
+		*out = make([]PrivateNetworkSubnetRef, len(*in))
+		copy(*out, *in)
+	}
+	if in.VlanID != nil {
+		in, out := &in.VlanID, &out.VlanID
+		*out = new(int)
+		**out = **in
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkAttachment.
+func (in *PrivateNetworkAttachment) DeepCopy() *PrivateNetworkAttachment {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkAttachment)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *PrivateNetworkEndpointAddressing) DeepCopyInto(out *PrivateNetworkEndpointAddressing) {
 	*out = *in
 	return
@@ -6079,6 +6105,107 @@ func (in *PrivateNetworkExternalEndpointStatus) DeepCopy() *PrivateNetworkExtern
 }
 
 // DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkNodeAttachment) DeepCopyInto(out *PrivateNetworkNodeAttachment) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
+	in.Spec.DeepCopyInto(&out.Spec)
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkNodeAttachment.
+func (in *PrivateNetworkNodeAttachment) DeepCopy() *PrivateNetworkNodeAttachment {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkNodeAttachment)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *PrivateNetworkNodeAttachment) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkNodeAttachmentList) DeepCopyInto(out *PrivateNetworkNodeAttachmentList) {
+	*out = *in
+	out.TypeMeta = in.TypeMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
+	if in.Items != nil {
+		in, out := &in.Items, &out.Items
+		*out = make([]PrivateNetworkNodeAttachment, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkNodeAttachmentList.
+func (in *PrivateNetworkNodeAttachmentList) DeepCopy() *PrivateNetworkNodeAttachmentList {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkNodeAttachmentList)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyObject is an autogenerated deepcopy function, copying the receiver, creating a new runtime.Object.
+func (in *PrivateNetworkNodeAttachmentList) DeepCopyObject() runtime.Object {
+	if c := in.DeepCopy(); c != nil {
+		return c
+	}
+	return nil
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkNodeAttachmentSpec) DeepCopyInto(out *PrivateNetworkNodeAttachmentSpec) {
+	*out = *in
+	out.PrivateNetworkRef = in.PrivateNetworkRef
+	in.NodeSelector.DeepCopyInto(&out.NodeSelector)
+	if in.Attachments != nil {
+		in, out := &in.Attachments, &out.Attachments
+		*out = make([]PrivateNetworkAttachment, len(*in))
+		for i := range *in {
+			(*in)[i].DeepCopyInto(&(*out)[i])
+		}
+	}
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkNodeAttachmentSpec.
+func (in *PrivateNetworkNodeAttachmentSpec) DeepCopy() *PrivateNetworkNodeAttachmentSpec {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkNodeAttachmentSpec)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkRef) DeepCopyInto(out *PrivateNetworkRef) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkRef.
+func (in *PrivateNetworkRef) DeepCopy() *PrivateNetworkRef {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkRef)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
 func (in *PrivateNetworkSpec) DeepCopyInto(out *PrivateNetworkSpec) {
 	*out = *in
 	if in.VNI != nil {
@@ -6134,6 +6261,22 @@ func (in *PrivateNetworkStatus) DeepCopy() *PrivateNetworkStatus {
 		return nil
 	}
 	out := new(PrivateNetworkStatus)
+	in.DeepCopyInto(out)
+	return out
+}
+
+// DeepCopyInto is an autogenerated deepcopy function, copying the receiver, writing into out. in must be non-nil.
+func (in *PrivateNetworkSubnetRef) DeepCopyInto(out *PrivateNetworkSubnetRef) {
+	*out = *in
+	return
+}
+
+// DeepCopy is an autogenerated deepcopy function, copying the receiver, creating a new PrivateNetworkSubnetRef.
+func (in *PrivateNetworkSubnetRef) DeepCopy() *PrivateNetworkSubnetRef {
+	if in == nil {
+		return nil
+	}
+	out := new(PrivateNetworkSubnetRef)
 	in.DeepCopyInto(out)
 	return out
 }

commit f5cee77fa273615d1eb06fa97b17e1cb0ec643c6

privnet: don't require unique interface in PNNA attachments list
Currently, a parent interface can only be specified once in the
attachments list of a PrivateNetworkNodeAttachmentSpec, e.g. applying

```yaml
apiVersion: isovalent.com/v1alpha1
kind: PrivateNetworkNodeAttachment
metadata:
  name: test
spec:
  privateNetworkRef:
    name: network
  attachments:
    - interface: eth1
      vlanID: 10
      subnetRefs:
        - name: bar
    - interface: eth1
      vlanID: 20
      subnetRefs:
        - name: foo
```

errors with

    The PrivateNetworkNodeAttachment "test" is invalid: spec.attachments[1]: Duplicate value: map[string]interface {}{"interface":"eth1"}

This is because the x-kubernetes-list-type: map marker along with
x-kubernetes-list-map-keys: interface is set on the attachments array.
Drop this restriction to allow specifying a parent device more than once
which is required when a user wants to create multiple sub-devices (i.e.
attach multiple VLANs) per parent device.

Fixes: 05b0fd4a1cac ("privnet: introducing privatenetworknodeattachment CRD")
Ref. https://github.com/isovalent/cilium/pull/10016#discussion_r2811837072
Reported-by: Marco Iorio <marco.iorio@isovalent.com>
Signed-off-by: Tobias Klauser <tobias@isovalent.com>

pkg/k8s/apis/isovalent.com/client/crds/v1alpha1/privatenetworknodeattachments.yaml
@@ -94,9 +94,6 @@ spec:
                   type: object
                 minItems: 1
                 type: array
-                x-kubernetes-list-map-keys:
-                - interface
-                x-kubernetes-list-type: map
               nodeSelector:
                 description: |-
                   NodeSelector selects the nodes to which this configuration applies.

pkg/k8s/apis/isovalent.com/v1alpha1/privatenetwork_types.go
@@ -521,8 +521,6 @@ type PrivateNetworkNodeAttachmentSpec struct {
 	// Attachments is a list of egress devices to be configured on the selected nodes.
 	//
 	// +kubebuilder:validation:Required
-	// +listType=map
-	// +listMapKey=interface
 	// +kubebuilder:validation:MinItems=1
 	Attachments []PrivateNetworkAttachment `json:"attachments"`
 }