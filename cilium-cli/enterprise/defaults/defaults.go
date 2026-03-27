8dcd450028735d2e76d7c0b791a731e1c20851ee
Marco Iorio <marco.iorio@isovalent.com>
2026-03-27 18:24:31 +0100

    privnet/tests: switch to alpine-based VM images
    Switch our testbed to using alpine-based lightweight VM images, rather
    than the ones based fedora, which are significantly more demanding in
    terms of resources. Consequently, let's switch the amount of RAM
    allocated per VM to 256MB.
    
    Configuration wise, the only difference is around the usage of openrc,
    instead of systemd, to manage the echoserver service.
    
    Suggested-by: David Bimmler <david.bimmler@isovalent.com>
    Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

--- cilium-cli/enterprise/defaults/defaults.go
+++ cilium-cli/enterprise/defaults/defaults.go
@@ -33,6 +33,6 @@ var (
 
 	PrivnetTestImages = map[string]string{
 		// renovate: datasource=docker
-		"VMImage": "quay.io/kubevirt/fedora-with-test-tooling-container-disk:v1.7.2@sha256:640e53b936ee5755172523cc1c50c18dd72271cc206996bcf5bc26fd9c2de178",
+		"VMImage": "quay.io/kubevirt/alpine-with-test-tooling-container-disk:v1.7.2@sha256:3189d81129c59432a13c612a69e87702620d554a859bfce7fd8026f05f00ba61",
 	}
 )
