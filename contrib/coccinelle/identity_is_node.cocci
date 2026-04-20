nobidev/cilium

commit b9444865a5ad7463f45eb5d46a42aa03acb066fa

bpf: remove unused identity_is_node()
1102220d000f ("bpf: wireguard: let strict-mode capture traffic to HOST_ID")
removed the last user.

Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

contrib/coccinelle/identity_is_node.cocci
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Authors of Cilium.
-/// Find missing calls to identity_is_remote_node and identity_is_node.
-/// We want to use those functions whenever possible, to make sure
+/// Find missing calls to identity_is_remote_node.
+/// We want to use this function whenever possible, to make sure
 /// KUBE_APISERVER_NODE_ID is properly accounted for and to prepare for
 /// future extensions.
 // Confidence: Medium
@@ -16,43 +16,25 @@ cnt = 0
 
 
 @rule@
-position p1 : script:python() { p1[0].current_element not in ["identity_is_node"] };
-position p2 : script:python() { p2[0].current_element not in ["identity_is_remote_node"] };
+position p1 : script:python() { p2[0].current_element not in ["identity_is_remote_node"] };
 expression e;
 @@
 
 (
-- e != HOST_ID && e != REMOTE_NODE_ID@p1
-+ !identity_is_node(e)
-|
-- e != REMOTE_NODE_ID@p1 && e != HOST_ID
-+ !identity_is_node(e)
-|
-- e == HOST_ID || e == REMOTE_NODE_ID@p1
-+ identity_is_node(e)
-|
-- e == REMOTE_NODE_ID@p1 || e == HOST_ID
-+ identity_is_node(e)
-|
-- e == REMOTE_NODE_ID@p2
+- e == REMOTE_NODE_ID@p1
 + identity_is_remote_node(e)
 |
-- e != REMOTE_NODE_ID@p2
+- e != REMOTE_NODE_ID@p1
 + !identity_is_remote_node(e)
 )
 
 
 @script:python@
 p1 << rule.p1 = [];
-p2 << rule.p2 = [];
 @@
 
 if len(p1) > 0:
-  print("* file %s: use identity_is_node on line %s" % (p1[0].file, p1[0].line))
-  cnt += 1
-
-if len(p2) > 0:
-  print("* file %s: use identity_is_remote_node on line %s" % (p2[0].file, p2[0].line))
+  print("* file %s: use identity_is_remote_node on line %s" % (p1[0].file, p1[0].line))
   cnt += 1
 
 

bpf/lib/identity.h
@@ -111,11 +111,6 @@ static __always_inline bool identity_is_remote_node(__u32 identity)
 		(identity & IDENTITY_LOCAL_SCOPE_MASK) == IDENTITY_LOCAL_SCOPE_REMOTE_NODE;
 }
 
-static __always_inline bool identity_is_node(__u32 identity)
-{
-	return identity_is_host(identity) || identity_is_remote_node(identity);
-}
-
 /**
  * identity_is_reserved is used to determine whether an identity is one of the
  * reserved identities that are not handed out to endpoints.

commit 472c6479309a60555e746f6e2f703023a5ce35d3

contrib: fix typo in identity_is_node.cocci
Traceback (most recent call last):
  File "<string>", line 3, in <module>
NameError: name 'p2' is not defined. Did you mean: 'p1'?
EXN: Failure("Error in Python script, line 19, file \"<snip>/cilium/contrib/coccinelle/identity_is_node.cocci\": Python failure") in ./lib/identity.h

Fixes: b9444865a5ad ("bpf: remove unused identity_is_node()")
Signed-off-by: Julian Wiedmann <jwi@isovalent.com>

contrib/coccinelle/identity_is_node.cocci
@@ -16,7 +16,7 @@ cnt = 0
 
 
 @rule@
-position p1 : script:python() { p2[0].current_element not in ["identity_is_remote_node"] };
+position p1 : script:python() { p1[0].current_element not in ["identity_is_remote_node"] };
 expression e;
 @@
 