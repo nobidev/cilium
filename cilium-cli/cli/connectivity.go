nobidev/cilium

commit 018cb7dccba4a8f863873bbd0e78dde7c141fc77

cilium-cli: Move ExternalDeploymentPort away from node-local-dns health
node-local-dns uses port 8080 for liveness probes [1], which prevents
Cilium connectivity tests from deploying echo-external-node on "nodes
without Cilium" if node-local-dns runs there:

Some error occurred Error: listen EADDRINUSE: address already in use :::8080
    at Server.setupListenHandle [as _listen2] (node:net:1940:16)
    at listenInCluster (node:net:1997:12)
    at Server.listen (node:net:2102:7)
    at app.listen (/usr/local/lib/node_modules/json-server/node_modules/express/lib/application.js:636:24)
    at /usr/local/lib/node_modules/json-server/lib/cli/run.js:112:20 {
  code: 'EADDRINUSE',
  errno: -98,
  syscall: 'listen',
  address: '::',
  port: 8080
}

Move the port used by Cilium tests to fix the clash with node-local-dns.

[1]: https://github.com/kubernetes/kubernetes/blob/7f890ab7ade4ecfff92cc81621216ae1b87fa827/cluster/addons/dns/nodelocaldns/nodelocaldns.yaml#L165

Signed-off-by: Alice Mikityanska <alice@isovalent.com>

cilium-cli/cli/connectivity.go
@@ -40,7 +40,7 @@ func newCmdConnectivity(hooks api.Hooks) *cobra.Command {
 }
 
 var params = check.Parameters{
-	ExternalDeploymentPort: 8080,
+	ExternalDeploymentPort: 8090,
 	EchoServerHostPort:     4000,
 	Writer:                 os.Stdout,
 	SysdumpOptions: sysdump.Options{

commit 0075763092d00da4adc7094098f112879ae1ba4e

cilium-cli: Fix port conflict for echo-external-node
Commit 018cb7dccba4 ("cilium-cli: Move ExternalDeploymentPort away from
node-local-dns health") moved the echo-external-node deployment from
TCP/8080 to TCP/8090 to avoid a conflict with node-local-dns health
checks.

TCP/8090 is the base port and it gets incremented for each new pod, in
case multiple pods are deployed on the same node. Unfortunately,
TCP/8094 can conflict with a konnectivity-agent component on AKS. We
thus need to move again.

The conflict wasn't identified when we moved to TCP/8090 because we
didn't have the same coverage for AKS at the time. The conflict was
identified on GKE instead.

Fixes: 018cb7dccba4 ("cilium-cli: Move ExternalDeploymentPort away from node-local-dns health")
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

cilium-cli/cli/connectivity.go
@@ -40,7 +40,7 @@ func newCmdConnectivity(hooks api.Hooks) *cobra.Command {
 }
 
 var params = check.Parameters{
-	ExternalDeploymentPort: 8090,
+	ExternalDeploymentPort: 8190,
 	EchoServerHostPort:     4000,
 	Writer:                 os.Stdout,
 	SysdumpOptions: sysdump.Options{