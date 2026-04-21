nobidev/cilium

commit 40b523eec7bd0a1e358f58d56b93de509d8559f3

chore(deps): update quay.io/goswagger/swagger docker tag to v0.33.2
Signed-off-by: cilium-renovate[bot] <134692979+cilium-renovate[bot]@users.noreply.github.com>

pkg/client/client.go
@@ -451,24 +451,24 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 		FormatStatusResponseRemoteClusters(w, sr.ClusterMesh.Clusters, verbosity)
 	}
 
-	if sr.IPV4BigTCP != nil {
+	if sr.IPv4BigTCP != nil {
 		status := "Disabled"
-		if sr.IPV4BigTCP.Enabled {
-			max := fmt.Sprintf("[%d]", sr.IPV4BigTCP.MaxGSO)
-			if sr.IPV4BigTCP.MaxGRO != sr.IPV4BigTCP.MaxGSO {
-				max = fmt.Sprintf("[%d, %d]", sr.IPV4BigTCP.MaxGRO, sr.IPV4BigTCP.MaxGSO)
+		if sr.IPv4BigTCP.Enabled {
+			max := fmt.Sprintf("[%d]", sr.IPv4BigTCP.MaxGSO)
+			if sr.IPv4BigTCP.MaxGRO != sr.IPv4BigTCP.MaxGSO {
+				max = fmt.Sprintf("[%d, %d]", sr.IPv4BigTCP.MaxGRO, sr.IPv4BigTCP.MaxGSO)
 			}
 			status = fmt.Sprintf("Enabled\t%s", max)
 		}
 		fmt.Fprintf(w, "IPv4 BIG TCP:\t%s\n", status)
 	}
 
-	if sr.IPV6BigTCP != nil {
+	if sr.IPv4BigTCP != nil {
 		status := "Disabled"
-		if sr.IPV6BigTCP.Enabled {
-			max := fmt.Sprintf("[%d]", sr.IPV6BigTCP.MaxGSO)
-			if sr.IPV6BigTCP.MaxGRO != sr.IPV6BigTCP.MaxGSO {
-				max = fmt.Sprintf("[%d, %d]", sr.IPV6BigTCP.MaxGRO, sr.IPV6BigTCP.MaxGSO)
+		if sr.IPv4BigTCP.Enabled {
+			max := fmt.Sprintf("[%d]", sr.IPv4BigTCP.MaxGSO)
+			if sr.IPv4BigTCP.MaxGRO != sr.IPv4BigTCP.MaxGSO {
+				max = fmt.Sprintf("[%d, %d]", sr.IPv4BigTCP.MaxGRO, sr.IPv4BigTCP.MaxGSO)
 			}
 			status = fmt.Sprintf("Enabled\t%s", max)
 		}
@@ -548,7 +548,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 
 		if sr.Masquerading.EnabledProtocols == nil {
 			status = enabled(sr.Masquerading.Enabled)
-		} else if !sr.Masquerading.EnabledProtocols.IPV4 && !sr.Masquerading.EnabledProtocols.IPV6 {
+		} else if !sr.Masquerading.EnabledProtocols.IPv4 && !sr.Masquerading.EnabledProtocols.IPv6 {
 			status = enabled(false)
 		} else {
 			if sr.Masquerading.Mode == models.MasqueradingModeBPF {
@@ -578,7 +578,7 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 			}
 
 			status = fmt.Sprintf("%s [IPv4: %s, IPv6: %s]", status,
-				enabled(sr.Masquerading.EnabledProtocols.IPV4), enabled(sr.Masquerading.EnabledProtocols.IPV6))
+				enabled(sr.Masquerading.EnabledProtocols.IPv4), enabled(sr.Masquerading.EnabledProtocols.IPv6))
 		}
 		fmt.Fprintf(w, "Masquerading:\t%s\n", status)
 	}

commit 2cc044e4bc88f919cd9e06c7cc15ebaa17ab4139

cilium: Fix incorrect IPv6 BIG TCP display
André fixed up Renovate's commit in 40b523eec7bd ("chore(deps): update
quay.io/goswagger/swagger docker tag to v0.33.2") and introduced a small
error that leads to the IPv4 BIG TCP status being displayed instead of
the IPv6 one.

Fixes: 40b523eec7bd ("chore(deps): update quay.io/goswagger/swagger docker tag to v0.33.2")
Reported-by: Lucas Fernando Cardoso Nunes <lucasfc.nunes@gmail.com>
Signed-off-by: Paul Chaignon <paul.chaignon@gmail.com>

pkg/client/client.go
@@ -463,12 +463,12 @@ func FormatStatusResponse(w io.Writer, sr *models.StatusResponse, sd StatusDetai
 		fmt.Fprintf(w, "IPv4 BIG TCP:\t%s\n", status)
 	}
 
-	if sr.IPv4BigTCP != nil {
+	if sr.IPv6BigTCP != nil {
 		status := "Disabled"
-		if sr.IPv4BigTCP.Enabled {
-			max := fmt.Sprintf("[%d]", sr.IPv4BigTCP.MaxGSO)
-			if sr.IPv4BigTCP.MaxGRO != sr.IPv4BigTCP.MaxGSO {
-				max = fmt.Sprintf("[%d, %d]", sr.IPv4BigTCP.MaxGRO, sr.IPv4BigTCP.MaxGSO)
+		if sr.IPv6BigTCP.Enabled {
+			max := fmt.Sprintf("[%d]", sr.IPv6BigTCP.MaxGSO)
+			if sr.IPv6BigTCP.MaxGRO != sr.IPv6BigTCP.MaxGSO {
+				max = fmt.Sprintf("[%d, %d]", sr.IPv6BigTCP.MaxGRO, sr.IPv6BigTCP.MaxGSO)
 			}
 			status = fmt.Sprintf("Enabled\t%s", max)
 		}